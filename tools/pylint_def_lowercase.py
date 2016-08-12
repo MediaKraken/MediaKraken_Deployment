a7323e0022a4bcca71a6a8e86ab1bf0ded739af2
SpootDev <spootdev@gmail.com>
2016-08-12 17:00:48 -0500

    code changer for def's

--- tools/pylint_def_lowercase.py
+++ tools/pylint_def_lowercase.py
@@ -16,6 +16,28 @@
   MA 02110-1301, USA.
 '''
 
+from __future__ import absolute_import, division, print_function, unicode_literals
 import os
-import common.common_file
+from common import common_file
+#import common.common_file
 
+
+"""
+This program will scan for all py files in specified directory
+skipping the lib directory.
+"""
+
+
+for file_name in common_file.com_file_dir_list(\
+        '/home/spoot/github/MediaKraken/MediaKraken_Deployment', filter_text='py', walk_dir=True,\
+        skip_junk=False, file_size=False, directory_only=False):
+    #print('File: %s' % file_name)
+    with open(file_name) as f_pointer:
+        for file_line in f_pointer:
+            if file_line.split(' ')[0] == 'def':
+                #print('line: %s' % file_line)
+                #print('chunk: %s' % file_line.split(' ')[1].split('(')[0])
+                if file_line.split(' ')[1].split('(')[0].islower() == False:
+                    print("Upper char found: %s" % file_line.split(' ')[1].split('(')[0])
+                    command_string = 'find . -type f -name "*.py" -exec sed -i \'s/' + file_line.split(' ')[1].split('(')[0] + '/' + file_line.split(' ')[1].split('(')[0].lower() + '/g\' {} +'
+                    print("command: %s" % command_string)
