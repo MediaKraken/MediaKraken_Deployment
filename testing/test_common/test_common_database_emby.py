f7edb11320bde3aba0582e9d559eafd6ca1456ff
SpootDev <spootdev@gmail.com>
2016-08-13 14:14:54 -0500

    pylint

--- testing/test_common/test_common_database_emby.py
+++ testing/test_common/test_common_database_emby.py
@@ -21,7 +21,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import pytest
 import sys
 sys.path.append("../common")
-from com_Database_Emby import *
+from com_db_emby import *
 
 
 class TestCommonDatabaseEmby(object):
