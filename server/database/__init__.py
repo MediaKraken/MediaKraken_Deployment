5fafe229d488a8266ff6f6cc9d4a11dcb5d7b01c
SpootDev <spootdev@gmail.com>
2016-08-14 00:07:41 -0500

    lowercase

--- server/database/__init__.py
+++ server/database/__init__.py
@@ -1,25 +1,25 @@
 class MK_Server_Database(object):
-    from db_base import srv_db_Open,\
-        srv_db_Open_Isolation,\
-        srv_db_Close,\
-        srv_db_Commit,\
-        srv_db_Rollback,\
+    from db_base import srv_db_open,\
+        srv_db_open_isolation,\
+        srv_db_close,\
+        srv_db_commit,\
+        srv_db_rollback,\
         srv_db_table_index_check,\
-        srv_db_Table_Count,\
-        srv_db_Query
-    from db_base_activity import srv_db_Activity_Insert,\
-        srv_db_Activity_Purge
-    from db_base_audit import srv_db_Audit_Path_Status,\
-        srv_db_Audit_Path_Update_Status,\
-        srv_db_Audit_Paths_Count,\
-        srv_db_Audit_Path_Update_By_UUID,\
-        srv_db_Audit_Path_Add,\
-        srv_db_Audit_Path_Check,\
-        srv_db_Audit_Directory_Timestamp_Update,\
-        srv_db_Audit_Paths,\
-        srv_db_Audit_Path_Delete,\
-        srv_db_Audit_Path_By_UUID
-    from db_base_channel import srv_db_Channel_Insert
+        srv_db_table_count,\
+        srv_db_query
+    from db_base_activity import srv_db_activity_insert,\
+        srv_db_activity_purge
+    from db_base_audit import srv_db_audit_path_status,\
+        srv_db_audit_path_update_status,\
+        srv_db_audit_paths_count,\
+        srv_db_audit_path_update_by_uuid,\
+        srv_db_audit_path_add,\
+        srv_db_audit_path_check,\
+        srv_db_audit_directory_timestamp_update,\
+        srv_db_audit_paths,\
+        srv_db_audit_path_delete,\
+        srv_db_audit_path_by_uuid
+    from db_base_channel import srv_db_channel_insert
     from db_base_collection import srv_db_media_collection_scan,\
         srv_db_collection_guid_by_name,\
         srv_db_collection_insert,\
@@ -27,63 +27,63 @@ class MK_Server_Database(object):
         srv_db_collection_list,\
         srv_db_collection_read_by_guid,\
         srv_db_collection_by_tmdb
-    from db_base_cron import srv_db_Cron_List_Count,\
-        srv_db_Cron_List,\
-        srv_db_Cron_Time_Update
-    from db_base_device import srv_db_Device_Count,\
-        srv_db_Device_List,\
-        srv_db_Device_Insert,\
-        srv_db_Device_Update,\
-        srv_db_Device_Delete,\
-        srv_db_Device_Read
-    from db_base_download import srv_db_Download_Insert,\
-        srv_db_Download_Read_By_Provider,\
-        srv_db_Download_Delete,\
-        srv_db_download_update_Provider,\
+    from db_base_cron import srv_db_cron_list_count,\
+        srv_db_cron_list,\
+        srv_db_cron_time_update
+    from db_base_device import srv_db_device_count,\
+        srv_db_device_list,\
+        srv_db_device_insert,\
+        srv_db_device_update,\
+        srv_db_device_delete,\
+        srv_db_device_read
+    from db_base_download import srv_db_download_insert,\
+        srv_db_download_read_by_provider,\
+        srv_db_download_delete,\
+        srv_db_download_update_provider,\
         srv_db_download_update
-    from db_base_kodi import srv_db_Kodi_User_Sync,\
-        srv_db_Kodi_User_Sync_List_Added
-    from db_base_iradio import srv_db_iRadio_Insert,\
-        srv_db_iRadio_List_Count,\
-        srv_db_iRadio_List
-    from db_base_link import srv_db_Link_List_Count,\
-        srv_db_Link_List,\
-        srv_db_Link_Insert,\
-        srv_db_Link_Delete
+    from db_base_kodi import srv_db_kodi_user_sync,\
+        srv_db_kodi_user_sync_list_added
+    from db_base_iradio import srv_db_iradio_insert,\
+        srv_db_iradio_list_count,\
+        srv_db_iradio_list
+    from db_base_link import srv_db_link_list_count,\
+        srv_db_link_list,\
+        srv_db_link_insert,\
+        srv_db_link_delete
     from db_base_media_class import srv_db_media_class_list_count,\
         srv_db_media_class_list,\
         srv_db_media_class_by_uuid,\
         srv_db_media_uuid_by_class
-    from db_base_media_games from common import common_media_Game_System_List_Count,\
+    from db_base_media_games import common_media_game_system_list_count,\
         com_media_game_system_list,\
         com_media_game_list_by_system_count,\
         com_media_game_list_by_system,\
         com_media_game_list_count,\
         com_media_game_list
-    from db_base_media_images from common import common_media_Images_List_Count,\
+    from db_base_media_images import common_media_images_list_count,\
         com_media_images_list
     from db_base_media import srv_db_insert_media,\
         srv_db_read_media,\
         srv_db_known_media_count,\
         srv_db_known_media,\
         srv_db_matched_media_count,\
-        srv_db_known_media_All_Unmatched_Count,\
-        srv_db_known_media_All_Unmatched,\
+        srv_db_known_media_all_unmatched_count,\
+        srv_db_known_media_all_unmatched,\
         srv_db_media_duplicate_count,\
         srv_db_media_duplicate,\
-        srv_db_media_duplicate_Detail_Count,\
-        srv_db_media_duplicate_Detail,\
+        srv_db_media_duplicate_detail_count,\
+        srv_db_media_duplicate_detail,\
         srv_db_media_path_by_uuid,\
         srv_db_media_watched_status_update,\
         srv_db_update_media_id,\
         srv_db_update_media_json,\
-        srv_db_known_media_Chapter_Scan,\
+        srv_db_known_media_chapter_scan,\
         srv_db_media_by_metadata_guid,\
         srv_db_media_image_path,\
-        srv_db_read_media_Metadata_Both,\
-        srv_db_read_media_Path_Like,\
-        srv_db_read_media_New,\
-        srv_db_read_media_New_Count,\
+        srv_db_read_media_metadata_both,\
+        srv_db_read_media_path_like,\
+        srv_db_read_media_new,\
+        srv_db_read_media_new_count,\
         srv_db_media_watched_checkpoint_update,\
         srv_db_media_favorite_status_update,\
         srv_db_media_poo_status_update,\
@@ -99,12 +99,12 @@ class MK_Server_Database(object):
         srv_db_known_remote_media_count,\
         srv_db_media_remote_read_new
     from db_base_media_tv import srv_db_web_tvmedia_list,\
-        srv_db_web_tvmedia_list_Count
-    from db_base_metadata import srv_db_read_media_Metadata,\
+        srv_db_web_tvmedia_list_count
+    from db_base_metadata import srv_db_read_media_metadata,\
         srv_db_meta_genre_list_count,\
         srv_db_meta_genre_list,\
         srv_db_meta_movie_count_by_genre,\
-        srv_db_meta_GUID_By_imdb,\
+        srv_db_meta_guid_by_imdb,\
         srv_db_meta_guid_by_tvdb,\
         srv_db_meta_guid_by_tmdb,\
         srv_db_meta_guid_by_rt,\
@@ -114,7 +114,7 @@ class MK_Server_Database(object):
         srv_db_meta_fetch_media_id_json,\
         srv_db_meta_fetch_series_media_id_json,\
         srv_db_find_metadata_guid,\
-        srv_db_meta_update_Media_ID_From_Scudlee
+        srv_db_meta_update_media_id_from_scudlee
     from db_base_metadata_book import srv_db_metabook_guid_by_isbn,\
         srv_db_metabook_guid_by_name,\
         srv_db_metabook_book_insert
@@ -141,12 +141,12 @@ class MK_Server_Database(object):
         srv_db_meta_music_video_detail_by_uuid,\
         srv_db_meta_music_video_count,\
         srv_db_meta_music_video_list
-    from db_base_nas import srv_db_NAS_Count,\
-        srv_db_NAS_List,\
-        srv_db_NAS_Insert,\
-        srv_db_NAS_Update,\
-        srv_db_NAS_Delete,\
-        srv_db_NAS_Read
+    from db_base_nas import srv_db_nas_count,\
+        srv_db_nas_list,\
+        srv_db_nas_insert,\
+        srv_db_nas_update,\
+        srv_db_nas_delete,\
+        srv_db_nas_read
     from db_base_metadata_people import srv_db_meta_person_list_count,\
         srv_db_meta_person_list,\
         srv_db_meta_person_by_guid,\
@@ -155,18 +155,18 @@ class MK_Server_Database(object):
         srv_db_metdata_person_insert,\
         srv_db_meta_person_id_count,\
         srv_db_meta_person_as_seen_in
-    from db_base_metadata_sports import srv_db_meta_Sports_GUID_By_thesportsdb,\
+    from db_base_metadata_sports import srv_db_meta_sports_guid_by_thesportsdb,\
         srv_db_meta_sports_list_count,\
         srv_db_meta_sports_list,\
         srv_db_meta_sports_guid_by_event_name
-    from db_base_metadata_thesportsdb import srv_db_metathesportsdb_Insert,\
+    from db_base_metadata_thesportsdb import srv_db_metathesportsdb_insert,\
         srv_db_metathesports_update,\
-        srv_db_metathesportsdb_Select_By_Guid
+        srv_db_metathesportsdb_select_by_guid
     from db_base_metadata_thetvdb import srv_db_metatvdb_insert,\
         srv_db_metatvdb_update
-    from db_base_metadata_tv import srv_db_metaTV_GUID_By_imdb,\
+    from db_base_metadata_tv import srv_db_metatv_guid_by_imdb,\
         srv_db_metatv_guid_by_tvdb,\
-        srv_db_metaTV_GUID_By_tvmaze,\
+        srv_db_metatv_guid_by_tvmaze,\
         srv_db_meta_tvshow_list_count,\
         srv_db_meta_tvshow_list,\
         srv_db_meta_tvshow_detail,\
@@ -177,27 +177,27 @@ class MK_Server_Database(object):
         srv_db_read_tvmetadata_eps_season,\
         srv_db_read_tvmetadata_season_eps_list,\
         srv_db_read_tvmetadata_episode
-    from db_base_metadata_tvmaze import srv_db_metatvmaze_Changed_UUID,\
-        srv_db_metatvmaze_Insert,\
-        srv_db_metatvmaze_Update
-    from db_base_notification import srv_db_Notification_Insert,\
-        srv_db_Notification_Read
-    from db_base_option_status import srv_db_Option_Status_Read,\
-        srv_db_Option_Status_Update,\
-        srv_db_Option_Status_Update_Scan_Json,\
-        srv_db_Option_Status_Update_Scan_Json_Rec
-    from db_base_postgresql import srv_db_Postgresql_Table_Sizes,\
-        srv_db_Postgresql_Row_Count,\
-        srv_db_Postgresql_Vacuum_Stat_By_Day,\
-        srv_db_Postgresql_Vacuum_Table
-    from db_base_review import srv_db_Review_Count,\
-        srv_db_Review_List_By_TMDB_GUID,\
+    from db_base_metadata_tvmaze import srv_db_metatvmaze_changed_uuid,\
+        srv_db_metatvmaze_insert,\
+        srv_db_metatvmaze_update
+    from db_base_notification import srv_db_notification_insert,\
+        srv_db_notification_read
+    from db_base_option_status import srv_db_option_status_read,\
+        srv_db_option_status_update,\
+        srv_db_option_status_update_scan_json,\
+        srv_db_option_status_update_scan_json_rec
+    from db_base_postgresql import srv_db_postgresql_table_sizes,\
+        srv_db_postgresql_row_count,\
+        srv_db_postgresql_vacuum_stat_by_day,\
+        srv_db_postgresql_vacuum_table
+    from db_base_review import srv_db_review_count,\
+        srv_db_review_list_by_tmdb_guid,\
         srv_db_review_insert
-    from db_base_sync import srv_db_Sync_List_Count,\
-        srv_db_Sync_List,\
-        srv_db_Sync_Insert,\
-        srv_db_Sync_Delete,\
-        srv_db_Sync_Progress_Update
+    from db_base_sync import srv_db_sync_list_count,\
+        srv_db_sync_list,\
+        srv_db_sync_insert,\
+        srv_db_sync_delete,\
+        srv_db_sync_progress_update
     from db_base_triggers import srv_db_trigger_insert,\
         srv_db_triggers_read,\
         srv_db_triggers_delete
@@ -208,23 +208,24 @@ class MK_Server_Database(object):
         srv_db_tuner_by_serial,\
         srv_db_tuner_update
     from db_base_tv_schedule import srv_db_tv_stations_read,\
-        srv_db_tv_stations_read_StationID_List,\
+        srv_db_tv_stations_read_stationid_list,\
         srv_db_tv_station_insert,\
         srv_db_tv_station_exist,\
         srv_db_tv_station_update,\
         srv_db_tv_schedule_insert,\
         srv_db_tv_program_insert,\
         srv_db_tv_schedule_by_date
-    from db_base_usage import srv_db_Usage_Top10_AllTime,\
-        srv_db_Usage_Top10_Movie,\
-        srv_db_Usage_Top10_TV_Show,\
-        srv_db_Usage_Top10_TV_Episode
-    from db_base_users import srv_db_User_List_Name_Count,\
-        srv_db_User_List_Name,\
-        srv_db_User_Detail,\
-        srv_db_User_Delete,\
-        srv_db_User_Login_Kodi
-
+    from db_base_usage import srv_db_usage_top10_alltime,\
+        srv_db_usage_top10_movie,\
+        srv_db_usage_top10_tv_show,\
+        srv_db_usage_top10_tv_episode
+    from db_base_users import srv_db_user_list_name_count,\
+        srv_db_user_list_name,\
+        srv_db_user_detail,\
+        srv_db_user_delete,\
+        srv_db_user_login_kodi
+    
     # class variables
-    sql3_conn = None
-    sql3_cursor = None
+    sql3_conn = none
+    sql3_cursor = none
+    
