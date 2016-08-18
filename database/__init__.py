232052b4454c69928a60ddf5c4bad0bbcb9825ff
SpootDev <spootdev@gmail.com>
2016-08-17 21:02:10 -0500

    path

--- database/__init__.py
+++ database/__init__.py
@@ -2,7 +2,7 @@ class MKServerDatabase(object):
     """
     Main database class for server
     """
-    from db_base import db_open,\
+    from database.db_base import db_open,\
         db_open_isolation,\
         db_close,\
         db_commit,\
@@ -10,9 +10,9 @@ class MKServerDatabase(object):
         db_table_index_check,\
         db_table_count,\
         db_query
-    from db_base_activity import db_activity_insert,\
+    from database.db_base_activity import db_activity_insert,\
         db_activity_purge
-    from db_base_audit import db_audit_path_status,\
+    from database.db_base_audit import db_audit_path_status,\
         db_audit_path_update_status,\
         db_audit_paths_count,\
         db_audit_path_update_by_uuid,\
@@ -22,50 +22,50 @@ class MKServerDatabase(object):
         db_audit_paths,\
         db_audit_path_delete,\
         db_audit_path_by_uuid
-    from db_base_channel import db_channel_insert
-    from db_base_collection import db_media_collection_scan,\
+    from database.db_base_channel import db_channel_insert
+    from database.db_base_collection import db_media_collection_scan,\
         db_collection_guid_by_name,\
         db_collection_insert,\
         db_collection_update,\
         db_collection_list,\
         db_collection_read_by_guid,\
         db_collection_by_tmdb
-    from db_base_cron import db_cron_list_count,\
+    from database.db_base_cron import db_cron_list_count,\
         db_cron_list,\
         db_cron_time_update
-    from db_base_device import db_device_count,\
+    from database.db_base_device import db_device_count,\
         db_device_list,\
         db_device_insert,\
         db_device_update,\
         db_device_delete,\
         db_device_read
-    from db_base_download import db_download_insert,\
+    from database.db_base_download import db_download_insert,\
         db_download_read_provider,\
         db_download_delete,\
         db_download_update_provider,\
         db_download_update
-    from db_base_kodi import db_kodi_user_sync,\
+    from database.db_base_kodi import db_kodi_user_sync,\
         db_kodi_user_sync_added
-    from db_base_iradio import db_iradio_insert,\
+    from database.db_base_iradio import db_iradio_insert,\
         db_iradio_list_count,\
         db_iradio_list
-    from db_base_link import db_link_list_count,\
+    from database.db_base_link import db_link_list_count,\
         db_link_list,\
         db_link_insert,\
         db_link_delete
-    from db_base_media_class import db_media_class_list_count,\
+    from database.db_base_media_class import db_media_class_list_count,\
         db_media_class_list,\
         db_media_class_by_uuid,\
         db_media_uuid_by_class
-    from db_base_media_games import db_media_game_system_list_count,\
+    from database.db_base_media_games import db_media_game_system_list_count,\
         db_media_game_system_list,\
         db_media_game_list_by_system_count,\
         db_media_game_list_by_system,\
         db_media_game_list_count,\
         db_media_game_list
-    from db_base_media_images import db_media_images_list_count,\
+    from database.db_base_media_images import db_media_images_list_count,\
         db_media_images_list
-    from db_base_media import db_insert_media,\
+    from database.db_base_media import db_insert_media,\
         db_read_media,\
         db_known_media_count,\
         db_known_media,\
@@ -91,19 +91,19 @@ class MKServerDatabase(object):
         db_media_favorite_status_update,\
         db_media_poo_status_update,\
         db_media_mismatch_status_update
-    from db_base_media_movie import db_web_media_list_count,\
+    from database.db_base_media_movie import db_web_media_list_count,\
         db_media_movie_count_by_genre,\
         db_web_media_list,\
         db_media_random
-    from db_base_media_music import db_media_album_count,\
+    from database.db_base_media_music import db_media_album_count,\
         db_media_album_list
-    from db_base_media_remote import db_insert_remote_media,\
+    from database.db_base_media_remote import db_insert_remote_media,\
         db_read_remote_media,\
         db_known_remote_media_count,\
         db_media_remote_read_new
-    from db_base_media_tv import db_web_tvmedia_list,\
+    from database.db_base_media_tv import db_web_tvmedia_list,\
         db_web_tvmedia_list_count
-    from db_base_metadata import db_read_media_metadata,\
+    from database.db_base_metadata import db_read_media_metadata,\
         db_meta_genre_list_count,\
         db_meta_genre_list,\
         db_meta_movie_count_genre,\
@@ -118,18 +118,18 @@ class MKServerDatabase(object):
         db_meta_fetch_series_media_id_json,\
         db_find_metadata_guid,\
         db_meta_update_media_id_from_scudlee
-    from db_base_metadata_book import db_metabook_guid_by_isbn,\
+    from database.db_base_metadata_book import db_metabook_guid_by_isbn,\
         db_metabook_guid_by_name,\
         db_metabook_book_insert
-    from db_base_metadata_games import db_meta_game_system_by_guid,\
+    from database.db_base_metadata_games import db_meta_game_system_by_guid,\
         db_meta_game_system_list_count,\
         db_meta_game_system_list,\
         db_meta_game_list_count,\
         db_meta_game_list,\
         db_meta_game_by_guid
-    from db_base_metadata_gamesdb import db_meta_gamesdb_system_insert
-    from db_base_metadata_movie import db_meta_movie_update_castcrew
-    from db_base_metadata_music import db_music_lookup,\
+    from database.db_base_metadata_gamesdb import db_meta_gamesdb_system_insert
+    from database.db_base_metadata_movie import db_meta_movie_update_castcrew
+    from database.db_base_metadata_music import db_music_lookup,\
         db_meta_musician_by_guid,\
         db_meta_musician_add,\
         db_meta_album_by_guid,\
@@ -139,18 +139,18 @@ class MKServerDatabase(object):
         db_meta_songs_by_album_guid,\
         db_meta_album_list,\
         db_meta_muscian_list
-    from db_base_metadata_music_video import db_meta_music_video_lookup,\
+    from database.db_base_metadata_music_video import db_meta_music_video_lookup,\
         db_meta_music_video_add,\
         db_meta_music_video_detail_by_uuid,\
         db_meta_music_video_count,\
         db_meta_music_video_list
-    from db_base_nas import db_nas_count,\
+    from database.db_base_nas import db_nas_count,\
         db_nas_list,\
         db_nas_insert,\
         db_nas_update,\
         db_nas_delete,\
         db_nas_read
-    from db_base_metadata_people import db_meta_person_list_count,\
+    from database.db_base_metadata_people import db_meta_person_list_count,\
         db_meta_person_list,\
         db_meta_person_by_guid,\
         db_meta_person_by_name,\
@@ -158,16 +158,16 @@ class MKServerDatabase(object):
         db_metdata_person_insert,\
         db_meta_person_id_count,\
         db_meta_person_as_seen_in
-    from db_base_metadata_sports import db_meta_sports_guid_by_thesportsdb,\
+    from database.db_base_metadata_sports import db_meta_sports_guid_by_thesportsdb,\
         db_meta_sports_list_count,\
         db_meta_sports_list,\
         db_meta_sports_guid_by_event_name
-    from db_base_metadata_thesportsdb import db_metathesportsdb_insert,\
+    from database.db_base_metadata_thesportsdb import db_metathesportsdb_insert,\
         db_metathesports_update,\
         db_metathesportsdb_select_by_guid
-    from db_base_metadata_thetvdb import db_metatvdb_insert,\
+    from database.db_base_metadata_thetvdb import db_metatvdb_insert,\
         db_metatvdb_update
-    from db_base_metadata_tv import db_metatv_guid_by_imdb,\
+    from database.db_base_metadata_tv import db_metatv_guid_by_imdb,\
         db_metatv_guid_by_tvdb,\
         db_metatv_guid_by_tvmaze,\
         db_meta_tvshow_list_count,\
@@ -180,37 +180,37 @@ class MKServerDatabase(object):
         db_read_tvmetadata_eps_season,\
         db_read_tvmetadata_season_eps_list,\
         db_read_tvmetadata_episode
-    from db_base_metadata_tvmaze import db_meta_tvmaze_changed_uuid,\
+    from database.db_base_metadata_tvmaze import db_meta_tvmaze_changed_uuid,\
         db_meta_tvmaze_insert,\
         db_meta_tvmaze_update
-    from db_base_notification import db_notification_insert,\
+    from database.db_base_notification import db_notification_insert,\
         db_notification_read
-    from db_base_option_status import db_option_status_read,\
+    from database.db_base_option_status import db_option_status_read,\
         db_option_status_update,\
         db_option_status_update_scan_json,\
         db_option_status_update_scan_json_rec
-    from db_base_postgresql import db_pgsql_table_sizes,\
+    from database.db_base_postgresql import db_pgsql_table_sizes,\
         db_pgsql_row_count,\
         db_pgsql_vacuum_stat_by_day,\
         db_pgsql_vacuum_table
-    from db_base_review import db_review_count,\
+    from database.db_base_review import db_review_count,\
         db_review_list_by_tmdb_guid,\
         db_review_insert
-    from db_base_sync import db_sync_list_count,\
+    from database.db_base_sync import db_sync_list_count,\
         db_sync_list,\
         db_sync_insert,\
         db_sync_delete,\
         db_sync_progress_update
-    from db_base_triggers import db_trigger_insert,\
+    from database.db_base_triggers import db_trigger_insert,\
         db_triggers_read,\
         db_triggers_delete
-    from db_base_tuner import db_tuner_count,\
+    from database.db_base_tuner import db_tuner_count,\
         db_tuner_insert,\
         db_tuner_delete,\
         db_tuner_list,\
         db_tuner_by_serial,\
         db_tuner_update
-    from db_base_tv_schedule import db_tv_stations_read,\
+    from database.db_base_tv_schedule import db_tv_stations_read,\
         db_tv_stations_read_stationid_list,\
         db_tv_station_insert,\
         db_tv_station_exist,\
@@ -218,11 +218,11 @@ class MKServerDatabase(object):
         db_tv_schedule_insert,\
         db_tv_program_insert,\
         db_tv_schedule_by_date
-    from db_base_usage import db_usage_top10_alltime,\
+    from database.db_base_usage import db_usage_top10_alltime,\
         db_usage_top10_movie,\
         db_usage_top10_tv_show,\
         db_usage_top10_tv_episode
-    from db_base_users import db_user_list_name_count,\
+    from database.db_base_users import db_user_list_name_count,\
         db_user_list_name,\
         db_user_detail,\
         db_user_delete,\
