bae90f09f57c80052efd7721c92abb95368e62e3
SpootDev <spootdev@gmail.com>
2016-08-23 14:00:29 -0500

    cast null

--- database/db_base_media_movie.py
+++ database/db_base_media_movie.py
@@ -246,7 +246,7 @@ def db_web_media_list(self, class_guid, list_type=None, list_genre='All',\
                         ' and mm_media_metadata_guid = mm_metadata_guid'\
                         ' and (mm_metadata_json->>\'belongs_to_collection\') is null'\
                         ' union select mm_metadata_collection_name as name,'\
-                        ' mm_metadata_collection_guid as guid, null as metajson'\
+                        ' mm_metadata_collection_guid as guid, null::jsonb as metajson'\
                         ' from mm_metadata_collection'\
                         ' order by mm_media_metadata_guid, name) as temp'\
                         ' order by LOWER(mm_media_name) offset %s limit %s',\
