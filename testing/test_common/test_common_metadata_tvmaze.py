009f611f367afea31277673bc9f61ddff85324f5
SpootDev <spootdev@gmail.com>
2016-08-23 15:35:46 -0500

    name fix

--- testing/test_common/test_common_metadata_tvmaze.py
+++ testing/test_common/test_common_metadata_tvmaze.py
@@ -42,28 +42,28 @@ class TestCommonMetadatatvmaze(object):
 
 
     # show when last updated
-    def test_com_meta_themaze_show_updated(self):
+    def test_com_meta_tvmaze_show_updated(self):
         """
         Test function
         """
-        self.db_connection.com_meta_themaze_show_updated()
+        self.db_connection.com_meta_tvmaze_show_updated()
 
 
 # lookup show
-# def com_meta_TheMaze_WideSearch(self, show_name, show_year=None):
+# def com_meta_TvMaze_WideSearch(self, show_name, show_year=None):
 
 
 # lookup specific show
-# def com_meta_TheMaze_NarrowSearch(self, show_name, show_year=None):
+# def com_meta_TvMaze_NarrowSearch(self, show_name, show_year=None):
 
 
 # lookup specific id
-# def com_meta_TheMaze_Show_by_ID(self, tvmaze_id, tvrage_id, imdb_id, tvdb_id, embed_info=True):
+# def com_meta_TvMaze_Show_by_ID(self, tvmaze_id, tvrage_id, imdb_id, tvdb_id, embed_info=True):
 
 
 # people search (doesnt' appear to have episode data here)
-# def com_meta_TheMaze_Person_by_Name(self, person_name):
+# def com_meta_TvMaze_Person_by_Name(self, person_name):
 
 
 # schedule
-# def com_meta_TheMaze_Schedule(self, country_code=None, schedule_date=None):
+# def com_meta_TvMaze_Schedule(self, country_code=None, schedule_date=None):
