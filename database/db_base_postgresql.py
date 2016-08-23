8334d2155ac79f6933dee9e51a617bd47101c442
SpootDev <spootdev@gmail.com>
2016-08-23 11:13:21 -0500

    fix index

--- database/db_base_postgresql.py
+++ database/db_base_postgresql.py
@@ -87,4 +87,4 @@ def db_pgsql_table_exits(self, table_name):
     Check to see if table exits. Will return NULL if not.
     """
     self.db_cursor.execute('SELECT to_regclass(%s)', (table_name,))
-    return self.db_cursor.fetchone()
+    return self.db_cursor.fetchone()[0]
