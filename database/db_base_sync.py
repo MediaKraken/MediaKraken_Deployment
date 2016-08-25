5239689f55637bf1c571cca0e6edb579a56d1e0e
SpootDev <spootdev@gmail.com>
2016-08-25 14:05:26 -0500

    cast

--- database/db_base_sync.py
+++ database/db_base_sync.py
@@ -49,15 +49,16 @@ def db_sync_list(self, offset=None, records=None, user_guid=None):
         if offset is None:
             self.db_cursor.execute('select mm_sync_guid uuid, mm_sync_path, mm_sync_path_to,'\
                 ' mm_sync_options_json from mm_sync'\
-                ' where mm_sync_options_json->\'User\'::text::numeric = %s'\
-                ' order by mm_sync_options_json->\'Priority\' desc, mm_sync_path', (user_guid,))
+                ' where mm_sync_options_json->\'User\'::text = %s'\
+                ' order by mm_sync_options_json->\'Priority\' desc, mm_sync_path',\
+                (str(user_guid),))
         else:
             self.db_cursor.execute('select mm_sync_guid uuid, mm_sync_path, mm_sync_path_to,'\
                 ' mm_sync_options_json from mm_sync where mm_sync_guid in (select mm_sync_guid'\
-                ' from mm_sync where mm_sync_options_json->\'User\'::text::numeric = %s'\
+                ' from mm_sync where mm_sync_options_json->\'User\'::text = %s'\
                 ' order by mm_sync_options_json->\'Priority\' desc, mm_sync_path'\
                 ' offset %s limit %s) order by mm_sync_options_json->\'Priority\''\
-                ' desc, mm_sync_path', (user_guid, offset, records))
+                ' desc, mm_sync_path', (str(user_guid), offset, records))
     return self.db_cursor.fetchall()
 
 
