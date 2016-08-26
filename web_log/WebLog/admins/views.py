3a6b73ca46b441a7a5de6d6743c4301ab0fcd29a
SpootDev <spootdev@gmail.com>
2016-08-25 21:02:06 -0500

    extra )

--- web_log/WebLog/admins/views.py
+++ web_log/WebLog/admins/views.py
@@ -253,7 +253,7 @@ def admin_log_mediakraken():
     Display server settings page
     """
     log_list = common_file.com_file_dir_list('../log_debug', 'log', False, False, True, False)
-    return render_template("admin/admin_server_log_list.html", data_list=log_list))
+    return render_template("admin/admin_server_log_list.html", data_list=log_list)
 
 
 @blueprint.before_request
