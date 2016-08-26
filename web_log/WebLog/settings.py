96f9da5cafc4cd28e9165d4b4d3beceb0e2d5834
SpootDev <spootdev@gmail.com>
2016-08-25 21:00:50 -0500

    sqlite db

--- web_log/WebLog/settings.py
+++ web_log/WebLog/settings.py
@@ -5,10 +5,6 @@ os_env = os.environ
 import sys
 sys.path.append('..')
 from common import common_file
-# pull in the ini file config
-import ConfigParser
-ConfigFile = ConfigParser.ConfigParser()
-ConfigFile.read("../WebLog.ini")
 
 
 class Config(object):
@@ -33,11 +29,7 @@ class ProdConfig(Config):
     """Production configuration."""
     ENV = 'prod'
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://'\
-        + ConfigFile.get('DB Connections','PostDBUser').strip() + ':'\
-        + ConfigFile.get('DB Connections','PostDBPass').strip() + '@'\
-        + ConfigFile.get('DB Connections','PostDBHost').strip() + '/'\
-        + ConfigFile.get('DB Connections','PostDBName').strip()
+    SQLALCHEMY_DATABASE_URI = "sqlite:///user.db"
     DEBUG_TB_ENABLED = False # Disable Debug toolbar
 
 
@@ -46,11 +38,7 @@ class DevConfig(Config):
     """Development configuration."""
     ENV = 'dev'
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://'\
-        + ConfigFile.get('DB Connections','PostDBUser').strip() + ':'\
-        + ConfigFile.get('DB Connections','PostDBPass').strip() + '@'\
-        + ConfigFile.get('DB Connections','PostDBHost').strip() + '/'\
-        + ConfigFile.get('DB Connections','PostDBName').strip()
+    SQLALCHEMY_DATABASE_URI = "sqlite:///user.db"
     DEBUG_TB_ENABLED = True
     ASSETS_DEBUG = True # Don't bundle/minify static assets
     CACHE_TYPE = 'simple' # Can be "memcached", "redis", etc.
