9f60e0cc08aa521e0a24ee123805e9de33f5acf4
SpootDev <spootdev@gmail.com>
2016-08-27 17:49:15 -0500

    fix join

--- subprogram_postgresql_backup.py
+++ subprogram_postgresql_backup.py
@@ -50,7 +50,7 @@ cloud_handle = common_cloud.CommonCloud(option_config_json)
 option_json = db_connection.db_opt_status_read()['mm_options_json']
 if option_json['Backup']['BackupType'] != 'local':
     cloud_handle.com_cloud_file_store(option_json['Backup']['BackupType'],\
-    os.join(option_config_json['MediaKrakenServer']['BackupLocal'],\
+    os.path.join(option_config_json['MediaKrakenServer']['BackupLocal'],\
     backup_file_name), backup_file_name, True)
 
 # log end
