d540cdf2d9c0662241efc9ca759b494e9e9c4c03
SpootDev <spootdev@gmail.com>
2016-08-30 21:28:47 -0500

    remove other junk

--- common/common_network_vm_proxmox.py
+++ common/common_network_vm_proxmox.py
@@ -40,23 +40,101 @@ class CommonNetworkProxMox(object):
         Do api call to specified connection
         """
         if request_type == "get":
-            api_response = requests.get(self.full_url + api_call_type, verify=False,
-                                         cookies=self.prox_ticket)
+            return requests.get(self.full_url + api_call_type, verify=False,
+                                cookies=self.prox_ticket).json()
         else:
-            api_response = requests.post(self.full_url + api_call_type, verify=False,
-                                          data=post_data,
-                                          cookies=self.prox_ticket,
-                                          headers=self.httpheaders)
-        try:
-            returned_data = api_response.json()
-            return returned_data
-        except: # pylint: disable=W0702
-            print("Error in trying to process JSON")
-            print(api_response)
+            return requests.post(self.full_url + api_call_type, verify=False,
+                                 data=post_data,
+                                 cookies=self.prox_ticket,
+                                 headers=self.httpheaders).json()
 
-
+    """
+    Cluster API
+    """
     def com_net_prox_api_version(self):
         """
         # grab version of api from node
         """
         return self.com_net_prox_api_call('get', 'version')
+
+
+    def com_net_prox_cluster_ndx(self):
+        """
+        Get the index of the cluster
+        """
+        return self.com_net_prox_api_call('get', 'cluster')
+
+
+    """
+    LXC api calls
+    """
+    def com_net_prox_node_lxc_list(self, node_name):
+        """
+        Get a list of the lxc vms on node
+        """
+        return self.com_net_prox_api_call('get', 'nodes/%s/lxc' % node_name)
+
+
+    def com_net_prox_node_lxc_status(self, node_name, vm_id):
+        """
+        Get a status of the lxc vm
+        """
+        return self.com_net_prox_api_call('get', 'nodes/%s/lxc/%s/status/current'\
+            % (node_name, vm_id))
+
+
+    def com_net_prox_node_lxc_start(self, node_name, vm_id):
+        """
+        Start lxc vm
+        """
+        return self.com_net_prox_api_call('get', 'nodes/%s/lxc/%s/status/start'\
+            % (node_name, vm_id))
+
+
+    def com_net_prox_node_lxc_stop(self, node_name, vm_id):
+        """
+        Stop lxc vm
+        """
+        return self.com_net_prox_api_call('get', 'nodes/%s/lxc/%s/status/stop' % (node_name, vm_id))
+
+
+    """
+    QEMU api calls
+    """
+    def com_net_prox_node_qemu_list(self, node_name):
+        """
+        Get a list of the qemu vms on node
+        """
+        return self.com_net_prox_api_call('get', 'nodes/%s/qemu' % node_name)
+
+
+    def com_net_prox_node_qemu_status(self, node_name, vm_id):
+        """
+        Get a status of the qemu vm
+        """
+        return self.com_net_prox_api_call('get', 'nodes/%s/qemu/%s/status/current'\
+            % (node_name, vm_id))
+
+
+    def com_net_prox_node_qemu_start(self, node_name, vm_id):
+        """
+        Start qemu vm
+        """
+        return self.com_net_prox_api_call('get', 'nodes/%s/qemu/%s/status/start'\
+            % (node_name, vm_id))
+
+
+    def com_net_prox_node_qemu_stop(self, node_name, vm_id):
+        """
+        Stop qemu vm
+        """
+        return self.com_net_prox_api_call('get', 'nodes/%s/qemu/%s/status/stop'\
+            % (node_name, vm_id))
+
+
+testn = CommonNetworkProxMox('10.0.0.190', 'root@pam', 'jenkinsbuild')
+print(testn.com_net_prox_api_version())
+print(testn.com_net_prox_cluster_ndx())
+print(testn.com_net_prox_node_lxc_list('pve'))
+print(testn.com_net_prox_node_qemu_list('pve'))
+print(testn.com_net_prox_node_qemu_status('pve', 104))
