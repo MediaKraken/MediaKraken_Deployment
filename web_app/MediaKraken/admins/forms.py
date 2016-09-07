09024c93ec3506ebdde321980d5a268ac273c258
SpootDev <spootdev@gmail.com>
2016-09-06 22:21:50 -0500

    remove os

--- web_app/MediaKraken/admins/forms.py
+++ web_app/MediaKraken/admins/forms.py
@@ -4,15 +4,16 @@ from flask_wtf import Form
 from wtforms import TextField, PasswordField, TextAreaField, BooleanField, SelectField, DecimalField
 from wtforms.validators import DataRequired, Email, EqualTo, Length
 from decimal import ROUND_UP
-import os
 
 # for editing the library
 class LibraryAddEditForm(Form):
     #description = TextAreaField('Description', validators=[DataRequired()])
     library_path = TextField('Library Path') # , validators=[DataRequired(), Length(min=1, max=255)])  # remove required due to browse buttons
 
+
     def __init__(self, *args, **kwargs):
         super(LibraryAddEditForm, self).__init__(*args, **kwargs)
+
 
     def validate(self):
         initial_validation = super(LibraryAddEditForm, self).validate()
@@ -24,11 +25,14 @@ class LibraryAddEditForm(Form):
 # for editing backups
 class BackupEditForm(Form):
     enabled = BooleanField('Enabled')
-    #interval = SelectField('Interval', choices=[('Hours', 'Hours'), ('Days', 'Days'), ('Weekly', 'Weekly')])
+    #interval = SelectField('Interval', choices=[('Hours', 'Hours'),\
+    #('Days', 'Days'), ('Weekly', 'Weekly')])
     #time = DecimalField('Time', places = 2, rounding=ROUND_UP)
+
 
     def __init__(self, *args, **kwargs):
         super(BackupEditForm, self).__init__(*args, **kwargs)
+
 
     def validate(self):
         initial_validation = super(BackupEditForm, self).validate()
@@ -41,8 +45,10 @@ class BackupEditForm(Form):
 class DLNAEditForm(Form):
     enabled = BooleanField('Enabled')
 
+
     def __init__(self, *args, **kwargs):
         super(DLNAEditForm, self).__init__(*args, **kwargs)
+
 
     def validate(self):
         initial_validation = super(DLNAEditForm, self).validate()
@@ -64,8 +70,10 @@ class UserEditForm(Form):
     enabled = BooleanField('Enabled')
     is_admin = BooleanField('Admin')
 
+
     def __init__(self, *args, **kwargs):
         super(UserEditForm, self).__init__(*args, **kwargs)
+
 
     def validate(self):
         initial_validation = super(UserEditForm, self).validate()
@@ -85,13 +93,16 @@ class AdminSettingsForm(Form):
         choices=[('Never', 'Never'), ('3', '3'), ('5', '5'), ('10', '10')])
     metadata_download_metadata = BooleanField('Download Metadata')
     metadata_artwork_with_media = BooleanField('Download Artwork')
-    #language = SelectField('Interval', choices=[('Hours', 'Hours'), ('Days', 'Days'), ('Weekly', 'Weekly')])
-    #country = SelectField('Interval', choices=[('Hours', 'Hours'), ('Days', 'Days'), ('Weekly', 'Weekly')])
+    #language = SelectField('Interval', choices=[('Hours', 'Hours'),\
+    #('Days', 'Days'), ('Weekly', 'Weekly')])
+    #country = SelectField('Interval', choices=[('Hours', 'Hours'),\
+    #('Days', 'Days'), ('Weekly', 'Weekly')])
     metadata_image_bio_person = BooleanField('Download Image/BIO of person(s)')
     metadata_path = TextField('Metadata Path', validators=[DataRequired(), Length(min=1, max=250)])
     metadata_sub_movie_down = BooleanField('Download Movie Subtitle')
     metadata_sub_episode_down = BooleanField('Download TV Subtitle')
-    #meta_language = SelectField('Interval', choices=[('Hours', 'Hours'), ('Days', 'Days'), ('Weekly', 'Weekly')])
+    #meta_language = SelectField('Interval', choices=[('Hours', 'Hours'),\
+    #('Days', 'Days'), ('Weekly', 'Weekly')])
     metadata_sub_skip_if_audio = BooleanField('Skip subtitle if lang in audio track')
     metadata_source_down_tvmaze = BooleanField('tvmaze')
     metadata_source_down_tmdb = BooleanField('TMDB')
@@ -112,6 +123,7 @@ class AdminSettingsForm(Form):
     def __init__(self, *args, **kwargs):
         super(AdminSettingsForm, self).__init__(*args, **kwargs)
 
+
     def validate(self):
         initial_validation = super(AdminSettingsForm, self).validate()
         if not initial_validation:
@@ -129,8 +141,10 @@ class CronEditForm(Form):
     time = DecimalField('Time', places = 2, rounding=ROUND_UP)
     script_path = TextField('Script Path', validators=[DataRequired(), Length(min=1, max=255)])
 
+
     def __init__(self, *args, **kwargs):
         super(CronEditForm, self).__init__(*args, **kwargs)
+
 
     def validate(self):
         initial_validation = super(CronEditForm, self).validate()
