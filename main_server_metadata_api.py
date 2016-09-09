b73d67b52f0a03fe625e6371ed53d2a3cc1af2c6
SpootDev <spootdev@gmail.com>
2016-09-09 13:38:26 -0500

    rt purge

--- main_server_metadata_api.py
+++ main_server_metadata_api.py
@@ -72,7 +72,7 @@ def signal_receive(signum, frame): # pylint: disable=W0613
     db_connection.db_rollback()
     # log stop
     db_connection.db_activity_insert('MediaKraken_Metadata API Stop', None,\
-        'System: Metadata API Stop', 'ServerMetadataAPIStop', None, None, 'System')
+        'System: Metadata API Stop', 'ServerMetadataAPIStop',\ None, None, 'System')
     db_connection.db_close()
     sys.stdout.flush()
     sys.exit(0)
@@ -231,21 +231,6 @@ def pitchfork(thread_db, download_data):
         pass
     elif download_data['mdq_download_json']['Status'] == "Fetch":
         pass
-
-
-# they went pay only
-#@ratelimited(common_metadata_limiter.API_LIMIT['rotten_tomatoes'][0]\
-#     / common_metadata_limiter.API_LIMIT['rotten_tomatoes'][1])
-#def rotten_tomatoes(thread_db, download_data):
-#    """
-#    Rate limiter for Rotten Tomatoes
-#    """
-#    logging.debug("here i am in rotten_tomatoes rate %s",\
-#        datetime.datetime.now().strftime("%H:%M:%S.%f"))
-#    if download_data['mdq_download_json']['Status'] == "Search":
-#        pass
-#    elif download_data['mdq_download_json']['Status'] == "Fetch":
-#        pass
 
 
 @ratelimited(common_metadata_limiter.API_LIMIT['televisiontunes'][0]\
@@ -426,62 +411,63 @@ def worker(content_providers):
     logging.debug("name: %s", content_providers)
     # open the database
     config_handle, option_config_json, thread_db = common_config_ini.com_config_read()
-#    while True:
-    for row_data in thread_db.db_download_read_provider(content_providers):
-        logging.debug("row: %s", row_data)
-        # mdq_id,mdq_download_json
-        if content_providers == 'anidb':
-            anidb(thread_db, row_data)
-        elif content_providers == 'chart_lyrics':
-            chart_lyrics(thread_db, row_data)
-        elif content_providers == 'comicvine':
-            comicvine(thread_db, row_data)
-        elif content_providers == 'giantbomb':
-            giantbomb(thread_db, row_data)
-        elif content_providers == 'imdb':
-            imdb(thread_db, row_data)
-        elif content_providers == 'imvdb':
-            imvdb(thread_db, row_data)
-        elif content_providers == 'netflixroulette':
-            netflixroulette(thread_db, row_data)
-        elif content_providers == 'omdb':
-            omdb(thread_db, row_data)
-        elif content_providers == 'pitchfork':
-            pitchfork(thread_db, row_data)
-        elif content_providers == 'televisiontunes':
-            televisiontunes(thread_db, row_data)
-        elif content_providers == 'theaudiodb':
-            theaudiodb(thread_db, row_data)
-        elif content_providers == 'thegamesdb':
-            thegamesdb(thread_db, row_data)
-        elif content_providers == 'thelogodb':
-            thelogodb(thread_db, row_data)
-        elif content_providers == 'themoviedb':
-            themoviedb(thread_db, row_data)
-        elif content_providers == 'thesportsdb':
-            thesportsdb(thread_db, row_data)
-        elif content_providers == 'thetvdb':
-            thetvdb(thread_db, row_data)
-        elif content_providers == 'tv_intros':
-            tv_intros(thread_db, row_data)
-        elif content_providers == 'tvmaze':
-            tvmaze(thread_db, row_data)
-        elif content_providers == 'tvshowtime':
-            tvshowtime(thread_db, row_data)
-        elif content_providers == 'Z':
-            metadata_uuid = metadata_identification.metadata_identification(thread_db,\
-                class_text_dict[row_data['mdq_download_json']['ClassID']],\
-                row_data['mdq_download_json']['Path'], row_data['mdq_download_json'],\
-                row_data['mdq_id'])
-            # update the media row with the json media id AND THE proper NAME!!!
-            if metadata_uuid is not None:
-                logging.debug("update: %s %s",\
-                    row_data['mdq_download_json']['MediaID'], metadata_uuid)
-                thread_db.db_update_media_id(row_data['mdq_download_json']['MediaID'],\
-                    metadata_uuid)
-                thread_db.db_download_delete(row_data['mdq_id'])
-    time.sleep(1)
-    thread_db.db_commit()
+    while True:
+        for row_data in thread_db.db_download_read_provider(content_providers):
+            logging.debug("row: %s", row_data)
+            # mdq_id,mdq_download_json
+            if content_providers == 'anidb':
+                anidb(thread_db, row_data)
+            elif content_providers == 'chart_lyrics':
+                chart_lyrics(thread_db, row_data)
+            elif content_providers == 'comicvine':
+                comicvine(thread_db, row_data)
+            elif content_providers == 'giantbomb':
+                giantbomb(thread_db, row_data)
+            elif content_providers == 'imdb':
+                imdb(thread_db, row_data)
+            elif content_providers == 'imvdb':
+                imvdb(thread_db, row_data)
+            elif content_providers == 'netflixroulette':
+                netflixroulette(thread_db, row_data)
+            elif content_providers == 'omdb':
+                omdb(thread_db, row_data)
+            elif content_providers == 'pitchfork':
+                pitchfork(thread_db, row_data)
+            elif content_providers == 'televisiontunes':
+                televisiontunes(thread_db, row_data)
+            elif content_providers == 'theaudiodb':
+                theaudiodb(thread_db, row_data)
+            elif content_providers == 'thegamesdb':
+                thegamesdb(thread_db, row_data)
+            elif content_providers == 'thelogodb':
+                thelogodb(thread_db, row_data)
+            elif content_providers == 'themoviedb':
+                themoviedb(thread_db, row_data)
+            elif content_providers == 'thesportsdb':
+                thesportsdb(thread_db, row_data)
+            elif content_providers == 'thetvdb':
+                thetvdb(thread_db, row_data)
+            elif content_providers == 'tv_intros':
+                tv_intros(thread_db, row_data)
+            elif content_providers == 'tvmaze':
+                tvmaze(thread_db, row_data)
+            elif content_providers == 'tvshowtime':
+                tvshowtime(thread_db, row_data)
+            elif content_providers == 'Z':
+                metadata_uuid = metadata_identification.metadata_identification(thread_db,\
+                    class_text_dict[row_data['mdq_download_json']['ClassID']],\
+                    row_data['mdq_download_json']['Path'], row_data['mdq_download_json'],\
+                    row_data['mdq_id'])
+                # update the media row with the json media id AND THE proper NAME!!!
+                if metadata_uuid is not None:
+                    logging.debug("update: %s %s",\
+                        row_data['mdq_download_json']['MediaID'], metadata_uuid)
+                    thread_db.db_update_media_id(row_data['mdq_download_json']['MediaID'],\
+                        metadata_uuid)
+                    thread_db.db_download_delete(row_data['mdq_id'])
+        thread_db.db_commit()
+        time.sleep(1)
+        break # TODO for now testing.......
     thread_db.db_close()
     return
 
