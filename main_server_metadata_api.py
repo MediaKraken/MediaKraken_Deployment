9de222eda694f63b671ce049e3a7477806f9b9d5
SpootDev <spootdev@gmail.com>
2016-09-10 14:26:03 -0500

    fix imdb check

--- main_server_metadata_api.py
+++ main_server_metadata_api.py
@@ -315,7 +315,7 @@ def themoviedb(thread_db, download_data):
                 thread_db.db_Download_Delete(download_data['mdq_id'])
     elif download_data['mdq_download_json']['Status'] == "Fetch":
         logging.debug('themoviedb fetch %s', download_data['mdq_download_json']['ProviderMetaID'])
-        if download_data['mdq_download_json']['ProviderMetaID'][0:2] != 'tt': # imdb id check
+        if download_data['mdq_download_json']['ProviderMetaID'][0:2] == 'tt': # imdb id check
             tmdb_id = metadata_movie.movie_fetch_tmdb_imdb(\
                 download_data['mdq_download_json']['ProviderMetaID'])
             if tmdb_id is not None:
