bc40f6efd25deb375c0897e439331837582a24fc
SpootDev <spootdev@gmail.com>
2016-09-19 23:03:38 -0500

    limit 1

--- database/db_base_download.py
+++ database/db_base_download.py
@@ -82,4 +82,4 @@ def db_download_que_exists(self, provider_name, provider_id):
     logging.debug('que exits: %s %s', provider_name, provider_id)
     return self.db_cursor.execute('select mdq_download_json->\'MetaNewID\' from mm_download_que'\
         ' where mdq_provider = %s and mdq_download_json->\'ProviderMetaID\' ? %s'\
-        ' and mdq_download_json->>\'Status\' <> \'Search\'', (provider_name, provider_id))
+        ' and mdq_download_json->>\'Status\' <> \'Search\' limit 1', (provider_name, provider_id))
