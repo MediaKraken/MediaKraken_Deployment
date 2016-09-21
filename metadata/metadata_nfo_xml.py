190bdacb2e312217b7717825074d52d689471fa1
SpootDev <spootdev@gmail.com>
2016-09-21 15:42:21 -0500

    let the meta movie set this

--- metadata/metadata_nfo_xml.py
+++ metadata/metadata_nfo_xml.py
@@ -83,22 +83,22 @@ def nfo_xml_db_lookup(db_connection, nfo_data, xml_data, download_que_json, down
     if tmdb_id is not None:
         # see if in local database by id
         metadata_uuid = db_connection.db_meta_guid_by_tmdb(tmdb_id)
-        if metadata_uuid is None:
-            # not in local database, set for fetch by id
-            download_que_json.update({'Status': 'Fetch', 'ProviderMetaID': tmdb_id})
-            db_connection.db_download_update(json.dumps(download_que_json), download_que_id)
-            db_connection.db_download_update_provider('themoviedb', download_que_id)
-            metadata_uuid = download_que_json['MetaNewID']
+#        if metadata_uuid is None:
+#            # not in local database, set for fetch by id
+#            download_que_json.update({'Status': 'Fetch', 'ProviderMetaID': tmdb_id})
+#            db_connection.db_download_update(json.dumps(download_que_json), download_que_id)
+#            db_connection.db_download_update_provider('themoviedb', download_que_id)
+#            metadata_uuid = download_que_json['MetaNewID']
     if metadata_uuid is None and imdb_id is not None:
         # see if in local database by id
         metadata_uuid = db_connection.db_meta_guid_by_imdb(imdb_id)
-        if metadata_uuid is None:
-            # not in local database, set for fetch by id
-            download_que_json.update({'Status': 'Fetch', 'ProviderMetaID': imdb_id})
-            db_connection.db_download_update(json.dumps(download_que_json), download_que_id)
-            # yes, grab data from themoviedb for imdbid
-            db_connection.db_download_update_provider('themoviedb', download_que_id)
-            metadata_uuid = download_que_json['MetaNewID']
+#        if metadata_uuid is None:
+#            # not in local database, set for fetch by id
+#            download_que_json.update({'Status': 'Fetch', 'ProviderMetaID': imdb_id})
+#            db_connection.db_download_update(json.dumps(download_que_json), download_que_id)
+#            # yes, grab data from themoviedb for imdbid
+#            db_connection.db_download_update_provider('themoviedb', download_que_id)
+#            metadata_uuid = download_que_json['MetaNewID']
 #    if metadata_uuid is None and rt_id is not None:
 #        metadata_uuid = db_connection.db_meta_guid_by_rt(rt_id)
 #        if metadata_uuid is None:
