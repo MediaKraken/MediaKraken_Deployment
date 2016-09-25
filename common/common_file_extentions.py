16804957d071f6352cd8acc9cc1913dc355120b3
SpootDev <spootdev@gmail.com>
2016-09-24 22:28:48 -0500

    new

---
+++ common/common_file_extentions.py
@@ -0,0 +1,76 @@
+'''
+  Copyright (C) 2015 Quinn D Granfor <spootdev@gmail.com>
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  version 2, as published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  General Public License version 2 for more details.
+
+  You should have received a copy of the GNU General Public License
+  version 2 along with this program; if not, write to the Free
+  Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+  MA 02110-1301, USA.
+'''
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+
+MEDIA_EXTENSION = [
+    'webm',
+    'mkv',
+    'flv',
+    'vob',
+    'ogv',
+    'ogg',
+    'drc',
+    'mng',
+    'avi',
+    'mov',
+    'qt',
+    'wmv',
+    'wma',
+    'yuv',
+    'rm',
+    'rmvb',
+    'asf',
+    'mp4',
+    'm4p',
+    'm4v',
+    'mpg',
+    'mp2',
+    'mpeg',
+    'mpe',
+    'mp3',
+    'flac',
+    'mpv',
+    'm2v',
+    'm4v',
+    'nsv',
+    'iso',
+    'chd',
+    'zip',
+    '7z',
+    'pdf',
+]
+
+
+SUBTITLE_EXTENSION = [
+    'srt',
+    'smi',
+    'ssa',
+    'ass',
+    'vtt',
+]
+
+
+MEDIA_EXTENSION_SKIP_FFMPEG = [
+    'pdf',
+    'zip',
+    '7z',
+    'iso',
+    'chd',
+]
