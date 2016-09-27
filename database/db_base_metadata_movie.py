579d1c3fa26366e591d0767a55dcd317ffe27633
SpootDev <spootdev@gmail.com>
2016-09-27 17:53:54 -0500

    don't dump

--- database/db_base_metadata_movie.py
+++ database/db_base_metadata_movie.py
@@ -28,15 +28,15 @@ def db_meta_movie_update_castcrew(self, cast_crew_json, metadata_id):
     logging.debug('upt castcrew: %s', metadata_id)
     self.db_cursor.execute('select mm_metadata_json from mm_metadata_movie'\
         ' where mm_metadata_guid = %s', (metadata_id,))
-
     cast_crew_json_row = self.db_cursor.fetchone()
     logging.debug('row: %s', cast_crew_json_row)
     if 'cast' in cast_crew_json:
-        cast_crew_json_row['mm_metadata_json']['Meta']['TMDB'].update({'Cast': cast_crew_json['cast']})
+        cast_crew_json_row['mm_metadata_json']['Meta']['TMDB'].update(\
+            {'Cast': cast_crew_json['cast']})
     if 'crew' in cast_crew_json:
-        cast_crew_json_row['mm_metadata_json']['Meta']['TMDB'].update({'Crew': cast_crew_json['crew']})
-
+        cast_crew_json_row['mm_metadata_json']['Meta']['TMDB'].update(\
+            {'Crew': cast_crew_json['crew']})
     logging.debug('upt: %s', cast_crew_json_row)
     self.db_cursor.execute('update mm_metadata_movie set mm_metadata_json = %s'\
-        ' where mm_metadata_guid = %s', (json.dumps(cast_crew_json_row), metadata_id))
+        ' where mm_metadata_guid = %s', (cast_crew_json_row, metadata_id))
     self.db_commit()
