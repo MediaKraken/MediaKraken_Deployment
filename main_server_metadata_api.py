1a3fadbf90a89dc07f5bd48fc230024d72ebe07c
SpootDev <spootdev@gmail.com>
2016-09-27 19:07:57 -0500

    set uuid

--- main_server_metadata_api.py
+++ main_server_metadata_api.py
@@ -312,7 +312,6 @@ def themoviedb(thread_db, download_data):
                 else:
                     thread_db.db_update_media_id(download_data['mdq_download_json']['MediaID'],\
                                                  metadata_uuid)
-                    # TODO put uuid on media record here?  add to match plan docs
                     download_data['mdq_download_json'].update({'ProviderMetaID': str(match_result)})
                     download_data['mdq_download_json'].update({'Status': 'Fetch'})
                     thread_db.db_download_update(json.dumps(download_data['mdq_download_json']),\
