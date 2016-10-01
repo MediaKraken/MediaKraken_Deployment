df076ad0ca63c92348087c0778dc637bd072e76f
SpootDev <spootdev@gmail.com>
2016-09-30 21:03:21 -0500

    change to 7 days

--- common/common_metadata_scudlee.py
+++ common/common_metadata_scudlee.py
@@ -32,13 +32,13 @@ def mk_scudlee_fetch_xml():
     # grab from github via direct raw link
     if not os.path.isfile('./cache/anime-list.xml')\
             or common_file.com_file_modification_timestamp('./cache/anime-list.xml')\
-            < (time.time() - (30 * 86400)):
+            < (time.time() - (7 * 86400)):
         common_network.mk_network_fetch_from_url(\
             'https://github.com/ScudLee/anime-lists/raw/master/anime-list.xml',\
             './cache/anime-list.xml')
     if not os.path.isfile('./cache/anime-movieset-list.xml')\
             or common_file.com_file_modification_timestamp('./cache/anime-movieset-list.xml')\
-            < (time.time() - (30 * 86400)):
+            < (time.time() - (7 * 86400)):
         common_network.mk_network_fetch_from_url(\
             'https://github.com/ScudLee/anime-lists/raw/master/anime-movieset-list.xml',\
             './cache/anime-movieset-list.xml')
