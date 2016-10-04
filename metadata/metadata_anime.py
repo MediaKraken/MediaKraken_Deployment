6ebf5907cea08f4177105160da3dd188b08fa442
SpootDev <spootdev@gmail.com>
2016-10-04 18:32:45 -0500

    rename

--- metadata/metadata_anime.py
+++ metadata/metadata_anime.py
@@ -18,7 +18,7 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 import logging # pylint: disable=W0611
-from . import metadata
+from . import metadata_general
 from guessit import guessit
 from common import common_config_ini
 from common import common_metadata_anidb
