3ce8a0614378c7beb2c88e5e1f2a16eb3dc57890
SpootDev <spootdev@gmail.com>
2016-10-06 16:34:35 -0500

    fetch thru limiter

--- subprogram_metadata_tmdb_updates.py
+++ subprogram_metadata_tmdb_updates.py
@@ -19,7 +19,6 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 import logging # pylint: disable=W0611
 import json
-import uuid
 import locale
 locale.setlocale(locale.LC_ALL, '')
 from common import common_config_ini
@@ -52,55 +51,26 @@ movie_updated = 0
 movie_inserted = 0
 
 
-def movie_fetch_save(tmdb_id):
-    """
-    # fetch from tmdb
-    """
-    metadata_uuid = None
-    logging.debug("fetch: %s", tmdb_id)
-    # fetch and save json data via tmdb id
-    result_json = tmdb.com_tmdb_meta_by_id(tmdb_id)
-    if result_json is not None:
-        logging.debug("here I am")
-        series_id_json, result_json, image_json = tmdb.com_tmdb_meta_info_build(result_json)
-        cast_json = tmdb.com_tmdb_meta_cast_by_id(tmdb_id)
-        # set and insert the record
-        meta_json = ({'Meta': {'TMDB': {'Meta': result_json, 'Cast': cast_json['cast'],\
-            'Crew': cast_json['crew']}}})
-        # check for previous record
-        if db_connection.db_meta_tmdb_count(result_json['id']) > 0:
-            # TODO if this is > 0......MUST use series id from DB.......so, stuff doesn't get wiped
-            #db_connection.db_meta_update(series_id_json, result_json['title'],
-        #json.dumps(meta_json), json.dumps(image_json))
-            pass
-        else:
-            # store person info
-            if 'cast' in cast_json:
-                db_connection.db_meta_person_insert_cast_crew('TMDB', cast_json['cast'])
-            if 'crew' in cast_json:
-                db_connection.db_meta_person_insert_cast_crew('TMDB', cast_json['crew'])
-            # grab reviews
-            review_json = tmdb.com_tmdb_meta_review_by_id(tmdb_id)
-            if review_json['total_results'] > 0:
-                review_json_id = ({'TMDB': str(review_json['id'])})
-                logging.debug("review: %s", review_json_id)
-                db_connection.db_review_insert(json.dumps(review_json_id),\
-                    json.dumps({'TMDB': review_json}))
-            # set and insert the record
-            metadata_uuid = str(uuid.uuid4())
-            db_connection.db_meta_insert_tmdb(metadata_uuid, series_id_json,\
-                result_json['title'], json.dumps(meta_json), json.dumps(image_json))
-    return metadata_uuid
-
-
 # grab the updated data
 tmdb = common_metadata_tmdb.CommonMetadataTMDB(option_config_json)
+
+# process movie changes
 for movie_change in tmdb.com_tmdb_meta_changes_movie()['results']:
     logging.debug("mov: %s", movie_change['id'])
-    movie_fetch_save(movie_change['id'])
+    dl_meta = db_connection.db_download_que_exists(None, 'themoviedb', str(movie_change['id']))
+    if dl_meta is None:
+        db_connection.db_download_insert('themoviedb', json.dumps({'MediaID': None,\
+            'Path': None, 'ClassID': None, 'Status': 'Fetch',\
+            'MetaNewID': None, 'ProviderMetaID': str(movie_change['id'])}))
+
+# process tv changes
 for tv_change in tmdb.com_tmdb_meta_changes_tv()['results']:
     logging.debug("tv: %s", tv_change['id'])
-    movie_fetch_save(tv_change['id'])
+    dl_meta = db_connection.db_download_que_exists(None, 'themoviedb', str(tv_change['id']))
+    if dl_meta is None:
+        db_connection.db_download_insert('themoviedb', json.dumps({'MediaID': None,\
+            'Path': None, 'ClassID': None, 'Status': 'Fetch',\
+            'MetaNewID': None, 'ProviderMetaID': str(tv_change['id'])}))
 
 
 # log end
