1dc754ba9fd3cce3aa3851aa667f5266de3334da
SpootDev <spootdev@gmail.com>
2016-10-17 20:38:35 -0500

    logging

--- main_server.py
+++ main_server.py
@@ -45,6 +45,7 @@ common_logging.com_logging_start()
 logging.info('Check Certs')
 # check for and create ssl certs if needed
 if not os.path.isfile('./key/cacert.pem'):
+    logging.info('Cert not found, generating.')
     proc_ssl = subprocess.Popen(['./subprogram_ssl_keygen'], shell=False)
     proc_ssl.wait()
     if not os.path.isfile('./key/cacert.pem'):
