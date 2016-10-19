6ac610273e7be8b8a42ca58abb250d31e87ccacf
SpootDev <spootdev@gmail.com>
2016-10-18 21:49:34 -0500

    rename and new

--- build_code/jenkins/pipeline-build-os/pipeline-build-os-source-server-ubuntu-16-04.py
+++ build_code/jenkins/pipeline-build-os/pipeline-build-os-source-server-ubuntu-16-04.py
@@ -31,8 +31,8 @@ from common import common_network_vm_proxmox
 ###
 # Will be used to build the deps like FFMPEG
 ###
-JENKINS_BUILD_SOURCE_VIM_LXC = 115
-JENKINS_BUILD_SOURCE_VIM_LNX_IP = '10.0.0.100'
+JENKINS_BUILD_SOURCE_VIM_LXC = 116
+JENKINS_BUILD_SOURCE_VIM_LNX_IP = '10.0.0.99'
 FFMPEG_BRANCH = 'dev-0.1.11'
 
 
@@ -60,16 +60,17 @@ SSH_BUILD.com_net_ssh_run_sudo_command('sudo apt-get update')
 # auto yes to accept install
 # this list at the moment is for building pip install
 SSH_BUILD.com_net_ssh_run_sudo_command('sudo apt-get -y install autoconf automake'\
-    ' build-essential libass-dev'\
+    ' build-essential libass-dev postgresql-server-dev-9.5'\
     ' libfreetype6-dev libsdl1.2-dev libtheora-dev libtool libva-dev libvdpau-dev'\
     ' libvorbis-dev libxcb1-dev libxcb-shm0-dev libxcb-xfixes0-dev pkg-config texinfo'\
-    ' zlib1g-dev yasm cmake curl wget libsmbclient-dev python-pip postgresql-server-dev-9.5'\
+    ' zlib1g-dev yasm cmake curl wget libsmbclient-dev python-pip '\
     ' libffi-dev libsnmp-dev libldap2-dev libsasl2-dev portaudio19-dev')
 # setup pip and pyinstaller
 SSH_BUILD.com_net_ssh_run_sudo_command('sudo pip install --upgrade pip')
 
 #SSH_BUILD.com_net_ssh_run_sudo_command('sudo pip install -r /home/metaman/MediaKraken_Deployment/'\
 #    'build_code/jenkins/pipeline-build-os/pipeline-build-os-pip-server-ubuntu.txt')
+
 # should fix pyopenssl error
 SSH_BUILD.com_net_ssh_run_sudo_command('sudo pip install requests[security]')
 
