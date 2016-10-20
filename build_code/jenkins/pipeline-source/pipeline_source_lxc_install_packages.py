518936eb97b33d7ec3ec8ee524ae0374efc507b9
SpootDev <spootdev@gmail.com>
2016-10-20 13:49:34 -0500

    install calibre bin to custom local

--- build_code/jenkins/pipeline-source/pipeline_source_lxc_install_packages.py
+++ build_code/jenkins/pipeline-source/pipeline_source_lxc_install_packages.py
@@ -84,6 +84,12 @@ for server_info in pipeline_source_lxc_definitions.SERVERS_TO_BUILD:
             SSH_BUILD.com_net_ssh_run_sudo_command('sudo sshpass -p \'metaman\''\
                 ' scp -r -o StrictHostKeyChecking=no ./MediaKraken.ini'\
                 ' metaman@%s:/home/metaman/.' % '10.0.0.109')
+            # install calibre binaries for ebook conversion support
+            SSH_BUILD.com_net_ssh_run_command('wget -nv -O- https://raw.githubusercontent.com/'\
+                'kovidgoyal/calibre/master/setup/linux-installer.py |'\
+                ' python -c "import sys; main=lambda x,y:'\
+                'sys.stderr.write(\'Download failed\n\'); exec(sys.stdin.read());'\
+                ' main(\'/home/metaman/mediakraken/bin\', True)"')
         else:
             # move only slave programs
             SSH_BUILD.com_net_ssh_run_sudo_command('sudo sshpass -p \'metaman\''\
