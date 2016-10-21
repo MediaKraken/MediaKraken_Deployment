5368fbeb136cb03a9419d2aa81cacbc2fc1567f3
SpootDev <spootdev@gmail.com>
2016-10-21 15:25:52 -0500

    curl options and other packages

--- build_code/jenkins/pipeline-source/pipeline-build-os-ffmpeg-ubuntu-1604.sh
+++ build_code/jenkins/pipeline-source/pipeline-build-os-ffmpeg-ubuntu-1604.sh
@@ -54,7 +54,7 @@ make install
 make distclean
 
 cd ~/ffmpeg_sources
-curl -O http://downloads.xiph.org/releases/ogg/libogg-1.3.2.tar.gz
+curl -L -O http://downloads.xiph.org/releases/ogg/libogg-1.3.2.tar.gz
 tar xzvf libogg-1.3.2.tar.gz
 cd libogg-1.3.2
 ./configure --prefix="$HOME/ffmpeg_build" --disable-shared
@@ -63,7 +63,7 @@ make install
 make distclean
 
 cd ~/ffmpeg_sources
-curl -O http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.5.tar.gz
+curl -L -O http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.5.tar.gz
 tar xzvf libvorbis-1.3.5.tar.gz
 cd libvorbis-1.3.5
 LDFLAGS="-L$HOME/ffmeg_build/lib" CPPFLAGS="-I$HOME/ffmpeg_build/include" ./configure --prefix="$HOME/ffmpeg_build" --with-ogg="$HOME/ffmpeg_build" --disable-shared
@@ -80,7 +80,7 @@ make install
 make clean
 
 cd ~/ffmpeg_sources
-curl -O http://downloads.xiph.org/releases/speex/speexdsp-1.2rc3.tar.gz
+curl -L -O http://downloads.xiph.org/releases/speex/speexdsp-1.2rc3.tar.gz
 tar xzvf speexdsp-1.2rc3.tar.gz
 cd speexdsp-1.2rc3
 ./configure
@@ -88,9 +88,18 @@ make -j8
 make install
 
 cd ~/ffmpeg_sources
-curl -O http://downloads.xvid.org/downloads/xvidcore-1.3.4.tar.gz
+curl -L -O http://downloads.xvid.org/downloads/xvidcore-1.3.4.tar.gz
 tar xzvf xvidcore-1.3.4.tar.gz
 cd xvidcore/build/generic
+./configure
+make -j8
+make install
+
+cd ~/ffmpeg_sources
+curl -L -O https://github.com/libass/libass/archive/0.13.4.tar.gz
+tar xzvf 0.13.4.tar.gz
+cd libass-0.13.4
+./autogen.sh
 ./configure
 make -j8
 make install
