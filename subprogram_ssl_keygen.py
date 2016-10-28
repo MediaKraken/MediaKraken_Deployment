0b68c5b4bfd69e14008c47184675f07c6d33879f
SpootDev <spootdev@gmail.com>
2016-10-27 22:15:49 -0500

    use config

--- subprogram_ssl_keygen.py
+++ subprogram_ssl_keygen.py
@@ -19,7 +19,6 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 import logging # pylint: disable=W0611
 from subprocess import Popen, PIPE
-from time import sleep
 import shutil
 import sys
 import os
@@ -29,39 +28,13 @@ import os
 if str.upper(sys.platform[0:3]) == 'WIN' or str.upper(sys.platform[0:3]) == 'CYG':
     PROC = Popen(['openssl', 'req', '-x509', '-nodes', '-newkey', 'rsa:2048',\
         '-keyout', 'privkey.pem', '-out', 'cacert.pem', '-days', '1000', '-config',\
-        './key/windows_openssl.cfg'], shell=False)
+        './key/openssl.cfg'], shell=False)
     PROC.wait() # have to do here so the move has sumthing to move
     shutil.move('privkey.pem', './key/.')
     shutil.move('cacert.pem', './key/.')
 else:
     PROC = Popen(['openssl', 'req', '-x509', '-nodes', '-newkey', 'rsa:2048',\
-        '-keyout', 'privkey.pem', '-out', 'cacert.pem', '-days', '1000'], shell=False, stdin=PIPE)
-    sleep(5)
-    # country code
-    PROC.stdin.write('US\n')
-    PROC.stdin.flush()
-    sleep(2)
-    # state or prov
-    PROC.stdin.write('ND\n')
-    PROC.stdin.flush()
-    sleep(2)
-    # city
-    PROC.stdin.write(b'\n')
-    PROC.stdin.flush()
-    sleep(2)
-    # organization
-    PROC.stdin.write('MediaKraken\n')
-    PROC.stdin.flush()
-    sleep(2)
-    # org unit name
-    PROC.stdin.write('MKServer\n')
-    PROC.stdin.flush()
-    sleep(2)
-    # common name
-    PROC.stdin.write('www.mediakraken.org\n')
-    PROC.stdin.flush()
-    # email addy
-    PROC.stdin.write('spootdev@gmail.com\n')
-    PROC.stdin.flush()
+        '-keyout', 'privkey.pem', '-out', 'cacert.pem', '-days', '1000',\
+        '-config', './key/openssl.cfg'], shell=False, stdin=PIPE)
     PROC.wait() # have to do here so the move has sumthing to move
     os.system('mv %s %s' % ('*.pem', './key/.'))
