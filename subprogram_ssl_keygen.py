aa7e1d1bf074b902cc37ac375e66caeab553e78b
SpootDev <spootdev@gmail.com>
2016-10-27 19:51:48 -0500

    set ssl key fields

--- subprogram_ssl_keygen.py
+++ subprogram_ssl_keygen.py
@@ -18,7 +18,8 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 import logging # pylint: disable=W0611
-import subprocess
+from subprocess import Popen, PIPE
+from time import sleep
 import shutil
 import sys
 import os
@@ -26,14 +27,41 @@ import os
 
 # generate key
 if str.upper(sys.platform[0:3]) == 'WIN' or str.upper(sys.platform[0:3]) == 'CYG':
-    PROC = subprocess.Popen(['openssl', 'req', '-x509', '-nodes', '-newkey', 'rsa:2048',\
+    PROC = Popen(['openssl', 'req', '-x509', '-nodes', '-newkey', 'rsa:2048',\
         '-keyout', 'privkey.pem', '-out', 'cacert.pem', '-days', '1000', '-config',\
         './key/windows_openssl.cfg'], shell=False)
     PROC.wait() # have to do here so the move has sumthing to move
     shutil.move('privkey.pem', './key/.')
     shutil.move('cacert.pem', './key/.')
 else:
-    PROC = subprocess.Popen(['openssl', 'req', '-x509', '-nodes', '-newkey', 'rsa:2048',\
-        '-keyout', 'privkey.pem', '-out', 'cacert.pem', '-days', '1000'], shell=False)
+    PROC = Popen(['openssl', 'req', '-x509', '-nodes', '-newkey', 'rsa:2048',\
+        '-keyout', 'privkey.pem', '-out', 'cacert.pem', '-days', '1000'], shell=False, stdin=PIPE)
+    sleep(5)
+    # country code
+    PROC.stdin.write('US\n')
+    PROC.stdin.flush()
+    sleep(2)
+    # state or prov
+    PROC.stdin.write('ND\n')
+    PROC.stdin.flush()
+    sleep(2)
+    # city
+    PROC.stdin.write(b'\n')
+    PROC.stdin.flush()
+    sleep(2)
+    # organization
+    PROC.stdin.write('MediaKraken\n')
+    PROC.stdin.flush()
+    sleep(2)
+    # org unit name
+    PROC.stdin.write('MKServer\n')
+    PROC.stdin.flush()
+    sleep(2)
+    # common name
+    PROC.stdin.write('www.mediakraken.org\n')
+    PROC.stdin.flush()
+    # email addy
+    PROC.stdin.write('spootdev@gmail.com\n')
+    PROC.stdin.flush()
     PROC.wait() # have to do here so the move has sumthing to move
     os.system('mv %s %s' % ('*.pem', './key/.'))
