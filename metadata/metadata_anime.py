cdcb6498d82b524a17c3ef92d76b64468ac642fa
SpootDev <spootdev@gmail.com>
2016-11-09 16:15:48 -0600

    begin proper lookup

--- metadata/metadata_anime.py
+++ metadata/metadata_anime.py
@@ -34,18 +34,52 @@ else:
     ANIDB_CONNECTION = None
 
 
-def metadata_anime_lookup(db_connection, media_file_path, download_que_id):
+def metadata_anime_lookup(db_connection, file_name, download_que_id):
     """
     Check for anime in tv sections of the metadata providers
     """
-    metadata_uuid, imdb_id, tmdb_id = nfo_xml_db_lookup_tv(db_connection, nfo_data, xml_data)
-    if metadata_uuid is None:
-        metadata_uuid = tveps_lookup(guessit(media_file_path), imdb_id, tvdb_id, 'en')
-    if metadata_uuid is None:
-        # check movie data
-        metadata_uuid, imdb_id, tmdb_id = nfo_xml_db_lookup(db_connection, nfo_data, xml_data)
-        if metadata_uuid is None:
-            metadata_uuid = movie_lookup(guessit(media_file_path), imdb_id, tmdb_id)
-    # TODO   or perhaps JMM hit
-    # TODO   hit anidb
+    if not hasattr(metadata_anime_lookup, "metadata_last_id"):
+        metadata_anime_lookup.metadata_last_id = None # it doesn't exist yet, so initialize it
+        metadata_anime_lookup.metadata_last_title = None
+        metadata_anime_lookup.metadata_last_year = None
+        metadata_anime_lookup.metadata_last_imdb = None
+        metadata_anime_lookup.metadata_last_tmdb = None
+        metadata_anime_lookup.metadata_last_rt = None
+        metadata_anime_lookup.metadata_last_anidb = None
+    metadata_uuid = None # so not found checks verify later
+    logging.info('meta anime look filename: %s', file_name)
+    # check for dupes by name/year
+    if 'year' in file_name:
+        if file_name['title'] == metadata_anime_lookup.metadata_last_title\
+                and file_name['year'] == metadata_anime_lookup.metadata_last_year:
+            db_connection.db_download_delete(download_que_id)
+            logging.info('meta movie return 1 %s',  metadata_anime_lookup.metadata_last_id)
+            # don't need to set last......since they are equal
+            return metadata_anime_lookup.metadata_last_id
+    elif file_name['title'] == metadata_anime_lookup.metadata_last_title:
+        db_connection.db_download_delete(download_que_id)
+        logging.info('meta movie return 2 %s',  metadata_anime_lookup.metadata_last_id)
+        # don't need to set last......since they are equal
+        return metadata_anime_lookup.metadata_last_id
+
+
+# themoviedb 
+# tvmaze
+# thetvdb
+# anidb
+
+
+
+
+    # set last values to negate lookups for same title/show
+    metadata_anime_lookup.metadata_last_id = metadata_uuid
+    metadata_anime_lookup.metadata_last_title = file_name['title']
+    try:
+        metadata_anime_lookup.metadata_last_year = file_name['year']
+    except:
+        metadata_anime_lookup.metadata_last_year = None
+    metadata_anime_lookup.metadata_last_imdb = imdb_id
+    metadata_anime_lookup.metadata_last_tmdb = tmdb_id
+    metadata_anime_lookup.metadata_last_rt = rt_id
+    metadata_anime_lookup.metadata_last_anidb = anidb
     return metadata_uuid
