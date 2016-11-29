f7401f6a6bfcd0a2911601ae5a99bdb9514c9a10
SpootDev <spootdev@gmail.com>
2016-11-29 14:15:25 -0600

    fix path

--- docker/alpine/source_sync.sh
+++ docker/alpine/source_sync.sh
@@ -6,23 +6,23 @@ cp /home/spoot/MediaKraken_Deployment/main_hardware_discover.py  /home/spoot/Med
 cp -R /home/spoot/MediaKraken_Deployment/common /home/spoot/MediaKraken_Deployment/docker/alpine/ComposeMediaKrakenMetadata/src/.
 cp -R /home/spoot/MediaKraken_Deployment/database /home/spoot/MediaKraken_Deployment/docker/alpine/ComposeMediaKrakenMetadata/src/.
 cp -R /home/spoot/MediaKraken_Deployment/metadata /home/spoot/MediaKraken_Deployment/docker/alpine/ComposeMediaKrakenMetadata/src/.
-cp /home/spoot/MediaKraken_Deployment/main_server_metadata_api.py  /home/spoot/MediaKraken_Deployment/docker/alpine/ComposeMediaKrakenDevicescan/src/.
+cp /home/spoot/MediaKraken_Deployment/main_server_metadata_api.py  /home/spoot/MediaKraken_Deployment/docker/alpine/ComposeMediaKrakenMetadata/src/.
 
 # server
 cp -R /home/spoot/MediaKraken_Deployment/common /home/spoot/MediaKraken_Deployment/docker/alpine/ComposeMediaKrakenServer/src/.
 cp -R /home/spoot/MediaKraken_Deployment/database /home/spoot/MediaKraken_Deployment/docker/alpine/ComposeMediaKrakenServer/src/.
 cp -R /home/spoot/MediaKraken_Deployment/network /home/spoot/MediaKraken_Deployment/docker/alpine/ComposeMediaKrakenServer/src/.
-cp /home/spoot/MediaKraken_Deployment/db_create_update.py  /home/spoot/MediaKraken_Deployment/docker/alpine/ComposeMediaKrakenDevicescan/src/.
-cp /home/spoot/MediaKraken_Deployment/db_update_version.py  /home/spoot/MediaKraken_Deployment/docker/alpine/ComposeMediaKrakenDevicescan/src/.
-cp /home/spoot/MediaKraken_Deployment/main_server.py  /home/spoot/MediaKraken_Deployment/docker/alpine/ComposeMediaKrakenDevicescan/src/.
-cp /home/spoot/MediaKraken_Deployment/main_server_link.py  /home/spoot/MediaKraken_Deployment/docker/alpine/ComposeMediaKrakenDevicescan/src/.
-cp /home/spoot/MediaKraken_Deployment/main_server_trigger.py  /home/spoot/MediaKraken_Deployment/docker/alpine/ComposeMediaKrakenDevicescan/src/.
-cp /home/spoot/MediaKraken_Deployment/subprogram*.py  /home/spoot/MediaKraken_Deployment/docker/alpine/ComposeMediaKrakenDevicescan/src/.
+cp /home/spoot/MediaKraken_Deployment/db_create_update.py  /home/spoot/MediaKraken_Deployment/docker/alpine/ComposeMediaKrakenServer/src/.
+cp /home/spoot/MediaKraken_Deployment/db_update_version.py  /home/spoot/MediaKraken_Deployment/docker/alpine/ComposeMediaKrakenServer/src/.
+cp /home/spoot/MediaKraken_Deployment/main_server.py  /home/spoot/MediaKraken_Deployment/docker/alpine/ComposeMediaKrakenServer/src/.
+cp /home/spoot/MediaKraken_Deployment/main_server_link.py  /home/spoot/MediaKraken_Deployment/docker/alpine/ComposeMediaKrakenServer/src/.
+cp /home/spoot/MediaKraken_Deployment/main_server_trigger.py  /home/spoot/MediaKraken_Deployment/docker/alpine/ComposeMediaKrakenServer/src/.
+cp /home/spoot/MediaKraken_Deployment/subprogram*.py  /home/spoot/MediaKraken_Deployment/docker/alpine/ComposeMediaKrakenServer/src/.
 
 # slave
 cp -R /home/spoot/MediaKraken_Deployment/common /home/spoot/MediaKraken_Deployment/docker/alpine/ComposeMediaKrakenSlave/src/.
 cp -R /home/spoot/MediaKraken_Deployment/database /home/spoot/MediaKraken_Deployment/docker/alpine/ComposeMediaKrakenSlave/src/.
-cp /home/spoot/MediaKraken_Deployment/main_server_slave.py  /home/spoot/MediaKraken_Deployment/docker/alpine/ComposeMediaKrakenDevicescan/src/.
+cp /home/spoot/MediaKraken_Deployment/main_server_slave.py  /home/spoot/MediaKraken_Deployment/docker/alpine/ComposeMediaKrakenSlave/src/.
 
 # webapi
 cp -R /home/spoot/MediaKraken_Deployment/common /home/spoot/MediaKraken_Deployment/docker/alpine/ComposeMediaKrakenWebAPI/src/.
