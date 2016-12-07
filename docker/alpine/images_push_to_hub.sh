eb823138cf88d764185dccfac4a74a4f97f702c1
SpootDev <spootdev@gmail.com>
2016-12-06 19:47:02 -0600

    tag and push

---
+++ docker/alpine/images_push_to_hub.sh
@@ -0,0 +1,8 @@
+docker push mediakraken/mkslave:latest
+docker push mediakraken/mkserver:latest
+docker push mediakraken/mkdatabase:latest
+docker push mediakraken/mkdevicescan:latest
+docker push mediakraken/mkmetadata:latest
+docker push mediakraken/mknginx:latest
+docker push mediakraken/mkredis:latest
+docker push mediakraken/mkwebapp:latest
