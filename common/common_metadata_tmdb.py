573d6f851142efd19b7895e5010a63991755bf73
SpootDev <spootdev@gmail.com>
2016-12-14 17:57:52 -0600

    fix for combined results

--- common/common_metadata_tmdb.py
+++ common/common_metadata_tmdb.py
@@ -83,13 +83,13 @@ class CommonMetadataTMDB(object):
             'person')
         logging.info('tmdb bio image path: %s', image_file_path)
         person_file_paths = []
-        if ['images']['profiles'] in result_json:
-            for profile_image in result_json['images']['profiles']:
-                image_file_path += profile_image['file_path']
+        if ['combined_credits']['cast'] in result_json:
+            for profile_image in result_json['combined_credits']['cast']:
+                image_file_path += profile_image['poster_path']
                 if not os.path.isfile('/mediakraken/web_app/MediaKraken/static/meta/images/' \
                                       + image_file_path):
                     common_network.mk_network_fetch_from_url('https://image.tmdb.org/t/p/original'\
-                        + result_json['poster_path'], \
+                        + profile_image['poster_path'], \
                         '/mediakraken/web_app/MediaKraken/static/meta/images/' \
                         + image_file_path)
                 person_file_paths.append(image_file_path)
