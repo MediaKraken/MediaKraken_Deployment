2f8d74a8e9e366a542e05b4cc105647eb0fa9cf8
SpootDev <spootdev@gmail.com>
2016-12-14 21:12:19 -0600

    fix up path and allow none posters

--- common/common_metadata_tmdb.py
+++ common/common_metadata_tmdb.py
@@ -84,13 +84,13 @@ class CommonMetadataTMDB(object):
         logging.info('tmdb bio image path: %s', image_file_path)
         person_file_paths = []
         for profile_image in result_json['combined_credits']['cast']:
-            image_file_path += profile_image['poster_path']
-            if not os.path.isfile('/mediakraken/web_app/MediaKraken/static/meta/images/' \
-                                  + image_file_path):
-                common_network.mk_network_fetch_from_url('https://image.tmdb.org/t/p/original'\
-                    + profile_image['poster_path'], \
-                    '/mediakraken/web_app/MediaKraken/static/meta/images/' \
-                    + image_file_path)
+            if profile_image['poster_path'] is not None:
+                if not os.path.isfile('/mediakraken/web_app/MediaKraken/static/meta/images/' \
+                                      + image_file_path + profile_image['poster_path']):
+                    common_network.mk_network_fetch_from_url('https://image.tmdb.org/t/p/original'\
+                        + profile_image['poster_path'], \
+                        '/mediakraken/web_app/MediaKraken/static/meta/images/' \
+                        + image_file_path + profile_image['poster_path'])
             person_file_paths.append(image_file_path)
         # set local image json
         return ({'Images': {'themoviedb':{'Profiles': person_file_paths}}})
