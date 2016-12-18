5f8de757565a0729d6a2f6c4a9a49717941cd9e9
SpootDev <spootdev@gmail.com>
2016-12-18 15:01:01 -0600

    docker hub

--- docker/alpine/images_push_to_hub.sh
+++ docker/alpine/images_push_to_hub.sh
@@ -4,5 +4,6 @@ docker push mediakraken/mkdatabase:latest
 docker push mediakraken/mkdevicescan:latest
 docker push mediakraken/mkmetadata:latest
 docker push mediakraken/mknginx:latest
+docker push mediakraken/mkpgbounce:latest
 docker push mediakraken/mkredis:latest
 docker push mediakraken/mkwebapp:latest
