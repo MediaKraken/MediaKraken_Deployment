b14465ff2e6a89ef63b548bfbbc96f2ece2911f7
SpootDev <spootdev@gmail.com>
2016-12-26 22:20:10 -0600

    openldap

--- docker/alpine/images_push_to_hub.sh
+++ docker/alpine/images_push_to_hub.sh
@@ -4,6 +4,7 @@ docker push mediakraken/mkdatabase:latest
 docker push mediakraken/mkdevicescan:latest
 docker push mediakraken/mkmetadata:latest
 docker push mediakraken/mknginx:latest
+docker push mediakraken/mkopenldap:latest
 docker push mediakraken/mkpgbounce:latest
 docker push mediakraken/mkredis:latest
 docker push mediakraken/mkwebapp:latest
--- docker/alpine/tag_rename_images.sh
+++ docker/alpine/tag_rename_images.sh
@@ -4,6 +4,7 @@ docker tag alpine_database:latest mediakraken/mkdatabase:latest
 docker tag alpine_devicescan:latest mediakraken/mkdevicescan:latest
 docker tag alpine_metadata:latest mediakraken/mkmetadata:latest
 docker tag alpine_nginx:latest mediakraken/mknginx:latest
+docker tag alpine_openldap:latest mediakraken/mkopenldap:latest
 docker tag alpine_pgbounce:latest mediakraken/mkpgbounce:latest
 docker tag alpine_redis:latest mediakraken/mkredis:latest
 docker tag alpine_webserver:latest mediakraken/mkwebapp:latest
