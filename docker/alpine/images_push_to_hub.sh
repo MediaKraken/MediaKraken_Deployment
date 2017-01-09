08ac0654e9aaca1cd8a6db8823734c2c7ed62a35
SpootDev <spootdev@gmail.com>
2017-01-08 22:22:47 -0600

    rename and move images

--- docker/alpine/images_push_to_hub.sh
+++ docker/alpine/images_push_to_hub.sh
@@ -1,11 +1,13 @@
 docker push mediakraken/mkbrainz:latest
-docker push mediakraken/mkslave:latest
-docker push mediakraken/mkserver:latest
 docker push mediakraken/mkdatabase:latest
 docker push mediakraken/mkdevicescan:latest
+docker push mediakraken/mkcelery:latest
+docker push mediakraken/mkslave:latest
+docker push mediakraken/mkserver:latest
 docker push mediakraken/mkmetadata:latest
 docker push mediakraken/mknginx:latest
 docker push mediakraken/mkopenldap:latest
 docker push mediakraken/mkpgbounce:latest
+docker push mediakraken/mkrabbitmq:latest
 docker push mediakraken/mkredis:latest
 docker push mediakraken/mkwebapp:latest
