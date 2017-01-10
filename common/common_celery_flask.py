e90c1e9b39f08f89b4fd25f5b8da1f58e50952a3
SpootDev <spootdev@gmail.com>
2017-01-10 10:56:26 -0600

    remove connection

--- common/common_celery_flask.py
+++ common/common_celery_flask.py
@@ -21,14 +21,6 @@ import logging # pylint: disable=W0611
 from celery import Celery
 
 
-flask_app = Flask(__name__)
-flask_app.config.update(
-    CELERY_BROKER_URL='redis://localhost:6379',
-    CELERY_RESULT_BACKEND='redis://localhost:6379'
-)
-celery = make_celery(flask_app)
-
-
 def make_celery(app):
     celery = Celery(app.import_name, backend=app.config['CELERY_RESULT_BACKEND'],
                     broker=app.config['CELERY_BROKER_URL'])
