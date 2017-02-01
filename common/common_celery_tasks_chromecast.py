53814b8dbcd6f4eaf9cd98c7d648b28e1441b2b8
SpootDev <spootdev@gmail.com>
2017-01-31 19:17:12 -0600

    hardcode path since docker

--- common/common_celery_tasks_chromecast.py
+++ common/common_celery_tasks_chromecast.py
@@ -20,7 +20,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import logging # pylint: disable=W0611
 from . import common_celery
 import sys
-sys.path.append('.')
+sys.path.append("/mediakraken")
 from network import network_base_string as network_base
 
 
