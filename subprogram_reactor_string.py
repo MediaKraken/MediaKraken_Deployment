ae53f2cc7dc3512ab514f56dd2bf4c207e773b70
SpootDev <spootdev@gmail.com>
2017-04-17 17:17:58 -0500

    turn off durable

--- subprogram_reactor_string.py
+++ subprogram_reactor_string.py
@@ -39,7 +39,8 @@ def run(connection):
     #queue = yield channel.queue_declare(queue='mkque', auto_delete=False, exclusive=False)
     #yield channel.queue_bind(exchange='topic_link', queue='mkque', routing_key='mkque.world')
 
-    queue = yield channel.queue_declare(queue='mkque', auto_delete=False, exclusive=False)
+    queue = yield channel.queue_declare(queue='mkque', auto_delete=False, exclusive=False,
+                                        durable=False)
     yield channel.queue_bind(queue='mkque')
 
     yield channel.basic_qos(prefetch_count=1)
