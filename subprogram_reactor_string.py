4dd7aa55ce69ecb9888404503f9892f30e2a3dd0
SpootDev <spootdev@gmail.com>
2017-04-22 13:44:36 -0500

    remove route key

--- subprogram_reactor_string.py
+++ subprogram_reactor_string.py
@@ -38,7 +38,7 @@ def run(connection):
     exchange = yield channel.exchange_declare(exchange='mkque_ex', type='direct')
     queue = yield channel.queue_declare(queue='mkque', auto_delete=False, exclusive=False,
                                         durable=True)
-    yield channel.queue_bind(exchange='mkque_ex', queue='mkque', routing_key='mkque.world')
+    yield channel.queue_bind(exchange='mkque_ex', queue='mkque') #, routing_key='mkque.world')
     yield channel.basic_qos(prefetch_count=1)
     queue_object, consumer_tag = yield channel.basic_consume(queue='mkque', no_ack=False)
     l = task.LoopingCall(read, queue_object)
