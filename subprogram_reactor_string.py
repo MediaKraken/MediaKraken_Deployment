201a751c895e6f1944b938d9642c0b5ad3cc2dfa
SpootDev <spootdev@gmail.com>
2017-04-22 13:06:19 -0500

    logging

--- subprogram_reactor_string.py
+++ subprogram_reactor_string.py
@@ -49,7 +49,7 @@ def run(connection):
 def read(queue_object):
     ch, method, properties, body = yield queue_object.get()
     if body:
-        print(body)
+        logging.info("body %s", body)
         network_base.NetworkEvents.broadcast_celery_message(body)
     yield ch.basic_ack(delivery_tag=method.delivery_tag)
 
