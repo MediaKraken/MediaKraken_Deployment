6b1089c760bdc0fdcb96301c87bb7f5c17bc8096
SpootDev <spootdev@gmail.com>
2017-05-16 20:07:09 -0500

    cleanup

--- web_app/MediaKraken/settings.py
+++ web_app/MediaKraken/settings.py
@@ -37,7 +37,6 @@ class ProdConfig(Config):
         'username': 'guest',  #convenience param for username
         'password': 'guest',  #convenience param for password
         'port': 5672            #amqp server port
-#        'virtual_host': 'vhost'   #amqp vhost
     }
     FLASK_PIKA_POOL_PARAMS = {
         'pool_size': 8,
