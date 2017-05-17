bccec9f24735f15163479cad08e86d5b900bad5a
SpootDev <spootdev@gmail.com>
2017-05-16 20:02:26 -0500

    drop vhost

--- web_app/MediaKraken/settings.py
+++ web_app/MediaKraken/settings.py
@@ -36,8 +36,8 @@ class ProdConfig(Config):
         'host': 'mkrabbitmq',      #amqp.server.com
         'username': 'guest',  #convenience param for username
         'password': 'guest',  #convenience param for password
-        'port': 5672,            #amqp server port
-        'virtual_host': 'vhost'   #amqp vhost
+        'port': 5672            #amqp server port
+#        'virtual_host': 'vhost'   #amqp vhost
     }
     FLASK_PIKA_POOL_PARAMS = {
         'pool_size': 8,
