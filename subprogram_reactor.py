1a53f216d1f990cc200261a590673cb9637eab42
SpootDev <spootdev@gmail.com>
2017-05-17 20:55:50 -0500

    more log log log

--- subprogram_reactor.py
+++ subprogram_reactor.py
@@ -64,12 +64,14 @@ def read(queue_object):
                                        + ' -subtitles_language ' + json_message['Language']
                 else:
                     subtitle_command = ''
+                logging.info('here before dock')
                 docker_inst = common_docker.CommonDocker()
+                logging.info('after dock')
                 docker_inst.com_docker_run_container('python /mediakraken/stream2chromecast/stream2chromecast.py'\
                     + ' -devicename' + json_message['Device']\
                     + subtitle_command + ' -transcodeopts -c:v copy -c:a ac3'\
                     + ' -movflags faststart+empty_moov -transcode \'' + json_message['Data'] + '\'')
-
+                logging.info(('after run'))
     yield ch.basic_ack(delivery_tag=method.delivery_tag)
 
 
