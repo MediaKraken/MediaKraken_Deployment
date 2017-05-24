0f7b017095d7b650a23f0f48dcf36bc73d8b1ecc
SpootDev <spootdev@gmail.com>
2017-05-23 20:13:21 -0500

    latest tag

--- docker/alpine/images_push_to_hub.sh
+++ docker/alpine/images_push_to_hub.sh
@@ -2,8 +2,10 @@ docker push mediakraken/mkbrainz:latest
 docker push mediakraken/mkbroadcast:latest
 docker push mediakraken/mkdatabase:latest
 docker push mediakraken/mkdevicescan:latest
-docker push mediakraken/mkbaseffmpeg
+docker push mediakraken/mkbaseffmpeg:latest
+docker push mediakraken/mkbaseffmpegnvidia:latest
 docker push mediakraken/mkslave:latest
+docker push mediakraken/mkslavenvidia:latest
 docker push mediakraken/mkserver:latest
 docker push mediakraken/mkmetadata:latest
 docker push mediakraken/mknginx:latest
