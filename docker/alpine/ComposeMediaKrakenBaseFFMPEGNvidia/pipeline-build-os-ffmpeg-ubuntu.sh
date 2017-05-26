36569016e0438dcc2783b91b15081c3147e48557
SpootDev <spootdev@gmail.com>
2017-05-26 15:36:16 -0500

    nvec compile for ffmpeg

--- docker/alpine/ComposeMediaKrakenBaseFFMPEGNvidia/pipeline-build-os-ffmpeg-ubuntu.sh
+++ docker/alpine/ComposeMediaKrakenBaseFFMPEGNvidia/pipeline-build-os-ffmpeg-ubuntu.sh
@@ -113,8 +113,8 @@ cd ffmpeg
 PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure \
     --prefix="$HOME/ffmpeg_build" \
     --pkg-config-flags="--static" \
-    --extra-cflags="-I$HOME/ffmpeg_build/include -I/usr/local/include" \
-    --extra-ldflags="-L$HOME/ffmpeg_build/lib -L/usr/local/lib" \
+    --extra-cflags="-I/usr/local/cuda/include -I$HOME/ffmpeg_build/include -I/usr/local/include" \
+    --extra-ldflags="-L$HOME/ffmpeg_build/lib -L/usr/local/lib -L/usr/local/cuda/lib64" \
     --bindir="$HOME/bin" \
     --enable-gpl \
     --enable-nonfree \
@@ -147,6 +147,10 @@ PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure \
     --enable-libx264 \
     --enable-libx265 \
     --enable-libxvid \
+    --enable-cuda \
+    --enable-cuvid \
+    --enable-nvenc \
+    --enable-libnpp \
     --enable-version3
 make -j16
 make install
