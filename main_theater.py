cacd3ea79cee6c9dbb081af502c2c01b7451f4ed
SpootDev <spootdev@gmail.com>
2017-05-28 19:39:03 -0500

    remove video_mpv for now

--- main_theater.py
+++ main_theater.py
@@ -17,8 +17,8 @@
 '''
 
 from __future__ import absolute_import, division, print_function, unicode_literals
-import pyximport; pyximport.install()
-from theater import video_mpv
+#import pyximport; pyximport.install()
+#from theater import video_mpv
 from common import common_logging
 from common import common_signal
 import platform
