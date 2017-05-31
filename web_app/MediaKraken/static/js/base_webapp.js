312f51858ea38231208d0b814b5a82f1e8e66b47
SpootDev <spootdev@gmail.com>
2017-05-31 18:27:53 -0500

    enable other menu

--- web_app/MediaKraken/static/js/base_webapp.js
+++ web_app/MediaKraken/static/js/base_webapp.js
@@ -47,27 +47,22 @@ $(function() {
 });
 
 
-$(function() {
-	$.contextMenu({
-	    selector: '.context-menu-one', 
-	    callback: function(key, options) {
-		var m = "clicked: " + key;
-		window.console && console.log(m) || alert(m); 
-	    },
-	    items: {
-		"edit": {name: "Edit", icon: "edit"},
-		"cut": {name: "Cut", icon: "cut"},
-	        "copy": {name: "Copy", icon: "copy"},
-		"paste": {name: "Paste", icon: "paste"},
-		"delete": {name: "Delete", icon: "delete"},
-		"sep1": "---------",
-		"quit": {name: "Quit", icon: function(){
-		    return 'context-menu-icon context-menu-icon-quit';
-		}}
-	    }
-	});
-
-	$('.context-menu-one').on('click', function(e){
-	    console.log('clicked', this);
-	})    
+$(function(){
+    $('#the-node').contextMenu({
+        selector: 'div', 
+        callback: function(key, options) {
+            var m = "clicked: " + key + " on " + $(this).text();
+            window.console && console.log(m) || alert(m); 
+        },
+        items: {
+            "edit": {name: "Edit", icon: "edit"},
+            "cut": {name: "Cut", icon: "cut"},
+            "copy": {name: "Copy", icon: "copy"},
+            "paste": {name: "Paste", icon: "paste"},
+            "delete": {name: "Delete", icon: "delete"},
+            "sep1": "---------",
+            "quit": {name: "Quit", icon: function($element, key, item){ return 'context-menu-icon context-menu-icon-quit'; }}
+        }
+    });
 });
+
