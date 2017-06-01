dbee718162bca7c4eaa681763dca7a5dd736ce0b
SpootDev <spootdev@gmail.com>
2017-06-01 10:48:54 -0500

    image path and ajax function

--- web_app/MediaKraken/static/js/base_webapp.js
+++ web_app/MediaKraken/static/js/base_webapp.js
@@ -52,15 +52,32 @@ $(function(){
         selector: 'div', 
         callback: function(key, options) {
             var m = "clicked: " + key + " on " + $(this).attr('data-id');
+
+		$.ajax({
+		        url: '../media_status/' + $(this).attr('data-id') + '/' + media_type + '/' + media_status,
+		        type: 'POST',
+		        success: function(res) {
+		            var result = JSON.parse(res);
+		            if (result.status == 'OK') {
+		                window.location = '../media_status/' + $(this).attr('data-id') + '/' + media_type + '/' + media_status;
+		            } else {
+		                alert(result.status);
+		            }
+		        },
+		        error: function(error) {
+		            console.log(error);
+		        }
+		    });
+
             window.console && console.log(m) || alert(m); 
         },
         items: {
-            "watched": {name: "Set Watched", icon: "../../static/images/microscope.png"},
-            "sync": {name: "Sync Media", icon: "../../static/images/synced.jpg"},
-            "favorite": {name: "Set Favorite", icon: "../../static/images/piggy.png"},
-            "downvote": {name: "Set Downvote", icon: "../../static/images/poo-icon.png"},
+            "watched": {name: "Set Watched", icon: "/static/images/microscope.png"},
+            "sync": {name: "Sync Media", icon: "/static/images/synced.jpg"},
+            "favorite": {name: "Set Favorite", icon: "/static/images/piggy.png"},
+            "poo": {name: "Set Downvote", icon: "/static/images/poo-icon.png"},
             "sep1": "---------",
-            "mismatch": {name: "Set Mismatch", icon: "../../static/images/exclamation-circle-frame.png"},
+            "mismatch": {name: "Set Mismatch", icon: "/static/images/exclamation-circle-frame.png"},
         }
     });
 });
