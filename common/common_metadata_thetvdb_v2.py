dad3a288d7c08fcb3037fdd53d7267aea16f4adc
SpootDev <spootdev@gmail.com>
2017-06-16 20:15:14 -0500

    accept

--- common/common_metadata_thetvdb_v2.py
+++ common/common_metadata_thetvdb_v2.py
@@ -30,4 +30,23 @@ class CommonMetadataTheTVDBv2(object):
     Class for interfacing with TheTVDB with swagger
     """
     def __init__(self, option_config_json):
-        self.thetvdb_connection = option_config_json['API']['thetvdb']
+        self.headers = {'apikey': option_config_json['API']['thetvdb'],
+#                        'username': None, 'userkey': None,
+                        'Accept': 'application/json'}
+        self.base_api_url = 'https://api.thetvdb.com/'
+
+
+    def com_meta_thetvdbv2_login(self):
+        print('header %s', self.headers)
+        resp = requests.post(self.base_api_url + "login", headers=self.headers)
+        logging.info("thetvdbv2_login Info Status: %s-%s", resp.status_code, resp.json())
+        return resp.json()
+
+
+    def com_meta_thetvdbv2_language(self):
+        resp = requests.post(self.base_api_url + "languages", headers=self.headers)
+        logging.info("thetvdbv2_lang Info Status: %s-%s", resp.status_code, resp.json())
+        return resp.json()
+
+
+
