cf18ccc162c9c228ed34ae2b529bc51abb75341e
SpootDev <spootdev@gmail.com>
2017-06-16 20:15:23 -0500

    api test

--- test_db.py
+++ test_db.py
@@ -22,12 +22,20 @@ psycopg2.extensions.register_type(psycopg2.extensions.UNICODEARRAY)
 db_connection = database_base.MKServerDatabase()
 db_connection.db_open(True)
 
-data_episode_count = db_connection.db_read_tvmeta_season_eps_list('a7ad882b-372a-4e46-bb77-26c09681ea01', 2)
-print('dataeps: %s', data_episode_count)
+# data_episode_count = db_connection.db_read_tvmeta_season_eps_list('a7ad882b-372a-4e46-bb77-26c09681ea01', 2)
+# print('dataeps: %s', data_episode_count)
+#
+# key_list = natsort.natsorted(data_episode_count)
+#
+# for key_data in key_list:
+#     print('key: %s', data_episode_count[key_data])
 
-key_list = natsort.natsorted(data_episode_count)
+from common import common_metadata_thetvdb_v2
 
-for key_data in key_list:
-    print('key: %s', data_episode_count[key_data])
+options, status = db_connection.db_opt_status_read()
+print(options)
+thetvdb_connection = common_metadata_thetvdb_v2.CommonMetadataTheTVDBv2(options)
+
+print(thetvdb_connection.com_meta_thetvdbv2_login())
 
 db_connection.db_close()
