a0aa4645b6658607d16f1388a3c81a89a092b146
SpootDev <spootdev@gmail.com>
2017-07-03 18:32:38 -0500

    remove logs

--- database/db_base_metadata_movie.py
+++ database/db_base_metadata_movie.py
@@ -46,23 +46,16 @@ def db_meta_movie_status_update(self, metadata_guid, user_id, status_text, statu
     """
     self.db_cursor.execute('SELECT mm_metadata_user_json from mm_metadata_movie'
                            ' where mm_metadata_guid = %s FOR UPDATE', (metadata_guid,))
-    logging.info('stat6')
     try:
-        logging.info('stat5')
         json_data = self.db_cursor.fetchone()['mm_metadata_user_json']
-        logging.info('stat4: %s', json_data)
         if json_data is None or 'UserStats' not in json_data:
-            logging.info('stat7')
             json_data = {}
             json_data['UserStats'] = {}
-        logging.info('stat: %s', json_data)
         if user_id in json_data['UserStats']:
             json_data['UserStats'][user_id][status_text] = status_bool
         else:
             json_data['UserStats'][user_id] = {status_text: status_bool}
-        logging.info('stat2: %s', json_data)
         self.db_meta_movie_json_update(metadata_guid, json.dumps(json_data))
-        logging.info('stat3: %s', json_data)
         #self.db_commit() - since done in update below
     except:
         self.db_rollback()
