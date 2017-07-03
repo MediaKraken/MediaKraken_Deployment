e4bbb93d6e599e2bb7d1013045d45108726140e7
SpootDev <spootdev@gmail.com>
2017-07-02 22:33:40 -0500

    remove extra $s in logging

--- web_app/MediaKraken/user/views_movie.py
+++ web_app/MediaKraken/user/views_movie.py
@@ -307,7 +307,7 @@ def metadata_movie_list():
                 = row_data['mm_metadata_user_json']['UserStats'][current_user.get_id()]['favorite']
         except:
             favorite_status = False
-        logging.info("status: %s %s %s %s %s", watched_status, poo_status, favorite_status)
+        logging.info("status: %s %s %s", watched_status, poo_status, favorite_status)
         media.append((row_data['mm_media_name'], row_data['mm_metadata_guid'], row_data['mm_date'],
                       row_data['mm_poster'], watched_status, poo_status, favorite_status))
 
