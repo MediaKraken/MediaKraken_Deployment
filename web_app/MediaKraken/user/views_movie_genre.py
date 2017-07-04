814428ef41a0cc23a147790dfea934b0e6966fad
SpootDev <spootdev@gmail.com>
2017-07-04 11:49:31 -0500

    use right json

--- web_app/MediaKraken/user/views_movie_genre.py
+++ web_app/MediaKraken/user/views_movie_genre.py
@@ -68,10 +68,10 @@ def user_movie_page(genre):
         except:
             sync_status = False
         # set rating
-        if 'UserStats' in row_data['mm_metadata_user_json']\
-            and current_user.get_id() in row_data['mm_metadata_user_json']['UserStats']\
-            and ['Rating'] in row_data['mm_metadata_user_json']['UserStats'][current_user.get_id()]:
-                rating_status = row_data['mm_metadata_user_json']['UserStats'][current_user.get_id()]['Rating']
+        if 'UserStats' in row_data['mm_media_json']\
+            and current_user.get_id() in row_data['mm_media_json']['UserStats']\
+            and ['Rating'] in row_data['mm_media_json']['UserStats'][current_user.get_id()]:
+                rating_status = row_data['mm_media_json']['UserStats'][current_user.get_id()]['Rating']
                 if rating_status == 'favorite':
                     rating_status = '/static/images/favorite-mark.png'
                 elif rating_status == 'like':
