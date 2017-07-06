8f60a7caedfbb3c4b1c907da22cdd67f82ee45ca
SpootDev <spootdev@gmail.com>
2017-07-05 23:04:43 -0500

    sends base64 encoded image

--- kivy_test/chat_client.py
+++ kivy_test/chat_client.py
@@ -1,22 +1,33 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+import base64
 from twisted.internet import reactor, stdio, protocol
 from twisted.protocols import basic
 from twisted.internet import ssl
 
+
 class Echo(basic.LineReceiver):
+    MAX_LENGTH = 32000000  # pylint: disable=C0103
+
     def connectionMade(self):
-        print "Welcome to the Chat, you have now connected"
+        print("Welcome to the Chat, you have now connected")
 
         # send some text when we connect
-        self.sendLine('hello')
+        self.sendLine('hello'.encode("utf8"))
 
     def lineReceived(self, line):
-        print 'client received:', line
+        print('client received:', line)
+
+        iconfile = open("theater/images/diskette.png", "rb")
+        icondata = iconfile.read()
+        icondata = base64.b64encode(icondata)
+        #print(icondata)
+        self.sendLine(icondata.encode("utf8"))
 
         if len(line) > 10:
-            self.sendLine('exit')
+            self.sendLine('exit'.encode("utf8"))
         else:
             # send a response in 2 seconds
-            reactor.callLater(2, self.sendLine, '>' + line)
+            reactor.callLater(2, self.sendLine, ('>' + line).encode("utf8"))
 
     def connectionLost(self, reason):
         reactor.stop()
--- kivy_test/subprogram_reactor_line.py
+++ kivy_test/subprogram_reactor_line.py
@@ -1,25 +1,30 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+import base64
 from twisted.internet import reactor, protocol, stdio
 from twisted.protocols import basic
 from twisted.internet import ssl
 
+
 class Echo(basic.LineReceiver):
-    print "Welcome to Chat"
+    print("Welcome to Chat")
+    MAX_LENGTH = 32000000  # pylint: disable=C0103
 
     def connectionMade(self):
-        print "A new client has connected"
+        print("A new client has connected")
 
     def lineReceived(self, line):
-        print 'server received:', line
-        print 'server sent:', line, '\n'
-        self.sendLine(line)
+        print('server received:', line)
+        print('server sent:', line, '\n')
+        self.sendLine(line.encode("utf8"))
         if line=="exit":
              self.transport.loseConnection()
+
 
 class EchoServerFactory(protocol.ServerFactory):
    protocol = Echo
 
+
 factory = EchoServerFactory()
 reactor.listenSSL(7500, factory, ssl.DefaultOpenSSLContextFactory('./key/privkey.pem', './key/cacert.pem'))
-#reactor.listenTCP(9999, factory)
 reactor.run()
 
