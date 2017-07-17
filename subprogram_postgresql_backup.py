58cc2fd63572e58a34c815f792ee15d65d3fd179
SpootDev <spootdev@gmail.com>
2017-07-16 23:07:11 -0500

    pg_dump host

--- subprogram_postgresql_backup.py
+++ subprogram_postgresql_backup.py
@@ -46,8 +46,9 @@ db_connection.db_activity_insert('MediaKraken_Server Postgresql Backup Start', N
 # generate dump file
 backup_file_name = 'MediaKraken_Backup_' + time.strftime("%Y%m%d%H%M%S") + '.dump'
 
-os.system('PGPASSWORD=' + os.environ['POSTGRES_PASSWORD'] + ' pg_dump -U '
-          + os.environ['POSTGRES_USER'] + ' '
+os.system('PGPASSWORD=' + os.environ['POSTGRES_PASSWORD']
+          + ' pg_dump -h ' + os.environ['POSTGRES_DB_HOST']
+          + ' -U ' + os.environ['POSTGRES_USER'] + ' '
           + os.environ['POSTGRES_DB'] + ' -F c -f '
           + os.path.join('/mediakraken/backup', backup_file_name))
 
