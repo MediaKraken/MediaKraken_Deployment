5c6c9851a583a9432b17bb24fb77a2852ec2ad4c
SpootDev <spootdev@gmail.com>
2017-07-23 11:54:21 -0500

    nvidia cuda debian

--- docker/alpine/build_update.sh
+++ docker/alpine/build_update.sh
@@ -6,6 +6,9 @@ docker-compose build
 cd ComposeMediaKrakenNvidia
 docker build -t mediakraken/mkbasenvidia .
 
+cd ComposeMediaKrakenNvidiaDebian
+docker build -t mediakraken/mkbasenvidiadebain .
+
 cd ../ComposeMediaKrakenBaseFFMPEG
 docker build -t mediakraken/mkbaseffmpeg .
 
