18814b5cc0d6b77d6c81b76512fef03cac9a3a0a
SpootDev <spootdev@gmail.com>
2017-07-23 16:35:02 -0500

    build debian cuda tree

--- docker/alpine/build_update.sh
+++ docker/alpine/build_update.sh
@@ -3,22 +3,31 @@ git pull
 docker-compose down
 docker-compose build
 
+# Build the base Nvidia Cuda
 cd ComposeMediaKrakenNvidia
-docker build -t mediakraken/mkbasenvidia .
+#docker build -t mediakraken/mkbasenvidia .
 
-#cd ../ComposeMediaKrakenNvidiaDebian
-#docker build -t mediakraken/mkbasenvidiadebain .
+cd ../ComposeMediaKrakenNvidiaDebian
+docker build -t mediakraken/mkbasenvidiadebian .
 
+# Build the base FFMPEG from base images
 cd ../ComposeMediaKrakenBaseFFMPEG
 docker build -t mediakraken/mkbaseffmpeg .
 
-cd ../ComposeMediaKrakenBaseFFMPEGNvidia
-docker build -t mediakraken/mkbaseffmpegnvidia .
+#cd ../ComposeMediaKrakenBaseFFMPEGNvidia
+#docker build -t mediakraken/mkbaseffmpegnvidia .
 
+cd ../ComposeMediaKrakenBaseFFMPEGNvidiaDebian
+docker build -t mediakraken/mkbaseffmpegnvidiadebian .
+
+# Build the base slave images from other base images
 cd ../ComposeMediaKrakenSlave
 docker build -t mediakraken/mkslave .
 
-cd ../ComposeMediaKrakenSlaveNvidia
-docker build -t mediakraken/mkslavenvidia .
+#cd ../ComposeMediaKrakenSlaveNvidia
+#docker build -t mediakraken/mkslavenvidia .
+
+cd ../ComposeMediaKrakenSlaveNvidiaDebian
+docker build -t mediakraken/mkslavenvidiadebian .
 
 
