fbe50cea928731241aada830213875c09a710209
SpootDev <spootdev@gmail.com>
2017-07-25 19:00:35 -0500

    base tvtuner edit

--- web_app/MediaKraken/admins/forms.py
+++ web_app/MediaKraken/admins/forms.py
@@ -244,3 +244,23 @@ class ChromecastEditForm(Form):
         if not initial_validation:
             return False
         return True
+
+
+class TVTunerEditForm(Form):
+    """
+    for editing the tvtuner devices
+    """
+    name = TextField('Name', validators=[DataRequired()])
+    ipaddr = TextField('IP Address', validators=[DataRequired()])
+    enabled = BooleanField('Enabled')
+
+
+    def __init__(self, *args, **kwargs):
+        super(TVTunerEditForm, self).__init__(*args, **kwargs)
+
+
+    def validate(self):
+        initial_validation = super(TVTunerEditForm, self).validate()
+        if not initial_validation:
+            return False
+        return True
