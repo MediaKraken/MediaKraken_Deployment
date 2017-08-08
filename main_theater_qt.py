5a415ad20378de145ff3a54ca99329a66ee3b3b4
SpootDev <spootdev@gmail.com>
2017-08-08 14:33:13 -0500

    base qt

---
+++ main_theater_qt.py
@@ -0,0 +1,40 @@
+'''
+  Copyright (C) 2017 Quinn D Granfor <spootdev@gmail.com>
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  version 2, as published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  General Public License version 2 for more details.
+
+  You should have received a copy of the GNU General Public License
+  version 2 along with this program; if not, write to the Free
+  Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+  MA 02110-1301, USA.
+'''
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+import logging # pylint: disable=W0611
+import sys
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import QGridLayout, QLabel, QLineEdit
+from PyQt5.QtWidgets import QTextEdit, QWidget, QDialog, QApplication
+sys.path.append('theater_qt')
+from ui import mk_login_ui
+from ui import mk_mainwindow_ui
+from ui import mk_player_ui
+
+class MainWindow(QDialog, mk_mainwindow_ui.Ui_MK_MainWindow):
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+        self.setupUi(self)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    form = MainWindow()
+    form.show()
+    sys.exit(app.exec_())
