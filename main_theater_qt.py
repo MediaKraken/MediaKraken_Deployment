3cebb51b37323778a154d25dd8e0d352fa7e8ff9
SpootDev <spootdev@gmail.com>
2017-08-08 18:56:17 -0500

    main so centralwidget

--- main_theater_qt.py
+++ main_theater_qt.py
@@ -31,7 +31,7 @@ from ui import mk_mainwindow_ui
 from ui import mk_player_ui
 
 
-class MainWindow(QDialog, mk_mainwindow_ui.Ui_MK_MainWindow):
+class MainWindow(QMainWindow, mk_mainwindow_ui.Ui_MK_MainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
         self.setupUi(self)
