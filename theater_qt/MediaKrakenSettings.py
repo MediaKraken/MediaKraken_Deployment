292622287aee0aacb415b71cc8b32eb4cec117ec
SpootDev <spootdev@gmail.com>
2017-08-09 18:54:54 -0500

    json the rest of data

--- theater_qt/MediaKrakenSettings.py
+++ theater_qt/MediaKrakenSettings.py
@@ -33,66 +33,13 @@ mediakraken_settings_base_json =\
     'Receiver supports Dolby Atmos passthrough': False},
     'Video': {'Blank Displays': ['Blank other displays in multimonitor environment',\
                                  'Run application fullscreen for GUI and playback',\
-                                 'Run application in windowed fullscreen mode']}}
-
-'''
-
-
-    {'type': 'options',
-     'title': 'Resolution',
-     'desc': 'Set resolution for GUI and playback',
-     'section': 'Video',
-     'key': 'Resolution',
-     'options': ['Window Size', 'Screen Size', '(4k) 3840 x 2160', '1920x1080p', '1920x1080i',
-         '1280x720p', '1280x720i']},
-    {'type': 'options',
-     'title': 'Main Display',
-     'desc': 'Select main screen for GUI and playback',
-     'section': 'Video',
-     'key': 'Display_Screen',
-     'options': ['Display #1', 'Display #2', 'Display #3', 'Display #4']},
-    {'type': 'options',
-     'title': 'Sleep Mode',
-     'desc': 'Select timer for entering sleep mode',
-     'section': 'Video',
-     'key': 'Sleep_Time',
-     'options': ['Never', '3 min', '10 min', '15 min', '30 min', '1 hour', '3 hour']}])
-
-# library settings
-mediakraken_settings_library_json = json.dumps([
-    {'type': 'title',
-     'title': 'Library Settings'},
-    {'type': 'bool',
-     'title': 'Show Plot of Unwatched',
-     'desc': 'Show the plot of unwatched media',
-     'section': 'Library',
-     'key': 'Show_Plot_Unwatched'},
-    {'type': 'options',
-     'title': 'Flatten TV Show Seasons',
-     'desc': 'Whether to display season(s) in directories',
-     'section': 'Library',
-     'key': 'Flatten_TV_Show_Seasons',
-     'options': ['If Only One Season', 'Always', 'Never']},
-    {'type': 'bool',
-     'title': 'Group Movies',
-     'desc': 'Group media into known collections',
-     'section': 'Library',
-     'key': 'Group_Movies_in_Sets'}])
-
-# playback settings
-mediakraken_settings_playback_json = json.dumps([
-    {'type': 'title',
-     'title': 'Media Playback Settings'},
-    {'type': 'options',
-     'title': 'Preferred audio stream',
-     'desc': 'Set language for preferred audio stream',
-     'section': 'Playback',
-     'key': 'Preferred_Audio_Language',
-     'options': ['Original Stream Language', 'English']},
-    {'type': 'bool',
-     'title': 'Play next media',
-     'desc': 'Automatically start next media file after end of media',
-     'section': 'Playback',
-     'key': 'Play_Next_Media_Automatically'}])
-
-'''
+                                 'Run application in windowed fullscreen mode'],
+              'Resolution': ['Window Size', 'Screen Size', '(4k) 3840 x 2160', '1920x1080p',
+                             '1920x1080i', '1280x720p', '1280x720i'],
+              'Main Display': ['Display #1', 'Display #2', 'Display #3', 'Display #4']},
+    'Sleep Mode': ['Never', '3 min', '10 min', '15 min', '30 min', '1 hour', '3 hour'],
+    'Library': {'Show Plot of Unwatched': False,
+                'Flatten TV Show Seasons': ['If Only One Season', 'Always', 'Never'],
+                'Group Movies': False},
+    'Playback': {'Preferred Audio': ['Original Stream Language', 'English'],
+                 'Play Next Media': False}}
