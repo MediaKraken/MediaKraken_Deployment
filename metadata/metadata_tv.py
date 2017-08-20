6dd1858a7a48d30992dfd571648c88248ee4f3ab
SpootDev <spootdev@gmail.com>
2017-08-19 22:15:42 -0500

    redo result check

--- metadata/metadata_tv.py
+++ metadata/metadata_tv.py
@@ -167,12 +167,12 @@ def tv_fetch_save_tvmaze(db_connection, tvmaze_id):
     """
     logging.info("meta tv tvmaze save fetch: %s", tvmaze_id)
     metadata_uuid = None
-    show_full_json = ({'Meta': {'tvmaze':
-        TVMAZE_CONNECTION.com_meta_tvmaze_show_by_id(
+    result_json = TVMAZE_CONNECTION.com_meta_tvmaze_show_by_id(
         tvmaze_id, tvrage_id=None, imdb_id=None, tvdb_id=None,
-        embed_info=True)}})
-    logging.info("tvmaze full: %s", show_full_json)
-    if show_full_json is not None:
+        embed_info=True)
+    show_full_json = ({'Meta': {'tvmaze': result_json}})
+    logging.info("tvmaze full: %s", result_json)
+    if result_json is not None:
         show_detail = show_full_json['Meta']['tvmaze']
         logging.info("detail: %s", show_detail)
         tvmaze_name = show_detail['name']
