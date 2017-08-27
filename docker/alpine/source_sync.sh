2a8cb6bc59797129825ba996870749e96cb5500d
SpootDev <spootdev@gmail.com>
2017-08-27 09:26:16 -0500

    remove trigger

--- docker/alpine/source_sync.sh
+++ docker/alpine/source_sync.sh
@@ -26,7 +26,6 @@ cp /home/spoot/MediaKraken_Deployment/db_create_update.py /home/spoot/MediaKrake
 cp /home/spoot/MediaKraken_Deployment/db_update_version.py /home/spoot/MediaKraken_Deployment/docker/alpine/ComposeMediaKrakenServer/src/.
 cp /home/spoot/MediaKraken_Deployment/main_server.py /home/spoot/MediaKraken_Deployment/docker/alpine/ComposeMediaKrakenServer/src/.
 cp /home/spoot/MediaKraken_Deployment/main_server_link.py /home/spoot/MediaKraken_Deployment/docker/alpine/ComposeMediaKrakenServer/src/.
-cp /home/spoot/MediaKraken_Deployment/main_server_trigger.py /home/spoot/MediaKraken_Deployment/docker/alpine/ComposeMediaKrakenServer/src/.
 cp /home/spoot/MediaKraken_Deployment/subprogram*.py  /home/spoot/MediaKraken_Deployment/docker/alpine/ComposeMediaKrakenServer/src/.
 
 # slave
