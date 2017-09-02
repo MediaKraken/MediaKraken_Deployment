c2487b2ea5c45d0bb365a601cc1497f2dbbc2424
SpootDev <spootdev@gmail.com>
2017-09-02 12:52:43 -0500

    sdl2-dev

--- theater/mame_build_alpine.sh
+++ theater/mame_build_alpine.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apk add sdl-dev fontconfig-dev alsa-lib-dev qt5-qtbase-dev
+apk add sdl2-dev fontconfig-dev alsa-lib-dev qt5-qtbase-dev
 wget https://github.com/mamedev/mame/releases/download/mame0189/mame0189s.zip
 unzip mame0189s.zip
 mkdir tmp
