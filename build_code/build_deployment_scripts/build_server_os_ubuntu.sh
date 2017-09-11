fde100f744a9b12fa9bd470f0f8b3b7597e4f3f9
SpootDev <spootdev@gmail.com>
2017-09-10 19:56:25 -0500

    not needed

--- build_code/build_deployment_scripts/build_server_os_ubuntu.sh
+++
@@ -1,3 +0,0 @@
-# don't think I need to do the following for ubuntu
-sudo cp /usr/lib/uwsgi/plugins/python_plugin.so /home/metaman/github/MediaKraken_Deployment/.
-sudo chmod +x /home/metaman/github/MediaKraken_Deployment/python_plugin.so
