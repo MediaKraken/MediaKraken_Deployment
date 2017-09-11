2f5e5edaef95f2fb4d2c82a78415e0edebb3413f
SpootDev <spootdev@gmail.com>
2017-09-10 20:30:08 -0500

    move cp and paths along with -j8

--- build/alpine_build_theater_os.sh
+++ build/alpine_build_theater_os.sh
@@ -2,34 +2,34 @@ sudo apt-get --assume-yes install build-essential &&
 dpkg-reconfigure dash &&
 sudo apt-get --assume-yes install libxml-parser-perl open-vm-tools libncurses5-dev gperf makeinfo md5deep &&
 
-git clone https://github.com/OpenELEC/OpenELEC.tv.git
+git clone https://github.com/LibreELEC/LibreELEC.tv.git
 
-cd OpenELEC.tv
+# copy over the mediakraken distro settings
+cp -R ./OpenMediaKraken/distributions/MediaKraken ./LibreELEC.tv/distributions/.
 
-# copy over the metaman distro settings
-cp -R ../OpenMediaKraken/distributions/MediaKraken ./distributions/.
+cd LibreELEC.tv
 
 # allow it to install packages
-PROJECT=Generic DISTRO=MediaKraken ARCH=x86_64 make release
-PROJECT=Generic DISTRO=MediaKraken ARCH=x86_64 make image
+PROJECT=Generic DISTRO=MediaKraken ARCH=x86_64 make -j8 release
+PROJECT=Generic DISTRO=MediaKraken ARCH=x86_64 make -j8 image
 
 # general rpi install
-PROJECT=RPi DISTRO=MediaKraken ARCH=arm make release
-PROJECT=RPi DISTRO=MediaKraken ARCH=arm make image
+PROJECT=RPi DISTRO=MediaKraken ARCH=arm make -j8 release
+PROJECT=RPi DISTRO=MediaKraken ARCH=arm make -j8 image
 
 # general rpi2 install
-PROJECT=RPi2 DISTRO=MediaKraken ARCH=arm make release
-PROJECT=RPi2 DISTRO=MediaKraken ARCH=arm make image
+PROJECT=RPi2 DISTRO=MediaKraken ARCH=arm make -j8 release
+PROJECT=RPi2 DISTRO=MediaKraken ARCH=arm make -j8 image
 
 # general x64 install
-PROJECT=Generic ARCH=x86_64 make release
-PROJECT=Generic ARCH=x86_64 make image
+PROJECT=Generic ARCH=x86_64 make -j8 release
+PROJECT=Generic ARCH=x86_64 make -j8 image
 
 # general rpi install
-PROJECT=RPi ARCH=arm make release
-PROJECT=RPi ARCH=arm make image
+PROJECT=RPi ARCH=arm make -j8 release
+PROJECT=RPi ARCH=arm make -j8 image
 
 # general rpi2 install
-PROJECT=RPi2 ARCH=arm make release
-PROJECT=RPi2 ARCH=arm make image
+PROJECT=RPi2 ARCH=arm make -j8 release
+PROJECT=RPi2 ARCH=arm make -j8 image
 
