a4b4481a25d9d145383fdea82e474324ba3f72ff
SpootDev <spootdev@gmail.com>
2017-09-12 19:02:48 -0500

    version

--- build/theater_os_deploy_pi_sd_(ubuntu).sh
+++ build/theater_os_deploy_pi_sd_(ubuntu).sh
@@ -2,7 +2,7 @@
 DRIVE_ID=/dev/sdb
 DRIVE_BOOT=$DRIVE_ID + '1'
 DRIVE_SYS=$DRIVE_ID + '2'
-CODEVERSION=7.0.3
+CODEVERSION=8.1.1
 
 # setup the partitions on DRIVE_ID
 # THIS WILL WIPE OUT DRIVE_ID!!!!!!!!!!
@@ -16,13 +16,13 @@ sudo mkfs.ext4 -L Storage $DRIVE_SYS
 sudo partprobe
 
 # copy bootloader files
-sudo cp LibreELEC.tv-$CODEVERSION/build.MediaKrakenCL-RPi.arm-devel/bcm2835-bootloader-*/arm128_start.elf /media/System/start.elf
-sudo cp LibreELEC.tv-$CODEVERSION/build.MediaKrakenCL-RPi.arm-devel/bcm2835-bootloader-*/bootcode.bin /media/System/
-sudo cp LibreELEC.tv-$CODEVERSION/build.MediaKrakenCL-RPi.arm-devel/bcm2835-bootloader-*/loader.bin /media/System/
+sudo cp LibreELEC.tv-$CODEVERSION/build.MediaKraken-RPi.arm-$CODEVERSION/bcm2835-bootloader-*/start.elf /media/System/start.elf
+sudo cp LibreELEC.tv-$CODEVERSION/build.MediaKraken-RPi.arm-$CODEVERSION/bcm2835-bootloader-*/bootcode.bin /media/System/
+#sudo cp LibreELEC.tv-$CODEVERSION/build.MediaKraken-RPi.arm-$CODEVERSION/bcm2835-bootloader-*/loader.bin /media/System/
 
 # copy system files
-sudo cp LibreELEC.tv-$CODEVERSION/target/MediaKrakenCL-RPi.arm-6.0-devel-20150929144729-r21337-geb0ed51.system /media/System/SYSTEM
-sudo cp LibreELEC.tv-$CODEVERSION/target/MediaKrakenCL-RPi.arm-6.0-devel-20150929144729-r21337-geb0ed51.kernel /media/System/kernel.img
+sudo cp LibreELEC.tv-$CODEVERSION/target/MediaKraken-RPi.arm-$CODEVERSION.system /media/System/SYSTEM
+sudo cp LibreELEC.tv-$CODEVERSION/target/MediaKraken-RPi.arm-$CODEVERSION.kernel /media/System/kernel.img
 
 # set boot options
 echo "boot=/dev/mmcblk0p1 disk=/dev/mmcblk0p2 ssh quiet" | sudo tee /media/System/cmdline.txt
