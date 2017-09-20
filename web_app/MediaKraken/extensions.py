2c5e3fce07ba15eba395ddd9e42047ea62d35269
SpootDev <spootdev@gmail.com>
2017-09-20 14:09:00 -0500

    fs cache

--- web_app/MediaKraken/extensions.py
+++ web_app/MediaKraken/extensions.py
@@ -17,7 +17,7 @@ from flask_migrate import Migrate
 migrate = Migrate()
 
 from flask_caching import Cache
-cache = Cache(config={'CACHE_TYPE': 'simple'})
+cache = Cache(config={'CACHE_TYPE': 'filesystem'})
 
 from flask_debugtoolbar import DebugToolbarExtension
 debug_toolbar = DebugToolbarExtension()
