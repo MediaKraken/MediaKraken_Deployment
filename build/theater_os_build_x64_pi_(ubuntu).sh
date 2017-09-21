88b5db5a70746565fc79c1c66f4aa1d3d5717c1c
SpootDev <spootdev@gmail.com>
2017-09-20 20:22:29 -0500

    just do x64 for now

--- build/theater_os_build_x64_pi_(ubuntu).sh
+++ build/theater_os_build_x64_pi_(ubuntu).sh
@@ -6,10 +6,6 @@ wget https://github.com/LibreELEC/LibreELEC.tv/archive/$CODEVERSION.zip
 unzip $CODEVERSION.zip
 cd LibreELEC.tv-$CODEVERSION
 
-#git clone https://github.com/LibreELEC/LibreELEC.tv.git
-#cd LibreELEC.tv
-#git checkout libreelec-8.2
-
 # copy over the mediakraken distro settings
 cp -R ../OpenMediaKraken/distributions/MediaKraken ./distributions/.
 cp -R ../OpenMediaKraken/packages/* ./packages/.
@@ -19,9 +15,9 @@ PROJECT=Generic DISTRO=MediaKraken ARCH=x86_64 make -j20 release
 PROJECT=Generic DISTRO=MediaKraken ARCH=x86_64 make -j20 image
 
 # general rpi and pi zero install
-PROJECT=RPi DISTRO=MediaKraken ARCH=arm make -j20 release
-PROJECT=RPi DISTRO=MediaKraken ARCH=arm make -j20 image
+#PROJECT=RPi DISTRO=MediaKraken ARCH=arm make -j20 release
+#PROJECT=RPi DISTRO=MediaKraken ARCH=arm make -j20 image
 
 # general rpi2 install
-PROJECT=RPi2 DISTRO=MediaKraken ARCH=arm make -j20 release
-PROJECT=RPi2 DISTRO=MediaKraken ARCH=arm make -j20 image
+#PROJECT=RPi2 DISTRO=MediaKraken ARCH=arm make -j20 release
+#PROJECT=RPi2 DISTRO=MediaKraken ARCH=arm make -j20 image
