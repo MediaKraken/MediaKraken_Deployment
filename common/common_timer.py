a7b538001fa80193ec0b1d5b0cdb7eb6787288e3
SpootDev <spootdev@gmail.com>
2017-09-30 15:41:21 -0500

    add timer fucntion

---
+++ common/common_timer.py
@@ -0,0 +1,50 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+import logging # pylint: disable=W0611
+from threading import Timer
+import time
+
+
+# https://stackoverflow.com/users/7370877/bill-schumacher
+class InfiniteTimer():
+    """A Timer class that does not stop, unless you want it to."""
+
+    def __init__(self, seconds, target):
+        self._should_continue = False
+        self.is_running = False
+        self.seconds = seconds
+        self.target = target
+        self.thread = None
+
+    def _handle_target(self):
+        self.is_running = True
+        self.target()
+        self.is_running = False
+        self._start_timer()
+
+    def _start_timer(self):
+        if self._should_continue: # Code could have been running when cancel was called.
+            self.thread = Timer(self.seconds, self._handle_target)
+            self.thread.start()
+
+    def start(self):
+        if not self._should_continue and not self.is_running:
+            self._should_continue = True
+            self._start_timer()
+        else:
+            print("Timer already started or running, please wait if you're restarting.")
+
+    def cancel(self):
+        if self.thread is not None:
+            self._should_continue = False # Just in case thread is running and cancel fails.
+            self.thread.cancel()
+        else:
+            print("Timer never started or failed to initialize.")
+
+
+# def tick():
+#     print('ipsem lorem')
+#
+#
+# # Example Usage
+# t = InfiniteTimer(0.5, tick)
+# t.start()
