74c2ff39487f20d9f05aa230c321657c39d5d524
SpootDev <spootdev@gmail.com>
2017-10-15 15:51:48 -0500

    get

--- test.py
+++ test.py
@@ -1,20 +1,28 @@
-stuff = 'where mm_media_name %% %s order by mm_media_name offset %s limit %s)' % ('table', 1, 3)
-
-print(stuff)
-
-
-stuff = 'hash/jfslhtr/jhytr'
-
-print stuff[0:5]
-
-print stuff.split('/',1)[1]
-
-print stuff[-1:]
-
+# stuff = 'where mm_media_name %% %s order by mm_media_name offset %s limit %s)' % ('table', 1, 3)
+#
+# print(stuff)
+#
+#
+# stuff = 'hash/jfslhtr/jhytr'
+#
+# print stuff[0:5]
+#
+# print stuff.split('/',1)[1]
+#
+# print stuff[-1:]
+import time
 from common import common_lirc
 
 lirc = common_lirc.CommonLIRC(None)
 
 lirc.com_lirc_init()
+
+
+code = ""
+while(code != "quit"):
+    print(lirc.com_lirc_nextcode())
+    # Delay...
+    time.sleep(1)
+
 
 lirc.com_lirc_close()
