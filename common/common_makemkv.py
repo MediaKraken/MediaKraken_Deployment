70ebc21e7cadf862fbc17b48058c4ee7fbf45b6b
SpootDev <spootdev@gmail.com>
2017-11-15 20:28:49 -0600

    cleanup

--- common/common_makemkv.py
+++ common/common_makemkv.py
@@ -20,61 +20,6 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import logging # pylint: disable=W0611
 import subprocess
 
-
-# info on inserted disk in drive 0
-# makemkvcon info disc:0
-
-'''
-Operation successfully completed
-Total 4 titles
-Title  0
-0 Video Mpeg2
-1 Audio Dolby Digital
-2 Audio Dolby Digital
-3 Audio Dolby Digital
-4 Audio Dolby Digital
-5 Subtitles Dvd Subtitles
-6 Subtitles Dvd Subtitles
-7 Subtitles Dvd Subtitles
-8 Subtitles Dvd Subtitles
-9 Subtitles Dvd Subtitles
-
-Title  1
-0 Video Mpeg2
-1 Audio Dolby Digital
-2 Subtitles Dvd Subtitles
-3 Subtitles Dvd Subtitles
-4 Subtitles Dvd Subtitles
-
-Title  2
-0 Video Mpeg2
-1 Audio Dolby Digital
-2 Audio Dolby Digital
-3 Audio Dolby Digital
-4 Subtitles Dvd Subtitles
-5 Subtitles Dvd Subtitles
-
-Title  3
-0 Video Mpeg2
-1 Audio Dolby Digital
-2 Audio Dolby Digital
-3 Audio Dolby Digital
-4 Subtitles Dvd Subtitles
-5 Subtitles Dvd Subtitles
-'''
-
-# title 0 to path
-# makemkvcon mkv disc:0 0 .
-# makemkvcon --progress=-same --cache=128 mkv disc:0 0 .
-## Current action: Saving to MKV file
-# makemkvcon mkv disc:0 all c:\folder
-
-'''
-Current progress - 100%  , Total progress - 100%
-1 titles saved
-Copy complete. 1 titles saved.
-'''
-
 # https://github.com/cmlburnett/PyBluRead/blob/master/setup.py
-
 # sudo apt-get install libbluray-dev
+
