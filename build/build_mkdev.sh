58a9a1146cf8f9c8a63b970f80a85c68196190d8
spootdev <spootdev@gmail.com>
2018-01-13 19:11:01 -0600

    build cleanup for mkdev

---
+++ build/build_mkdev.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+apk update
+apk upgrade
+apk install alpine-sdk
+
+pip install -r ../testing/pip_requirements.txt
+pip install -r ../docker/alpine/ComposeMediaKrakenWebServer/requirements.txt
+pip install -r ../docker/alpine/ComposeMediaKrakenServer/requirements.txt
+
+./theater_client_build_alpine.sh
+
+
