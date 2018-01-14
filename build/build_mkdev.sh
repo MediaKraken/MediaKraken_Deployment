86a33791e292b9cfe25299b545184e732f7d9c18
SpootDev <spootdev@gmail.com>
2018-01-13 19:47:24 -0600

    more deps

--- build/build_mkdev.sh
+++ build/build_mkdev.sh
@@ -1,8 +1,10 @@
+#@IgnoreInspection BashAddShebang
 apk update
 apk upgrade
-apk add alpine-sdk
+apk add alpine-sdk linux-headers postgresql-dev python-dev libffi-dev
 
 pip install -r ../testing/pip_requirements.txt
+pip install -r ../docker/alpine/ComposeMediaKrakenMetadata/requirements.txt
 pip install -r ../docker/alpine/ComposeMediaKrakenWebServer/requirements.txt
 pip install -r ../docker/alpine/ComposeMediaKrakenServer/requirements.txt
 
