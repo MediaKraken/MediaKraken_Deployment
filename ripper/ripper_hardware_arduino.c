9e1c13869bb80690b5bfd54cb815a4a5ea07982b
spootdev <spootdev@gmail.com>
2018-01-14 18:58:13 -0600

    sketch format

--- ripper/ripper_hardware_arduino.c
+++ ripper/ripper_hardware_arduino.c
@@ -19,16 +19,15 @@
 #include <AccelStepper.h>
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
-#include "utility/Adafruit_PWMServoDriver.h"
 #include <SoftwareSerial.h>
 
-const int water_pin 5;
-const int vaccuum_pin 6;
+#define water_pin 5
+#define vaccuum_pin 6
 #define home_switch_spindle 7 // Pin 7 connected to Home Switch (MicroSwitch)
 #define home_switch_arm 8 // Pin 8 connected to Home Switch (MicroSwitch)
 #define home_switch_track 9 // Pin 9 connected to Home Switch (MicroSwitch)
-long initial_arm_homing=-1;  // Used to Home Stepper at startup
-long initial_track_homing=-1;  // Used to Home Stepper at startup
+long initial_arm_homing = -1; // Used to Home Stepper at startup
+long initial_track_homing = -1; // Used to Home Stepper at startup
 char serialdata[20];
 
 // the track/arm motor shield
@@ -49,98 +48,99 @@ AccelStepper Accelstepper_arm(stepper_arm->onestep(FORWARD, MICROSTEP), stepper_
 
 void arm_home()
 {
-    // home the arm
-    while (digitalRead(home_switch_arm)) {  // Make the Stepper move CCW until the switch is activated
-        Accelstepper_arm.moveTo(initial_arm_homing);  // Set the position to move to
-        initial_arm_homing--;  // Decrease by 1 for next move if needed
-        Accelstepper_arm.run();  // Start moving the stepper
-        delay(5);
-    }
-    Accelstepper_arm.setCurrentPosition(0);  // Set the current position as zero for now
-    Accelstepper_arm.setMaxSpeed(100.0);      // Set Max Speed of Stepper (Slower to get better accuracy)
-    Accelstepper_arm.setAcceleration(100.0);  // Set Acceleration of Stepper
-    initial_arm_homing=1;
-    while (!digitalRead(home_switch_arm)) { // Make the Stepper move CW until the switch is deactivated
-        Accelstepper_arm.moveTo(initial_arm_homing);
-        Accelstepper_arm.run();
-        initial_arm_homing++;
-        delay(5);
-    }
-    Accelstepper_arm.setCurrentPosition(0);
+  // home the arm
+  while (digitalRead(home_switch_arm)) {  // Make the Stepper move CCW until the switch is activated
+    Accelstepper_arm.moveTo(initial_arm_homing);  // Set the position to move to
+    initial_arm_homing--;  // Decrease by 1 for next move if needed
+    Accelstepper_arm.run();  // Start moving the stepper
+    delay(5);
+  }
+  Accelstepper_arm.setCurrentPosition(0);  // Set the current position as zero for now
+  Accelstepper_arm.setMaxSpeed(100.0);      // Set Max Speed of Stepper (Slower to get better accuracy)
+  Accelstepper_arm.setAcceleration(100.0);  // Set Acceleration of Stepper
+  initial_arm_homing = 1;
+  while (!digitalRead(home_switch_arm)) { // Make the Stepper move CW until the switch is deactivated
+    Accelstepper_arm.moveTo(initial_arm_homing);
+    Accelstepper_arm.run();
+    initial_arm_homing++;
+    delay(5);
+  }
+  Accelstepper_arm.setCurrentPosition(0);
 }
 
 void track_home()
 {
-    // home the track
-    while (digitalRead(home_switch_track)) {  // Make the Stepper move CCW until the switch is activated
-        Accelstepper_track.moveTo(initial_track_homing);  // Set the position to move to
-        initial_track_homing--;  // Decrease by 1 for next move if needed
-        Accelstepper_track.run();  // Start moving the stepper
-        delay(5);
-    }
-    Accelstepper_track.setCurrentPosition(0);  // Set the current position as zero for now
-    Accelstepper_track.setMaxSpeed(100.0);      // Set Max Speed of Stepper (Slower to get better accuracy)
-    Accelstepper_track.setAcceleration(100.0);  // Set Acceleration of Stepper
-    initial_track_homing=1;
-    while (!digitalRead(home_switch_track)) { // Make the Stepper move CW until the switch is deactivated
-        Accelstepper_track.moveTo(initial_track_homing);
-        Accelstepper_track.run();
-        initial_track_homing++;
-        delay(5);
-    }
-    Accelstepper_track.setCurrentPosition(0);
+  // home the track
+  while (digitalRead(home_switch_track)) {  // Make the Stepper move CCW until the switch is activated
+    Accelstepper_track.moveTo(initial_track_homing);  // Set the position to move to
+    initial_track_homing--;  // Decrease by 1 for next move if needed
+    Accelstepper_track.run();  // Start moving the stepper
+    delay(5);
+  }
+  Accelstepper_track.setCurrentPosition(0);  // Set the current position as zero for now
+  Accelstepper_track.setMaxSpeed(100.0);      // Set Max Speed of Stepper (Slower to get better accuracy)
+  Accelstepper_track.setAcceleration(100.0);  // Set Acceleration of Stepper
+  initial_track_homing = 1;
+  while (!digitalRead(home_switch_track)) { // Make the Stepper move CW until the switch is deactivated
+    Accelstepper_track.moveTo(initial_track_homing);
+    Accelstepper_track.run();
+    initial_track_homing++;
+    delay(5);
+  }
+  Accelstepper_track.setCurrentPosition(0);
 }
 
-# setup the devices and home them
+// setup the devices and home them
 void setup()
 {
-    AFMSbottom.begin(); // Start the bottom board
-    // Setup cd spinner motor
-    Accelstepper_cd_spinner.setMaxSpeed(400.0);
-    Accelstepper_cd_spinner.setAcceleration(100.0);
-    // line serial communication
-    ripper_serial.begin(1200);
-    arm_home()
-    track_home()
+  AFMSbottom.begin(); // Start the bottom board
+  // Setup cd spinner motor
+  Accelstepper_cd_spinner.setMaxSpeed(400.0);
+  Accelstepper_cd_spinner.setAcceleration(100.0);
+  // line serial communication
+  ripper_serial.begin(1200);
+  arm_home();
+  track_home();
+}
 
 // begin main loop
 void loop()
 {
-    while (ripper_serial.available() > 0)
+  while (ripper_serial.available() > 0)
+  {
+    ripper_serial.readBytesUntil('|', serialdata, 20);
+    if (serialdata == 'arm')
     {
-        ripper_serial.readBytesUntil('|', serialdata, 20);
-        if (serialdata == 'arm')
-        {
-            Accelstepper_arm.moveTo(ripper_serial.parseInt());
-            Accelstepper_arm.run();
-        }
-        else if (serialdata == 'track')
-        {
-            Accelstepper_track.moveTo(ripper_serial.parseInt());
-            Accelstepper_track.run();
-        }
-        else if (serialdata == 'pickup')
-        {
-            while (digitalRead(home_switch_spindle)) {
-                Accelstepper_arm.moveTo(initial_arm_homing);
-                initial_arm_homing--;
-                Accelstepper_arm.run();
-                delay(5);
-            }
-            digitalWrite(vaccuum_pin, HIGH);
-            arm_home()
-        }
-        else if (serialdata == 'drop')
-        {
-            Accelstepper_arm.moveTo(ripper_serial.parseInt());
-            Accelstepper_arm.run();
-            digitalWrite(vaccuum_pin, LOW);
-            arm_home()
-        }
-        else
-        {
-            stepper_to_move = Accelstepper_cd_spinner;
-        }
-
+      Accelstepper_arm.moveTo(ripper_serial.parseInt());
+      Accelstepper_arm.run();
     }
+    else if (serialdata == 'track')
+    {
+      Accelstepper_track.moveTo(ripper_serial.parseInt());
+      Accelstepper_track.run();
+    }
+    else if (serialdata == 'pickup')
+    {
+      while (digitalRead(home_switch_spindle)) {
+        Accelstepper_arm.moveTo(initial_arm_homing);
+        initial_arm_homing--;
+        Accelstepper_arm.run();
+        delay(5);
+      }
+      digitalWrite(vaccuum_pin, HIGH);
+      arm_home();
+    }
+    else if (serialdata == 'drop')
+    {
+      Accelstepper_arm.moveTo(ripper_serial.parseInt());
+      Accelstepper_arm.run();
+      digitalWrite(vaccuum_pin, LOW);
+      arm_home();
+    }
+    else
+    {
+      //stepper_to_move = Accelstepper_cd_spinner;
+    }
+
+  }
 }
