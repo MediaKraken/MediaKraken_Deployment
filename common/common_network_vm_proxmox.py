3e30f9bab8abf1bac47f69a56ebaf8796d82d67a
SpootDev <spootdev@gmail.com>
2018-01-24 22:21:23 -0600

    security warning

--- common/common_network_vm_proxmox.py
+++ common/common_network_vm_proxmox.py
@@ -19,7 +19,8 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 import logging # pylint: disable=W0611
 import requests
-
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 class CommonNetworkProxMox(object):
     """
@@ -282,12 +283,16 @@ class CommonNetworkProxMox(object):
         create lxc on code
         """
         post_data = {'vmid': self.com_net_prox_cluster_nextid()['data'],
-                     'ostemplate': template_name, 'storage': storage_id,
-                     'cpulimit': 8, 'hostname': host_name,
-                     'memory': memory_size, 'ostype': os_type,
-                     'password': 'metaman'}
-                     #'net': 'name=eth0'}
-                     #'rootfs': 'vm-118-disk-1a, size=32G'}
+                     'ostemplate': template_name}
+                      # 'hostname': host_name,
+                     # 'storage': storage_id,
+                     # 'cpulimit': 8,
+                     # 'memory': memory_size, 'ostype': os_type,
+                     # 'password': 'metaman',
+                     # #'net': 'name=eth0'}
+                     # 'rootfs': '32'}
+                     # #'rootfs': 'vm-118-disk-1a, size=32G'}
+        print('post %s' % post_data)
         return self.com_net_prox_api_call('post', 'nodes/%s/lxc' % node_name, post_data)
 
 
