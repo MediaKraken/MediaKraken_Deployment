f950c548c589a07f17973826be96afc1cac1a7b0
SpootDev <spootdev@gmail.com>
2018-01-31 20:58:14 -0600

    fix bucket

--- testing/test_common/test_common_cloud_aws_s3.py
+++ testing/test_common/test_common_cloud_aws_s3.py
@@ -57,8 +57,7 @@ class TestCommonCloudAWSS3(object):
         """
         Test function
         """
-        self.awss3.com_aws_s3_download(source_key, destination_filename,
-                                       backup_bucket=backup_bucket)
+        self.awss3.com_aws_s3_download(source_key, destination_filename, backup_bucket)
         os.remove("./cache/HashCalcDown.txt")
         os.remove("./cache/HashCalcDown2.txt")
 
