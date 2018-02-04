4beb2942ffc3ad548318c067073729788ab5c30a
SpootDev <spootdev@gmail.com>
2018-02-04 14:17:36 -0600

    pass db config

--- testing/test_common/test_common_metadata_imvdb.py
+++ testing/test_common/test_common_metadata_imvdb.py
@@ -21,6 +21,7 @@ import pytest  # pylint: disable=W0611
 import sys
 
 sys.path.append('.')
+from common import common_config_ini
 from common import common_metadata_imvdb
 
 
@@ -28,7 +29,9 @@ class TestCommonimvdb(object):
 
     @classmethod
     def setup_class(self):
-        self.imvdb_connection = common_metadata_imvdb.CommonMetadataIMVdb()
+        # open the database
+        option_config_json, db_connection = common_config_ini.com_config_read()
+        self.imvdb_connection = common_metadata_imvdb.CommonMetadataIMVdb(option_config_json)
 
     @classmethod
     def teardown_class(self):
