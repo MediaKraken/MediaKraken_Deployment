71759642b24f39e883b4830dd5f5a89e673dfa7c
SpootDev <spootdev@gmail.com>
2018-02-06 13:32:09 -0600

    save secret key and uploads

--- web_app/MediaKraken/settings.py
+++ web_app/MediaKraken/settings.py
@@ -10,13 +10,14 @@ from common import common_file
 
 
 class Config(object):
-    if os.path.exists('web_secret_key.txt'):
+    if os.path.exists('/mediakraken/key/web_secret_key.txt'):
         pass
     else:
-        data = os.urandom(24).encode('hex')
-        common_file.com_file_save_data('web_secret_key.txt', data, False)
+        common_file.com_file_save_data('/mediakraken/key/web_secret_key.txt',
+                                       os.urandom(24).encode('hex'), False)
     SECRET_KEY = os_env.get('MEDIAKRAKEN_SECRET',
-                            common_file.com_file_load_data('web_secret_key.txt', False))
+                            common_file.com_file_load_data('/mediakraken/key/web_secret_key.txt',
+                                                           False))
     APP_DIR = os.path.abspath(os.path.dirname(__file__))  # This directory
     PROJECT_ROOT = os.path.abspath(os.path.join(APP_DIR, os.pardir))
     BCRYPT_LOG_ROUNDS = 13
