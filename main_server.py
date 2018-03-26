9a426bb54ca90e2be4f4f6350350250e93248ab5
SpootDev <spootdev@gmail.com>
2018-03-26 14:42:52 -0500

    cleanup

--- main_server.py
+++ main_server.py
@@ -20,34 +20,17 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import logging  # pylint: disable=W0611
 import sys
 import subprocess
-import json
-import signal
 import os
 from common import common_config_ini
 from common import common_docker
 from common import common_logging
 from common import common_network_share
-from common import common_signal
-# from common import common_watchdog
 from common import common_version
-
-# rmda_enabled_os = False
-# try:
-#    from common import common_rmda
-#    rmda_enabled_os = True
-# except:
-#    pass
-
-# set signal exit breaks
-common_signal.com_signal_set_break()
 
 # start logging
 common_logging.com_logging_start()
 
 logging.info('PATH: %s' % os.environ['PATH'])
-# os.environ['PATH'] += ":./"
-# logging.info(os.environ['PATH'])
-
 
 logging.info('Check Certs')
 # check for and create ssl certs if needed
@@ -90,12 +73,6 @@ common_network_share.com_net_share_mount(db_connection.db_audit_shares())
 
 logging.info("Validate Paths")
 # validate paths in ini file
-# keep the checks split so user can be told which one is wrong
-# if not os.path.isdir(option_config_json['MediaKrakenServer']['MetadataImageLocal']):
-#    logging.critical("MediaKrakenServer/MetadataImageLocal is not a valid directory!  Exiting...")
-#    logging.critical("Invalid Path: %s" %
-#        option_config_json['MediaKrakenServer']['MetadataImageLocal'])
-#    sys.exit()
 if not os.path.isdir(option_config_json['MediaKrakenServer']['BackupLocal']):
     logging.critical(
         "MediaKrakenServer/BackupLocal is not a valid directory!  Exiting...")
@@ -103,38 +80,15 @@ if not os.path.isdir(option_config_json['MediaKrakenServer']['BackupLocal']):
                      option_config_json['MediaKrakenServer']['BackupLocal'])
     sys.exit()
 
-# look for infiniband rdma devices
-# if rmda_enabled_os:
-#    rmda_devices = common_rmda.com_rdma_get_devices()
-#    if rmda_devices is None:
-#        rmda_enabled_os = False
-
-
-# logging.info("Start Watchdog")
-# startup watchdog
-# watchdog = common_watchdog.CommonWatchdog()
-# watchdog.com_watchdog_start(db_connection.db_audit_paths(None, None))
-
-
 # startup the other reactor via popen as it's non-blocking
 proc = subprocess.Popen(
     ['python', './subprogram_reactor_line.py'], shell=False)
 logging.info("Reactor PID: %s", proc.pid)
 
-# fire up web image server
-# proc_image = subprocess.Popen(['python', './subprogram_reactor_web_images.py'], shell=False)
-# logging.info("Reactor Web Image PID: %s", proc_image.pid)
-
-
 # fire up cron service
 proc_cron = subprocess.Popen(
     ['python', './subprogram_cron_checker.py'], shell=False)
 logging.info("Cron PID: %s", proc_cron.pid)
-
-# fire up trigger procress
-# proc_trigger = subprocess.Popen(['python', './main_server_trigger.py'], shell=False)
-# logging.info("Trigger PID: %s", proc_trigger.pid)
-
 
 # fire up link servers
 link_pid = {}
@@ -148,10 +102,6 @@ for link_data in db_connection.db_link_list():
 # this will key off the twisted reactor...only reason is so watchdog doesn't shut down
 proc.wait()
 
-# stop watchdog
-# watchdog.com_watchdog_stop()
-
-
 # log stop
 db_connection.db_activity_insert('MediaKraken_Server Stop', None, 'System: Server Stop',
                                  'ServerStop', None, None, 'System')
@@ -161,11 +111,3 @@ db_connection.db_commit()
 
 # close the database
 db_connection.db_close()
-
-# stop children
-os.kill(proc.pid, signal.SIGTERM)
-# os.kill(proc_image.pid, signal.SIGTERM)
-os.kill(proc_cron.pid, signal.SIGTERM)
-# os.kill(proc_trigger.pid, signal.SIGTERM)
-for link_data in link_pid.keys():
-    os.kill(link_pid[link_data], signal.SIGTERM)
