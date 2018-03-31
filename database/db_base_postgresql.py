cd1c5fc056e446e4fdca9dfc5db0d770b32be22e
SpootDev <spootdev@gmail.com>
2018-03-30 19:23:42 -0500

    fix import

--- database/db_base_postgresql.py
+++ database/db_base_postgresql.py
@@ -18,7 +18,7 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 import os
-from .. import common_global
+from common import common_global
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 from psycopg2.extensions import ISOLATION_LEVEL_READ_COMMITTED  # the default
 
