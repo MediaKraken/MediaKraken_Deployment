588bec6eceeba73c142ebb9f2dc527aa6cd544fa
SpootDev <spootdev@gmail.com>
2018-04-09 13:27:07 -0500

    new

---
+++ common/common_docker_definitions.py
@@ -0,0 +1,32 @@
+'''
+  Copyright (C) 2018 Quinn D Granfor <spootdev@gmail.com>
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  version 2, as published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  General Public License version 2 for more details.
+
+  You should have received a copy of the GNU General Public License
+  version 2 along with this program; if not, write to the Free
+  Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+  MA 02110-1301, USA.
+'''
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import os
+
+DOCKER_TRANSMISSION = ('/start-transmission.sh', 'mktransmission',
+                       'mediakraken/mktransmission', True,
+                       {"9091": None, "51413/tcp": None, "51413/udp": None},
+                       'mk_mediakraken_network',
+                       ['/var/opt/mediakraken/transmission/downloads:/transmission/downloads',
+                        '/var/opt/mediakraken/transmission/incomplete:/transmission/incomplete'],
+                       True,
+                       {'USERNAME': os.environ['TRANSUSER'],
+                        'PASSWORD': os.environ['TRANSPASSWORD']}
+                       )
