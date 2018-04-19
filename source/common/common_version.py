eb596357dfab2dd07945f797ea8485240c7350ed
SpootDev <spootdev@gmail.com>
2018-04-19 10:19:50 -0500

    wireshark launch

--- source/common/common_version.py
+++ source/common/common_version.py
@@ -18,8 +18,8 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-APP_VERSION = 'dev'
+APP_VERSION = 'dev37'
 
-DB_VERSION = 20
+DB_VERSION = 21
 
 MAME_VERSION = '196'
