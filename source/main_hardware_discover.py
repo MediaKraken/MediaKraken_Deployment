143c4d0b5be301146b2ab4457486d5a01395ae84
SpootDev <spootdev@gmail.com>
2018-04-29 19:16:25 -0500

    logging

--- source/main_hardware_discover.py
+++ source/main_hardware_discover.py
@@ -32,10 +32,12 @@ common_global.es_inst = common_logging_elasticsearch.CommonElasticsearch('main_h
 
 media_devices = []
 
-# tuner discovery
+# hdhomerun tuner discovery
 tuner_api = common_hardware_hdhomerun.CommonHardwareHDHomeRun()
 tuner_api.com_hdhomerun_discover()
 for row_tuner in tuner_api.com_hdhomerun_list():
+    common_global.es_inst.com_elastic_index('info', {
+        'hdhomerun out': common_string.com_string_ip_int_to_ascii(row_tuner.get_device_ip())})
     media_devices.append({'Model': row_tuner.get_var(item='/sys/model'),
                           'HWModel': row_tuner.get_var(item='/sys/hwmodel'),
                           'Name': row_tuner.get_name(),
@@ -61,6 +63,7 @@ chrome_pid.wait()
 
 # roku discover
 for roku in common_hardware_roku_network.com_roku_network_discovery():
+    common_global.es_inst.com_elastic_index('info', {'roku out': roku})
     media_devices.append({'Roku': roku})
 
 common_file.com_file_save_data('/mediakraken/devices/device_scan.txt',
