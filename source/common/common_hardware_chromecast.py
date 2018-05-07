c1696c2d0aa1d0e8425682301ad1204fb15982b6
SpootDev <spootdev@gmail.com>
2018-05-07 14:14:59 -0500

    set socket opts

--- source/common/common_hardware_chromecast.py
+++ source/common/common_hardware_chromecast.py
@@ -47,8 +47,8 @@ def com_hard_chrome_discover(timeout=5, retries=1):
     for _ in range(retries):
         ssdp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         ssdp_sock.setblocking(0)
-        #     ssdp_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        #     ssdp_sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
+        ssdp_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        ssdp_sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
         ssdp_sock.sendto(message, ("239.255.255.250", 1900))
         while True:
             try:
