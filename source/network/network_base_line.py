984861b85d27a49ea9324cc140d5c5aae5e63869
SpootDev <spootdev@gmail.com>
2018-05-14 21:12:45 -0500

    correct double encode

--- source/network/network_base_line.py
+++ source/network/network_base_line.py
@@ -235,7 +235,7 @@ class NetworkEvents(basic.LineReceiver):
                 if media_path is not None:
                     if json_message['Sub'] == 'Client':
                         self.send_single_ip(
-                            json.dumps({'Target': 'Play', 'Data': media_path}).encode("utf8"),
+                            json.dumps({'Target': 'Play', 'Data': media_path}),
                             json_message['Target'])
                 #     # launch and attach to local running ffserver
                 #     # TODO set server port for ffmpeg
