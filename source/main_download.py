eefa91db80d76a2272567596cacd4f39020ba009
SpootDev <spootdev@gmail.com>
2018-07-03 13:18:55 -0500

    async consumer

--- source/main_download.py
+++ source/main_download.py
@@ -1,28 +1,8 @@
-'''
-  Copyright (C) 2017 Quinn D Granfor <spootdev@gmail.com>
-
-  This program is free software; you can redistribute it and/or
-  modify it under the terms of the GNU General Public License
-  version 2, as published by the Free Software Foundation.
-
-  This program is distributed in the hope that it will be useful, but
-  WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-  General Public License version 2 for more details.
-
-  You should have received a copy of the GNU General Public License
-  version 2 along with this program; if not, write to the Free
-  Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-  MA 02110-1301, USA.
-'''
-
-from __future__ import absolute_import, division, print_function, unicode_literals
-
 import json
 import subprocess
-import time
 
 import pika
+
 from common import common_global
 from common import common_logging_elasticsearch
 from common import common_network
@@ -31,55 +11,142 @@ from common import common_network
 common_global.es_inst = common_logging_elasticsearch.CommonElasticsearch('main_download')
 
 
-def on_message(channel, method_frame, header_frame, body):
-    """
-    Process pika message
-    """
-    if body is not None:
-        common_global.es_inst.com_elastic_index('info', {'msg body': body})
-        json_message = json.loads(body)
-        if json_message['Type'] == 'Download':
-            # file, image, etc
-            if json_message['Subtype'] == 'File':
-                common_network.mk_network_fetch_from_url(json_message['URL'],
-                                                         json_message['Local Save Path'])
-            elif json_message['Subtype'] == 'Youtube':
-                dl_pid = subprocess.Popen(['youtube-dl', '-i', '--download-archive',
-                                           '/mediakraken/yt_dl_archive.txt',
-                                           json_message['URL']],
-                                          shell=False)
-                dl_pid.wait()  # wait for finish so doesn't startup a bunch of dl's
-        channel.basic_ack(delivery_tag=method_frame.delivery_tag)
+class MKConsumer(object):
+    EXCHANGE = 'mkque_download_ex'
+    EXCHANGE_TYPE = 'direct'
+    QUEUE = 'mkdownload'
+    ROUTING_KEY = 'mkdownload'
+
+    def __init__(self, amqp_url):
+        self._connection = None
+        self._channel = None
+        self._closing = False
+        self._consumer_tag = None
+        self._url = amqp_url
+
+    def connect(self):
+        return pika.SelectConnection(pika.URLParameters(self._url),
+                                     self.on_connection_open,
+                                     stop_ioloop_on_close=False)
+
+    def on_connection_open(self, unused_connection):
+        self.add_on_connection_close_callback()
+        self.open_channel()
+
+    def add_on_connection_close_callback(self):
+        self._connection.add_on_close_callback(self.on_connection_closed)
+
+    def on_connection_closed(self, connection, reply_code, reply_text):
+        self._channel = None
+        if self._closing:
+            self._connection.ioloop.stop()
+        else:
+            self._connection.add_timeout(5, self.reconnect)
+
+    def reconnect(self):
+        self._connection.ioloop.stop()
+        if not self._closing:
+            self._connection = self.connect()
+            self._connection.ioloop.start()
+
+    def open_channel(self):
+        self._connection.channel(on_open_callback=self.on_channel_open)
+
+    def on_channel_open(self, channel):
+        self._channel = channel
+        self.add_on_channel_close_callback()
+        self.setup_exchange(self.EXCHANGE)
+
+    def add_on_channel_close_callback(self):
+        self._channel.add_on_close_callback(self.on_channel_closed)
+
+    def on_channel_closed(self, channel, reply_code, reply_text):
+        self._connection.close()
+
+    def setup_exchange(self, exchange_name):
+        self._channel.exchange_declare(self.on_exchange_declareok,
+                                       exchange_name,
+                                       self.EXCHANGE_TYPE)
+
+    def on_exchange_declareok(self, unused_frame):
+        self.setup_queue(self.QUEUE)
+
+    def setup_queue(self, queue_name):
+        self._channel.queue_declare(self.on_queue_declareok, queue_name)
+
+    def on_queue_declareok(self, method_frame):
+        self._channel.queue_bind(self.on_bindok, self.QUEUE,
+                                 self.EXCHANGE, self.ROUTING_KEY)
+
+    def on_bindok(self, unused_frame):
+        self.start_consuming()
+
+    def start_consuming(self):
+        self.add_on_cancel_callback()
+        self._consumer_tag = self._channel.basic_consume(self.on_message,
+                                                         self.QUEUE)
+
+    def add_on_cancel_callback(self):
+        self._channel.add_on_cancel_callback(self.on_consumer_cancelled)
+
+    def on_consumer_cancelled(self, method_frame):
+        if self._channel:
+            self._channel.close()
+
+    def on_message(self, unused_channel, basic_deliver, properties, body):
+        if body is not None:
+            common_global.es_inst.com_elastic_index('info', {'msg body': body})
+            json_message = json.loads(body)
+            if json_message['Type'] == 'Download':
+                # file, image, etc
+                if json_message['Subtype'] == 'File':
+                    common_network.mk_network_fetch_from_url(json_message['URL'],
+                                                             json_message['Local Save Path'])
+                elif json_message['Subtype'] == 'Youtube':
+                    dl_pid = subprocess.Popen(['youtube-dl', '-i', '--download-archive',
+                                               '/mediakraken/yt_dl_archive.txt',
+                                               json_message['URL']],
+                                              shell=False)
+                    dl_pid.wait()  # wait for finish so doesn't startup a bunch of dl's
+        self.acknowledge_message(basic_deliver.delivery_tag)
+
+    def acknowledge_message(self, delivery_tag):
+        self._channel.basic_ack(delivery_tag)
+
+    def stop_consuming(self):
+        if self._channel:
+            self._channel.basic_cancel(self.on_cancelok, self._consumer_tag)
+
+    def on_cancelok(self, unused_frame):
+        self.close_channel()
+
+    def close_channel(self):
+        self._channel.close()
+
+    def run(self):
+        self._connection = self.connect()
+        self._connection.ioloop.start()
+
+    def stop(self):
+        self._closing = True
+        self.stop_consuming()
+        self._connection.ioloop.start()
+
+    def close_connection(self):
+        self._connection.close()
 
 
-# fire off wait for it script to allow rabbitmq connection
-wait_pid = subprocess.Popen(['/mediakraken/wait-for-it-ash.sh', '-h',
-                             'mkrabbitmq', '-p', ' 5672'], shell=False)
-wait_pid.wait()
+def main():
+    # fire off wait for it script to allow rabbitmq connection
+    wait_pid = subprocess.Popen(['/mediakraken/wait-for-it-ash.sh', '-h',
+                                 'mkrabbitmq', '-p', ' 5672'], shell=False)
+    wait_pid.wait()
+    mk_rabbit = MKConsumer('amqp://guest:guest@mkrabbitmq:5672/%2F')
+    try:
+        mk_rabbit.run()
+    except KeyboardInterrupt:
+        mk_rabbit.stop()
 
-# pika rabbitmq connection
-parameters = pika.ConnectionParameters('mkrabbitmq',
-                                       credentials=pika.PlainCredentials('guest', 'guest'))
-connection = pika.BlockingConnection(parameters)
 
-# setup channels and queue
-channel = connection.channel()
-exchange = channel.exchange_declare(exchange="mkque_download_ex", exchange_type="direct",
-                                    durable=True)
-queue = channel.queue_declare(queue='mkdownload', durable=True)
-channel.queue_bind(exchange="mkque_download_ex", queue='mkdownload')
-channel.basic_qos(prefetch_count=1)
-
-while True:
-    time.sleep(1)
-    # grab message from rabbitmq if available
-    try:  # since can get connection drops
-        method_frame, header_frame, body = channel.basic_get(
-            queue='mkdownload', no_ack=False)
-        on_message(channel, method_frame, header_frame, body)
-    except:
-        pass
-
-channel.cancel()
-# close the pika connection
-connection.close()
+if __name__ == '__main__':
+    main()
