f64fa5f170b1659fef4c664d0b94ff09bb29399f
SpootDev <spootdev@gmail.com>
2018-07-03 19:54:34 -0500

    paths and source sync

--- jenkins_scripts/source_sync.sh
+++ jenkins_scripts/source_sync.sh
@@ -1,7 +1,6 @@
 # broadcast
 cp /home/metaman/MediaKraken_Deployment/source/subprogram_broadcast.py /home/metaman/MediaKraken_Deployment/docker/alpine/ComposeMediaKrakenBroadcast/src/.
-cp /home/metaman/MediaKraken_Deployment/source/common/common_global.py /home/metaman/MediaKraken_Deployment/docker/alpine/ComposeMediaKrakenBroadcast/src/.
-cp /home/metaman/MediaKraken_Deployment/source/common/common_logging_elasticsearch.py /home/metaman/MediaKraken_Deployment/docker/alpine/ComposeMediaKrakenBroadcast/src/.
+cp /home/metaman/MediaKraken_Deployment/source/common /home/metaman/MediaKraken_Deployment/docker/alpine/ComposeMediaKrakenBroadcast/src/.
 
 # devicescanner
 cp -R /home/metaman/MediaKraken_Deployment/source/common /home/metaman/MediaKraken_Deployment/docker/alpine/ComposeMediaKrakenDevicescan/src/.
