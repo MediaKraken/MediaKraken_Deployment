6d0e5734d974312b9ff10c90622a0dea24a40e9c
SpootDev <spootdev@gmail.com>
2018-07-05 10:13:55 -0500

    clean up the dedicated os build

--- source/theater_os_dedicated/theater_os_build_(ubuntu).sh
+++ source/theater_os_dedicated/theater_os_build_(ubuntu).sh
@@ -13,17 +13,17 @@ fi
 cd LibreELEC.tv-$CODEVERSION
 
 # copy over the mediakraken distro settings
-cp -R ../OpenMediaKraken/distributions/MediaKraken ./distributions/.
-cp -R ../OpenMediaKraken/packages/* ./packages/.
+cp -R ./OpenMediaKraken/distributions/MediaKraken ./distributions/.
+cp -R ./OpenMediaKraken/packages/* ./packages/.
 
 # allow it to install packages
 PROJECT=Generic DISTRO=MediaKraken ARCH=x86_64 make -j`getconf _NPROCESSORS_ONLN` release
 PROJECT=Generic DISTRO=MediaKraken ARCH=x86_64 make -j`getconf _NPROCESSORS_ONLN` image
 
 # general rpi and pi zero install
-#PROJECT=RPi DISTRO=MediaKraken ARCH=arm make -j`getconf _NPROCESSORS_ONLN` release
-#PROJECT=RPi DISTRO=MediaKraken ARCH=arm make -j`getconf _NPROCESSORS_ONLN` image
+PROJECT=RPi DISTRO=MediaKraken ARCH=arm make -j`getconf _NPROCESSORS_ONLN` release
+PROJECT=RPi DISTRO=MediaKraken ARCH=arm make -j`getconf _NPROCESSORS_ONLN` image
 
 # general rpi2 install
-#PROJECT=RPi2 DISTRO=MediaKraken ARCH=arm make -j`getconf _NPROCESSORS_ONLN` release
-#PROJECT=RPi2 DISTRO=MediaKraken ARCH=arm make -j`getconf _NPROCESSORS_ONLN` image
+PROJECT=RPi2 DISTRO=MediaKraken ARCH=arm make -j`getconf _NPROCESSORS_ONLN` release
+PROJECT=RPi2 DISTRO=MediaKraken ARCH=arm make -j`getconf _NPROCESSORS_ONLN` image
