f45f3233546ea26cae953c69adcad6eeb2b13266
SpootDev <spootdev@gmail.com>
2018-07-04 21:17:03 -0500

    path for vidstab

--- docker/alpine/ComposeMediaKrakenBaseFFMPEG/scripts/vidstab.sh
+++ docker/alpine/ComposeMediaKrakenBaseFFMPEG/scripts/vidstab.sh
@@ -1,7 +1,7 @@
 cd /mediakraken/install/
 git clone https://github.com/georgmartius/vid.stab.git
 cd /mediakraken/install/vid.stab/
-cmake .
+cmake -DCMAKE_INSTALL_PREFIX:PATH=/mediakraken
 make -j`getconf _NPROCESSORS_ONLN`
 make install
 
