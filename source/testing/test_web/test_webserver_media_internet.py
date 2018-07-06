2a1fff8f33ead43e24094e96955842bf6d9e236d
SpootDev <spootdev@gmail.com>
2018-07-06 11:37:32 -0500

    test main links

--- source/testing/test_web/test_webserver_media_internet.py
+++ source/testing/test_web/test_webserver_media_internet.py
@@ -31,3 +31,50 @@ def test_main_menu(driver):
     driver.get(TEST_TARGET)
     driver.find_element_by_id('menu_home').click()
     assert 'MediaKraken' in driver.title
+
+
+def test_main_internet(driver):
+    """
+    Click internet
+    """
+    driver.get(TEST_TARGET)
+    driver.find_element_by_id('user_internet_page').click()
+    assert 'MediaKraken' in driver.title
+
+
+def test_main_internet_youtube(driver):
+    """
+    Click youtube
+    """
+    driver.get(TEST_TARGET)
+    driver.find_element_by_id('user_internet_youtube_page').click()
+    assert 'MediaKraken' in driver.title
+
+def test_main_internet_vimeo(driver):
+    """
+    Click vimeo
+    """
+    driver.get(TEST_TARGET)
+    driver.back() # go back to main internet page
+    driver.find_element_by_id('user_internet_vimeo_page').click()
+    assert 'MediaKraken' in driver.title
+
+
+def test_main_internet_twitch(driver):
+    """
+    Click vimeo
+    """
+    driver.get(TEST_TARGET)
+    driver.back() # go back to main internet page
+    driver.find_element_by_id('user_internet_twitch_page').click()
+    assert 'MediaKraken' in driver.title
+
+
+def test_main_internet_flickr(driver):
+    """
+    Click vimeo
+    """
+    driver.get(TEST_TARGET)
+    driver.back() # go back to main internet page
+    driver.find_element_by_id('user_internet_flickr_page').click()
+    assert 'MediaKraken' in driver.title
