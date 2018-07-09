5e06adc685c2baca7a5ee939cdfdb25d4a205803
SpootDev <spootdev@gmail.com>
2018-07-09 12:05:09 -0500

    merge theater remote code

--- source/common/common_hardware_controller.py
+++ source/common/common_hardware_controller.py
@@ -16,9 +16,89 @@
   MA 02110-1301, USA.
 '''
 
+import json
+
+from common import common_network_telnet
+from common import common_serial
+
 from . import common_hardware_marantz
 from . import common_hardware_pioneer
 from . import common_hardware_samsung
+
+
+def main_remote_control_event_process(self, action_type_list):
+    """
+    # process remote control button
+    """
+    try:
+        json_data = json.loads(
+            self.remote_mode_details_item[self.remote_mode_current_item])
+        # check to see if rs232 device is already open
+        if json_data["Protocol"]["Method"] == "RS232":
+            if not json_data["Protocol"]["Hardware Port"] in self.rs232_devices_dict:
+                com_net_telnet_device = common_network_telnet.CommonNetworkTelnet()
+                self.rs232_devices_dict[json_data["Protocol"]["Host IP"]] \
+                    = com_net_telnet_device.com_net_telnet_open_device(
+                    json_data["Protocol"]["Host IP"],
+                    json_data["Protocol"]["Host Port"], json_data["Protocol"]["User"],
+                    json_data["Protocol"]["Password"])
+                com_net_telnet_device.com_net_telnet_write_device(
+                    self.octmote_json_fetch_data_for_command(json_data,
+                                                             action_type_list))
+        # check to see if IR device is already open
+        elif json_data["Protocol"]["Method"] == "IR":
+            if not json_data["fake"] in self.ir_devices_dict:
+                pass
+        # check to see if lan device already open
+        elif json_data["Protocol"]["Method"] == "LAN":
+            if not (json_data["Protocol"]["Host IP"], json_data["Protocol"]["Hardware Port"]) \
+                   in self.lan_devices_dict:
+                pass
+        elif json_data["Protocol"]["Method"] == "Telnet":
+            # check to see if telnet device already opened
+            if not json_data["Protocol"]["Host IP"] in self.telnet_devices_dict:
+                com_net_telnet_device = common_network_telnet.CommonNetworkTelnet()
+                self.telnet_devices_dict[json_data["Protocol"]["Host IP"]] \
+                    = com_net_telnet_device.com_net_telnet_open_device(
+                    json_data["Protocol"]["Host IP"],
+                    json_data["Protocol"]["Host Port"], json_data["Protocol"]["User"],
+                    json_data["Protocol"]["Password"])
+                com_net_telnet_device.com_net_telnet_write_device(
+                    self.octmote_json_fetch_data_for_command(json_data, action_type_list))
+        elif json_data["Protocol"]["Method"] == "Serial":
+            # check to see if serial device already opened
+            if not (json_data["Protocol"]["Host IP"], json_data["Protocol"]["Hardware Port"]) \
+                   in self.serial_devices_dict:
+                com_net_serial_device = common_serial.CommonSerial()
+                self.serial_devices_dict[(json_data["Protocol"]["Host IP"],
+                                          json_data["Protocol"]["Hardware Port"])] \
+                    = com_net_serial_device.com_serial_open_device(
+                    json_data["Protocol"]["Hardware Port"],
+                    json_data["Protocol"]["Baud Rate"], json_data["Protocol"]["Parity Bit"],
+                    json_data["Protocol"]["Stop Bit"], json_data["Protocol"]["Data Length"])
+                com_net_serial_device.com_serial_write_device(
+                    self.octmote_json_fetch_data_for_command(json_data, action_type_list))
+        elif json_data["Protocol"]["Method"] == "EISCP":
+            # check to see if eiscp device already open
+            if not json_data["Protocol"]["Host IP"] in self.eiscp_devices_dict:
+                pass
+        elif json_data["Protocol"]["Method"] == "Kivy":
+            if not (json_data["Protocol"]["Host IP"], json_data["Protocol"]["Hardware Port"]) \
+                   in self.kivy_lan_devices_dict:
+                self.kivy_lan_devices_dict[(json_data["Protocol"]["Host IP"],
+                                            json_data["Protocol"]["Hardware Port"])] \
+                    = (json_data["Protocol"]["Host IP"],
+                       json_data["Protocol"]["Hardware Port"])
+            common_network_kodi.com_net_kodi_command(json_data["Protocol"]["Host IP"],
+                                                     json_data["Protocol"]["Hardware Port"],
+                                                     self.octmote_json_fetch_data_for_command(
+                                                         json_data,
+                                                         action_type_list))
+        else:
+            print(("Unhandled Protocol Method %s",
+                   json_data["Protocol"]["Method"]))
+    except:
+        pass
 
 
 class CommonHardwareController(object):
