8578b1ed5bc879fa99a58079988b87bbbf80b875
SpootDev <spootdev@gmail.com>
2018-07-15 12:29:28 -0500

    naem fix

--- jenkins_scripts/dockerhub_deploy/tag_rename_images.sh
+++ jenkins_scripts/dockerhub_deploy/tag_rename_images.sh
@@ -14,7 +14,7 @@ docker tag alpine_nginx:latest mediakraken/mknginx:latest
 #docker tag alpine_pgadmin mediakraken/mkpgadmin:latest
 docker tag alpine_pgbounce:latest mediakraken/mkpgbounce:latest
 #docker tag alpine_prefetchtmdb:latest mediakraken/mkprefetchtmdb:latest
-#docker tag alpine_prefetchtmdb:latest mediakraken/mkprefetchtvmaze:latest
+#docker tag alpine_prefetchtvmaze:latest mediakraken/mkprefetchtvmaze:latest
 docker tag alpine_rabbit:latest mediakraken/mkrabbitmq:latest
 docker tag alpine_redis:latest mediakraken/mkredis:latest
 #docker tag alpine_ripper mediakraken/mkripper:latest
