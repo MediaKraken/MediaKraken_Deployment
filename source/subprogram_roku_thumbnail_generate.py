9fa74b0442d70d12b0867e9e8e51af4de438e41c
SpootDev <spootdev@gmail.com>
2018-07-16 15:46:18 -0500

    real consumer for roku thumbnail

--- source/subprogram_roku_thumbnail_generate.py
+++ source/subprogram_roku_thumbnail_generate.py
@@ -16,26 +16,146 @@
   MA 02110-1301, USA.
 '''
 
-from common import common_config_ini
+import json
+import subprocess
+
+import pika
 from common import common_global
 from common import common_logging_elasticsearch
 
 # start logging
 common_global.es_inst = common_logging_elasticsearch.CommonElasticsearch(
-    'subprogram_roku_thumb_generate')
+    'subprogram_roku_thumbnail_generate')
 
-# open the database
-option_config_json, db_connection = common_config_ini.com_config_read()
 
-# go through ALL known media files
-thumbnails_generated = 0
-for row_data in db_connection.db_known_media():
-    # TODO  actually, this should probably be the metadata
-    # TODO the common roku code has the bif/thumb gen
-    common_global.es_inst.com_elastic_index('info', {'row': row_data})
+class MKConsumer(object):
+    EXCHANGE = 'mkque_roku_ex'
+    EXCHANGE_TYPE = 'direct'
+    QUEUE = 'mkroku'
+    ROUTING_KEY = 'mkroku'
 
-# commit all changes
-db_connection.db_commit()
+    def __init__(self, amqp_url):
+        self._connection = None
+        self._channel = None
+        self._closing = False
+        self._consumer_tag = None
+        self._url = amqp_url
 
-# close DB
-db_connection.db_close()
+    def connect(self):
+        return pika.SelectConnection(pika.URLParameters(self._url),
+                                     self.on_connection_open,
+                                     stop_ioloop_on_close=False)
+
+    def on_connection_open(self, unused_connection):
+        self.add_on_connection_close_callback()
+        self.open_channel()
+
+    def add_on_connection_close_callback(self):
+        self._connection.add_on_close_callback(self.on_connection_closed)
+
+    def on_connection_closed(self, connection, reply_code, reply_text):
+        self._channel = None
+        if self._closing:
+            self._connection.ioloop.stop()
+        else:
+            self._connection.add_timeout(5, self.reconnect)
+
+    def reconnect(self):
+        self._connection.ioloop.stop()
+        if not self._closing:
+            self._connection = self.connect()
+            self._connection.ioloop.start()
+
+    def open_channel(self):
+        self._connection.channel(on_open_callback=self.on_channel_open)
+
+    def on_channel_open(self, channel):
+        self._channel = channel
+        self.add_on_channel_close_callback()
+        self.setup_exchange(self.EXCHANGE)
+
+    def add_on_channel_close_callback(self):
+        self._channel.add_on_close_callback(self.on_channel_closed)
+
+    def on_channel_closed(self, channel, reply_code, reply_text):
+        self._connection.close()
+
+    def setup_exchange(self, exchange_name):
+        self._channel.exchange_declare(self.on_exchange_declareok,
+                                       exchange_name,
+                                       self.EXCHANGE_TYPE, durable=True)
+
+    def on_exchange_declareok(self, unused_frame):
+        self.setup_queue(self.QUEUE)
+
+    def setup_queue(self, queue_name):
+        self._channel.queue_declare(self.on_queue_declareok, queue_name, durable=True)
+
+    def on_queue_declareok(self, method_frame):
+        self._channel.queue_bind(self.on_bindok, self.QUEUE,
+                                 self.EXCHANGE, self.ROUTING_KEY)
+
+    def on_bindok(self, unused_frame):
+        self.start_consuming()
+
+    def start_consuming(self):
+        self.add_on_cancel_callback()
+        self._consumer_tag = self._channel.basic_consume(self.on_message,
+                                                         self.QUEUE)
+
+    def add_on_cancel_callback(self):
+        self._channel.add_on_cancel_callback(self.on_consumer_cancelled)
+
+    def on_consumer_cancelled(self, method_frame):
+        if self._channel:
+            self._channel.close()
+
+    def on_message(self, unused_channel, basic_deliver, properties, body):
+        if body is not None:
+            common_global.es_inst.com_elastic_index('info', {'msg body': body})
+            json_message = json.loads(body)
+            if json_message['Type'] == 'Roku':
+                if json_message['Subtype'] == 'Thumbnail':
+                    pass
+        self.acknowledge_message(basic_deliver.delivery_tag)
+
+    def acknowledge_message(self, delivery_tag):
+        self._channel.basic_ack(delivery_tag)
+
+    def stop_consuming(self):
+        if self._channel:
+            self._channel.basic_cancel(self.on_cancelok, self._consumer_tag)
+
+    def on_cancelok(self, unused_frame):
+        self.close_channel()
+
+    def close_channel(self):
+        self._channel.close()
+
+    def run(self):
+        self._connection = self.connect()
+        self._connection.ioloop.start()
+
+    def stop(self):
+        self._closing = True
+        self.stop_consuming()
+        self._connection.ioloop.start()
+
+    def close_connection(self):
+        self._connection.close()
+
+
+def main():
+    # fire off wait for it script to allow rabbitmq connection
+    wait_pid = subprocess.Popen(['/mediakraken/wait-for-it-ash.sh', '-h',
+                                 'mkrabbitmq', '-p', ' 5672'], shell=False)
+    wait_pid.wait()
+    mk_rabbit = MKConsumer('amqp://guest:guest@mkrabbitmq:5672/%2F')
+    try:
+        mk_rabbit.run()
+    except KeyboardInterrupt:
+        mk_rabbit.stop()
+
+
+if __name__ == '__main__':
+    main()
