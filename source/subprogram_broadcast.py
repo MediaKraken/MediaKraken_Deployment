ead4412c5877189686497fd9c9e3c659086292e4
SpootDev <spootdev@gmail.com>
2018-07-26 11:45:44 -0500

    return server

--- source/subprogram_broadcast.py
+++ source/subprogram_broadcast.py
@@ -16,17 +16,16 @@ docker_inst = common_docker.CommonDocker()
 # it returns a dict, not a json
 mediakraken_ip = docker_inst.com_docker_info()['Swarm']['NodeAddr']
 
-# grab container list
-for container_json in docker_inst.com_docker_container_list():
-    # grab ports for server
-    if container_json['Names'][0] == '/mkbroadcast':
-        docker_port = str(docker_inst.com_docker_port(container_json['Id'], 8903))
-        break
-
 # begin loop to respond to all broadcast messages
 while True:
     recv_data, addr = server_socket.recvfrom(2048)
     if recv_data == b"who is MediaKrakenServer?":
+        # grab container list - do here since server could have restarted on other port
+        for container_json in docker_inst.com_docker_container_list():
+            # grab ports for server
+            if container_json['Names'][0] == '/mkserver':
+                docker_port = str(docker_inst.com_docker_port(container_json['Id'], 8903))
+                break
         common_global.es_inst.com_elastic_index('info', {'addr': str(addr),
                                                          'data': str(recv_data)})
         server_socket.sendto((mediakraken_ip + ":" + docker_port).encode(), addr)
