daba424b0d49cd76b784a0dd73c8577814ce9e51
SpootDev <spootdev@gmail.com>
2018-10-31 14:18:49 -0500

    hide ffprobe banner

--- source/common/common_ffmpeg.py
+++ source/common/common_ffmpeg.py
@@ -27,7 +27,8 @@ def com_ffmpeg_media_attr(file_path):
     """
     try:
         media_json = subprocess.check_output(
-            split('ffprobe -show_format -show_streams -show_chapters -print_format json \"'
+            split('ffprobe -hide_banner -show_format -show_streams'
+                  ' -show_chapters -print_format json \"'
                   + file_path + '\"'))
     except:
         return None
