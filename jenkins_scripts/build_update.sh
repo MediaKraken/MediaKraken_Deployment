cc2414d5c9f0d67ee912381db7465ce297f3cb42
SpootDev <spootdev@gmail.com>
2018-11-07 20:57:22 -0600

    fix dir path

--- jenkins_scripts/build_update.sh
+++ jenkins_scripts/build_update.sh
@@ -143,5 +143,5 @@ docker build -t mediakraken/mkwireshark --build-arg ALPMIRROR=10.0.0.122 --build
 # nuke old images (commented due to base ffmpeg)
 #../../purge_images_none.sh
 
-# retag all the images
-./dockerhub_deploy/tag_rename_images.sh
+# retag all the images - double .. since cd'd into last docker image directory
+../dockerhub_deploy/tag_rename_images.sh
