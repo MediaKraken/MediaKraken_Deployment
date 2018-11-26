990bece06fed82d9c3ed08fb24f6bad22c4468c7
spootdev <spootdev@gmail.com>
2018-11-25 20:32:35 -0600

    fix up containers

--- docker/mediakraken/mediakraken_update.sh
+++ docker/mediakraken/mediakraken_update.sh
@@ -1,19 +1,19 @@
 docker-compose down
 docker-compose pull
-docker pull mediakraken/mkbasecuda
 docker pull mediakraken/mkbaseffmpeg
 docker pull mediakraken/mkdevicescan
 docker pull mediakraken/mkelk
+docker pull mediakraken/mkgamedata
 docker pull mediakraken/mkmusicbrainz
 docker pull mediakraken/mkmumble
 docker pull mediakraken/mknginxrtmp
 docker pull mediakraken/mkopenldap
+docker pull mediakraken/mkpgadmin
 docker pull mediakraken/mkslave
 docker pull mediakraken/mkstream
 docker pull mediakraken/mkteamspeak
 docker pull mediakraken/mktransmission
 docker pull mediakraken/mkwireshark
-docker pull dpage/pgadmin4
 docker pull portainer/portainer
 ./purge_images_none.sh
 docker-compose up -d
