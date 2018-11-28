4caf564d93adb9bb5e71a02e43ecbdb817aef78a
spootdev <spootdev@gmail.com>
2018-11-28 17:57:22 -0600

    allow syslog

--- source/common/common_logging_elasticsearch.py
+++ source/common/common_logging_elasticsearch.py
@@ -18,6 +18,7 @@
 
 import json
 import os
+import sys
 from datetime import datetime
 
 from elasticsearch import Elasticsearch
@@ -25,29 +26,34 @@ from elasticsearch import Elasticsearch
 
 class CommonElasticsearch(object):
     """
-    Class for interfacing with Elasticsearch
+    Class for interfacing with Elasticsearch or docker logging directly
     """
 
     def __init__(self, index_type='mediakraken', es_host='mkelk', es_port=9200):
-        try:
-            # doing the try as the environment not set if run from command line
-            if os.environ['DEBUG'] == 'True':
-                self.debug = True
+        if 'DEBUG' in os.environ:
+            self.debug = os.environ['DEBUG'].lower()
+            if self.debug == 'es':
                 self.es_inst = Elasticsearch([{'host': es_host, 'port': es_port}])
                 self.es_index = index_type
-            else:
-                self.debug = False
-        except:
-            self.debug = False
+        else:
+            self.debug = None
 
     def com_elastic_index(self, log_type, body_data):
-        if self.debug:
+        # write log to elk
+        if self.debug == 'es':
+            # leave the try....as I don't want the container to fail if mkelk not accepting
             try:
                 self.es_inst.index(index=self.es_index, doc_type='MediaKraken',
-                                   body={"text": {"type": log_type, "data": json.dumps(body_data),
+                                   body={"text": {"type": log_type,
+                                                  "data": json.dumps(body_data),
                                                   "timestamp": datetime.now()}})
             except:
                 print((log_type, body_data))
+        # write log to host syslog
+        elif self.debug == 'sys':
+            sys.stdout.write({"type": log_type,
+                              "data": json.dumps(body_data),
+                              "timestamp": datetime.now()})
 
     def com_elastic_get(self, id):
         self.es_inst.get(index=self.es_index, doc_type='MediaKraken', id=id)
