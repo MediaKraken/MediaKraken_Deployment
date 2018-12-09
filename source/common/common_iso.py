c9e1c6cf865393cdb0130b523c00aecd6918c25c
spootdev <spootdev@gmail.com>
2018-12-08 22:06:42 -0600

    add read features

--- source/common/common_iso.py
+++ source/common/common_iso.py
@@ -16,6 +16,7 @@
   MA 02110-1301, USA.
 '''
 
+# https://github.com/MediaKraken-Dependancies/isoparser
 import isoparser
 
 from . import common_global
@@ -27,11 +28,17 @@ class CommonISO(object):
     """
 
     def __init__(self):
-        self.iso = None
+        self.iso_inst = None
 
     def com_iso_load(self, url_file):
         """
         Open the iso file for parsing (url or file)
         """
         common_global.es_inst.com_elastic_index('info', {"iso url/file": url_file})
-        self.iso = isoparser.parse(url_file)
+        self.iso_inst = isoparser.parse(url_file)
+
+    def com_iso_find_children(self, file_directory):
+        return self.iso_inst.record(file_directory.split()).children
+
+    def com_iso_read_content(self, file_directory):
+        return self.iso_inst.record(file_directory.split()).content
