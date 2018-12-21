69e880c084b1190365cda9320f0215ceeef2bd8c
spootdev <spootdev@gmail.com>
2018-12-21 14:33:10 -0600

    remove dupe args

--- jenkins_scripts/build_update.sh
+++ jenkins_scripts/build_update.sh
@@ -36,7 +36,7 @@ docker build -t mediakraken/mkbaseffmpeg --build-arg ALPMIRROR=10.0.0.122 --buil
 
 # this will include the nvidia cuda/nvec/driver
 cd ../ComposeMediaKrakenBaseFFMPEGUbuntu
-docker build -t mediakraken/mkbaseffmpegubuntu --build-arg --build-arg PIPMIRROR=pypi.python.org 
+docker build -t mediakraken/mkbaseffmpegubuntu --build-arg PIPMIRROR=pypi.python.org 
 
 # build the base node
 # Adds NODE to the base ffmpeg and ffprobe.
