f1a1984d70f9c9becace540f2e91e1cd6949c0e4
SpootDev <spootdev@gmail.com>
2019-06-19 15:22:13 -0500

    fix loggin name

--- source/main_hardware.py
+++ source/main_hardware.py
@@ -62,18 +62,18 @@ class MKConsumer(object):
         self._consuming = False
         if self._connection.is_closing or self._connection.is_closed:
             common_global.es_inst.com_elastic_index('info', {
-                'ffprobe': 'Connection is closing or already closed'})
+                'hardware': 'Connection is closing or already closed'})
         else:
-            common_global.es_inst.com_elastic_index('info', {'ffprobe': 'Closing connection'})
+            common_global.es_inst.com_elastic_index('info', {'hardware': 'Closing connection'})
             self._connection.close()
 
     def on_connection_open(self, _unused_connection):
-        common_global.es_inst.com_elastic_index('info', {'ffprobe': 'Closing opened'})
+        common_global.es_inst.com_elastic_index('info', {'hardware': 'Closing opened'})
         self.open_channel()
 
     def on_connection_open_error(self, _unused_connection, err):
         common_global.es_inst.com_elastic_index('info',
-                                                {'ffprobe': ('Connection open failed: %s', err)})
+                                                {'hardware': ('Connection open failed: %s', err)})
         self.reconnect()
 
     def on_connection_closed(self, _unused_connection, reason):
@@ -82,7 +82,7 @@ class MKConsumer(object):
             self._connection.ioloop.stop()
         else:
             common_global.es_inst.com_elastic_index('info',
-                                                    {'ffprobe': (
+                                                    {'hardware': (
                                                     'Connection closed, reconnect necessary: %s',
                                                     reason)})
             self.reconnect()
@@ -92,28 +92,28 @@ class MKConsumer(object):
         self.stop()
 
     def open_channel(self):
-        common_global.es_inst.com_elastic_index('info', {'ffprobe': 'Creating a new channel'})
+        common_global.es_inst.com_elastic_index('info', {'hardware': 'Creating a new channel'})
         self._connection.channel(on_open_callback=self.on_channel_open)
 
     def on_channel_open(self, channel):
-        common_global.es_inst.com_elastic_index('info', {'ffprobe': 'Channel opened'})
+        common_global.es_inst.com_elastic_index('info', {'hardware': 'Channel opened'})
         self._channel = channel
         self.add_on_channel_close_callback()
         self.setup_exchange(self.EXCHANGE)
 
     def add_on_channel_close_callback(self):
         common_global.es_inst.com_elastic_index('info',
-                                                {'ffprobe': 'Adding channel close callback'})
+                                                {'hardware': 'Adding channel close callback'})
         self._channel.add_on_close_callback(self.on_channel_closed)
 
     def on_channel_closed(self, channel, reason):
         common_global.es_inst.com_elastic_index('info', {
-            'ffprobe': ('Channel %i was closed: %s', channel, reason)})
+            'hardware': ('Channel %i was closed: %s', channel, reason)})
         self.close_connection()
 
     def setup_exchange(self, exchange_name):
         common_global.es_inst.com_elastic_index('info', {
-            'ffprobe': ('Declaring exchange: %s', exchange_name)})
+            'hardware': ('Declaring exchange: %s', exchange_name)})
         # Note: using functools.partial is not required, it is demonstrating
         # how arbitrary data can be passed to the callback when it is called
         cb = functools.partial(
@@ -126,18 +126,18 @@ class MKConsumer(object):
 
     def on_exchange_declareok(self, _unused_frame, userdata):
         common_global.es_inst.com_elastic_index('info',
-                                                {'ffprobe': ('Exchange declared: %s', userdata)})
+                                                {'hardware': ('Exchange declared: %s', userdata)})
         self.setup_queue(self.QUEUE)
 
     def setup_queue(self, queue_name):
         common_global.es_inst.com_elastic_index('info',
-                                                {'ffprobe': ('Declaring queue %s', queue_name)})
+                                                {'hardware': ('Declaring queue %s', queue_name)})
         cb = functools.partial(self.on_queue_declareok, userdata=queue_name)
         self._channel.queue_declare(queue=queue_name, callback=cb, durable=True)
 
     def on_queue_declareok(self, _unused_frame, userdata):
         queue_name = userdata
-        common_global.es_inst.com_elastic_index('info', {'ffprobe': ('Binding %s to %s with %s',
+        common_global.es_inst.com_elastic_index('info', {'hardware': ('Binding %s to %s with %s',
                                                                      self.EXCHANGE, queue_name,
                                                                      self.ROUTING_KEY)})
         cb = functools.partial(self.on_bindok, userdata=queue_name)
@@ -148,7 +148,7 @@ class MKConsumer(object):
             callback=cb)
 
     def on_bindok(self, _unused_frame, userdata):
-        common_global.es_inst.com_elastic_index('info', {'ffprobe': ('Queue bound: %s', userdata)})
+        common_global.es_inst.com_elastic_index('info', {'hardware': ('Queue bound: %s', userdata)})
         self.set_qos()
 
     def set_qos(self):
@@ -157,12 +157,12 @@ class MKConsumer(object):
 
     def on_basic_qos_ok(self, _unused_frame):
         common_global.es_inst.com_elastic_index('info', {
-            'ffprobe': ('QOS set to: %d', self._prefetch_count)})
+            'hardware': ('QOS set to: %d', self._prefetch_count)})
         self.start_consuming()
 
     def start_consuming(self):
         common_global.es_inst.com_elastic_index('info', {
-            'ffprobe': 'Issuing consumer related RPC commands'})
+            'hardware': 'Issuing consumer related RPC commands'})
         self.add_on_cancel_callback()
         self._consumer_tag = self._channel.basic_consume(
             self.QUEUE, self.on_message)
@@ -171,12 +171,12 @@ class MKConsumer(object):
 
     def add_on_cancel_callback(self):
         common_global.es_inst.com_elastic_index('info', {
-            'ffprobe': 'Adding consumer cancellation callback'})
+            'hardware': 'Adding consumer cancellation callback'})
         self._channel.add_on_cancel_callback(self.on_consumer_cancelled)
 
     def on_consumer_cancelled(self, method_frame):
         common_global.es_inst.com_elastic_index('info', {
-            'ffprobe': ('Consumer was cancelled remotely, shutting down: %r', method_frame)})
+            'hardware': ('Consumer was cancelled remotely, shutting down: %r', method_frame)})
         if self._channel:
             self._channel.close()
 
@@ -199,14 +199,14 @@ class MKConsumer(object):
 
     def acknowledge_message(self, delivery_tag):
         common_global.es_inst.com_elastic_index('error', {
-            'ffprobe null': ('Acknowledging message %s', delivery_tag)})
+            'hardware': ('Acknowledging message %s', delivery_tag)})
         self._channel.basic_ack(delivery_tag)
 
     def stop_consuming(self):
         if self._channel:
             common_global.es_inst.com_elastic_index('error',
                                                     {
-                                                        'ffprobe null': 'Sending a Basic.Cancel RPC command to RabbitMQ'})
+                                                        'hardware': 'Sending a Basic.Cancel RPC command to RabbitMQ'})
             cb = functools.partial(
                 self.on_cancelok, userdata=self._consumer_tag)
             self._channel.basic_cancel(self._consumer_tag, cb)
@@ -214,11 +214,11 @@ class MKConsumer(object):
     def on_cancelok(self, _unused_frame, userdata):
         self._consuming = False
         common_global.es_inst.com_elastic_index('info', {
-            'ffprobe': ('RabbitMQ acknowledged the cancellation of the consumer: %s', userdata)})
+            'hardware': ('RabbitMQ acknowledged the cancellation of the consumer: %s', userdata)})
         self.close_channel()
 
     def close_channel(self):
-        common_global.es_inst.com_elastic_index('error', {'ffprobe null': 'Closing the channel'})
+        common_global.es_inst.com_elastic_index('error', {'hardware': 'Closing the channel'})
         self._channel.close()
 
     def run(self):
@@ -228,13 +228,13 @@ class MKConsumer(object):
     def stop(self):
         if not self._closing:
             self._closing = True
-            common_global.es_inst.com_elastic_index('error', {'ffprobe null': 'Stopping'})
+            common_global.es_inst.com_elastic_index('error', {'hardware': 'Stopping'})
             if self._consuming:
                 self.stop_consuming()
                 self._connection.ioloop.start()
             else:
                 self._connection.ioloop.stop()
-            common_global.es_inst.com_elastic_index('error', {'ffprobe null': 'Stopped'})
+            common_global.es_inst.com_elastic_index('error', {'hardware': 'Stopped'})
 
     class ReconnectingExampleConsumer(object):
         """This is an example consumer that will reconnect if the nested
@@ -260,7 +260,7 @@ class MKConsumer(object):
                 self._consumer.stop()
                 reconnect_delay = self._get_reconnect_delay()
                 common_global.es_inst.com_elastic_index('error',
-                                                        {'ffprobe': (
+                                                        {'hardware': (
                                                         'Reconnecting after %d seconds',
                                                         reconnect_delay)})
                 time.sleep(reconnect_delay)
