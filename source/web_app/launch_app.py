529b860704b4204702c2fc39eda71f380122efe4
SpootDev <spootdev@gmail.com>
2019-06-25 15:04:07 -0500

    overrie print

--- source/web_app/launch_app.py
+++ source/web_app/launch_app.py
@@ -3,6 +3,8 @@
 
 import os
 import sys
+from common import common_global
+from common import common_logging_elasticsearch
 
 BASE_DIR = os.path.join(os.path.dirname(__file__))
 
@@ -11,4 +13,7 @@ if BASE_DIR not in sys.path:
 
 from MediaKraken.app import create_app
 
+# start logging
+common_global.es_inst = common_logging_elasticsearch.CommonElasticsearch('main_webapp', debug_override='print')
+common_global.es_inst.com_elastic_index('info', {'stuff': 'Creating webapp instance'})
 application = create_app()
