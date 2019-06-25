a706686e51ac82f50f8d3c1424f54e62d4254499
SpootDev <spootdev@gmail.com>
2019-06-25 10:54:35 -0500

    redo es body

--- source/common/common_logging_elasticsearch.py
+++ source/common/common_logging_elasticsearch.py
@@ -20,6 +20,7 @@ import json
 import os
 import sys
 import time
+from datetime import datetime
 
 from elasticsearch import Elasticsearch
 
@@ -48,9 +49,9 @@ class CommonElasticsearch:
             # leave the try....as I don't want the container to fail if mkelk not accepting
             try:
                 self.es_inst.index(index=self.es_index, doc_type='MediaKraken',
-                                   body={"text": {"type": log_type,
-                                                  "data": json.dumps(body_data),
-                                                  "timestamp": time.strftime("%Y%m%d%H%M%S")}})
+                                   body={"type": log_type,
+                                         "data": json.dumps(body_data),
+                                         "timestamp": datetime.now()})
             except:
                 print((log_type, body_data))
         # write log to host syslog
