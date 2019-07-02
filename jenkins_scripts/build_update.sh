a0e864b22648b15dd96569b41bd5513f5cb77704
SpootDev <spootdev@gmail.com>
2019-07-02 14:10:38 -0500

    build to :dev

--- jenkins_scripts/build_update.sh
+++ jenkins_scripts/build_update.sh
@@ -19,77 +19,77 @@ cd ../
 
 # base python 3 images
 cd ../ComposeMediaKrakenBase38Py3
-docker build -t mediakraken/mkbase38py3 --build-arg ALPMIRROR=dl-cdn.alpinelinux.org --build-arg PIPMIRROR=pypi.python.org .
+docker build -t mediakraken/mkbase38py3:dev --build-arg ALPMIRROR=dl-cdn.alpinelinux.org --build-arg PIPMIRROR=pypi.python.org .
 
 cd ../ComposeMediaKrakenBase39Py3
-docker build -t mediakraken/mkbase39py3 --build-arg ALPMIRROR=dl-cdn.alpinelinux.org --build-arg PIPMIRROR=pypi.python.org .
+docker build -t mediakraken/mkbase39py3:dev --build-arg ALPMIRROR=dl-cdn.alpinelinux.org --build-arg PIPMIRROR=pypi.python.org .
 
 cd ../ComposeMediaKrakenBase310Py3
-docker build -t mediakraken/mkbase310py3 --build-arg ALPMIRROR=dl-cdn.alpinelinux.org --build-arg PIPMIRROR=pypi.python.org .
+docker build -t mediakraken/mkbase310py3:dev --build-arg ALPMIRROR=dl-cdn.alpinelinux.org --build-arg PIPMIRROR=pypi.python.org .
 
 # Build the base FFMPEG from base images
 # Image that simply has ffmpeg and ffprobe for use by other containers.
 cd ../ComposeMediaKrakenBaseFFMPEG
-docker build -t mediakraken/mkbaseffmpeg --build-arg ALPMIRROR=dl-cdn.alpinelinux.org --build-arg PIPMIRROR=pypi.python.org .
+docker build -t mediakraken/mkbaseffmpeg:dev --build-arg ALPMIRROR=dl-cdn.alpinelinux.org --build-arg PIPMIRROR=pypi.python.org .
 
 # build the base node
 # Adds NODE to the base ffmpeg and ffprobe.
 cd ../ComposeMediaKrakenBaseNodeFFMPEG
-docker build -t mediakraken/mkbasenodeffmpeg --build-arg ALPMIRROR=dl-cdn.alpinelinux.org --build-arg PIPMIRROR=pypi.python.org .
+docker build -t mediakraken/mkbasenodeffmpeg:dev --build-arg ALPMIRROR=dl-cdn.alpinelinux.org --build-arg PIPMIRROR=pypi.python.org .
 
 # this will include the nvidia cuda/nvec/driver
 cd ../ComposeMediaKrakenBaseFFMPEGUbuntu
-docker build -t mediakraken/mkbaseffmpegubuntu --build-arg PIPMIRROR=pypi.python.org 
+docker build -t mediakraken/mkbaseffmpegubuntu:dev --build-arg PIPMIRROR=pypi.python.org
 
 # build the base node for ubuntu
 # Adds NODE to the base ffmpeg and ffprobe for ubuntu
-cd ../ComposeMediaKrakenBaseNodeFFMPEGUbuntu
+cd ../ComposeMediaKrakenBaseNodeFFMPEGUbuntu:dev
 docker build -t mediakraken/mkbasenodeffmpegubuntu --build-arg PIPMIRROR=pypi.python.org .
 
 # build the base NODE image from alpine 3.9
 cd ../ComposeMediaKrakenBaseNodeJS
-docker build -t mediakraken/mkbasenode --build-arg PIPMIRROR=pypi.python.org .
+docker build -t mediakraken/mkbasenode:dev --build-arg PIPMIRROR=pypi.python.org .
 
 # build the device scan
 # When run it will scan the HOST network for HDHomerun, Chromecast and Roku devices.
 cd ../ComposeMediaKrakenDevicescan
-docker build -t mediakraken/mkdevicescan --build-arg ALPMIRROR=dl-cdn.alpinelinux.org --build-arg PIPMIRROR=pypi.python.org .
+docker build -t mediakraken/mkdevicescan:dev --build-arg ALPMIRROR=dl-cdn.alpinelinux.org --build-arg PIPMIRROR=pypi.python.org .
 
 # build the openldap
 #cd ../ComposeMediaKrakenOpenLDAP
-#docker build -t mediakraken/mkopenldap .
+#docker build -t mediakraken/mkopenldap:dev .
 
 # build the tmdb prefetch
 cd ../ComposeMediaKrakenPrefetchTMDB
-docker build -t mediakraken/mkprefetchtmdb --build-arg ALPMIRROR=dl-cdn.alpinelinux.org --build-arg PIPMIRROR=pypi.python.org .
+docker build -t mediakraken/mkprefetchtmdb:dev --build-arg ALPMIRROR=dl-cdn.alpinelinux.org --build-arg PIPMIRROR=pypi.python.org .
 
 # build the tvmaze prefetch
 cd ../ComposeMediaKrakenPrefetchTVMaze
-docker build -t mediakraken/mkprefetchtvmaze --build-arg ALPMIRROR=dl-cdn.alpinelinux.org --build-arg PIPMIRROR=pypi.python.org .
+docker build -t mediakraken/mkprefetchtvmaze:dev --build-arg ALPMIRROR=dl-cdn.alpinelinux.org --build-arg PIPMIRROR=pypi.python.org .
 
 # build the game metadata
 cd ../ComposeMediaKrakenGameData
-docker build -t mediakraken/mkgamedata --build-arg ALPMIRROR=dl-cdn.alpinelinux.org --build-arg PIPMIRROR=pypi.python.org .
+docker build -t mediakraken/mkgamedata:dev --build-arg ALPMIRROR=dl-cdn.alpinelinux.org --build-arg PIPMIRROR=pypi.python.org .
 
 # Build the base slave images from other base images (basenodeffmpeg)
 cd ../ComposeMediaKrakenSlave
-docker build -t mediakraken/mkslave --build-arg ALPMIRROR=dl-cdn.alpinelinux.org --build-arg PIPMIRROR=pypi.python.org .
+docker build -t mediakraken/mkslave:dev --build-arg ALPMIRROR=dl-cdn.alpinelinux.org --build-arg PIPMIRROR=pypi.python.org .
 
 # Build the base slave images from other base images (basenodeffmpeg) for ubuntu
 cd ../ComposeMediaKrakenSlaveUbuntu
-docker build -t mediakraken/mkslaveubuntu --build-arg PIPMIRROR=pypi.python.org .
+docker build -t mediakraken/mkslaveubuntu:dev --build-arg PIPMIRROR=pypi.python.org .
 
 # Build the base twitch recorder
 cd ../ComposeMediaKrakenTwitchRecordUser
-docker build -t mediakraken/mktwitchrecorduser --build-arg ALPMIRROR=dl-cdn.alpinelinux.org --build-arg PIPMIRROR=pypi.python.org .
+docker build -t mediakraken/mktwitchrecorduser:dev --build-arg ALPMIRROR=dl-cdn.alpinelinux.org --build-arg PIPMIRROR=pypi.python.org .
 
 # Build the castpy
 cd ../ComposeMediaKrakenCastImage
-docker build -t mediakraken/mkcastimage --build-arg ALPMIRROR=dl-cdn.alpinelinux.org --build-arg PIPMIRROR=pypi.python.org .
+docker build -t mediakraken/mkcastimage:dev --build-arg ALPMIRROR=dl-cdn.alpinelinux.org --build-arg PIPMIRROR=pypi.python.org .
 
 # Build the grapesjs
 cd ../ComposeMediaKrakenGrapesJS
-docker build -t mediakraken/mkgrapesjs --build-arg ALPMIRROR=dl-cdn.alpinelinux.org --build-arg PIPMIRROR=pypi.python.org .
+docker build -t mediakraken/mkgrapesjs:dev --build-arg ALPMIRROR=dl-cdn.alpinelinux.org --build-arg PIPMIRROR=pypi.python.org .
 
 # move here so all the "deps" are built first
 docker-compose build --build-arg ALPMIRROR=dl-cdn.alpinelinux.org --build-arg PIPMIRROR=pypi.python.org
@@ -98,31 +98,31 @@ docker-compose build --build-arg ALPMIRROR=dl-cdn.alpinelinux.org --build-arg PI
 
 # build the retroarch web
 cd ../ComposeMediaKrakenRetroArchWeb
-docker build -t mediakraken/mkretroarchweb --build-arg ALPMIRROR=dl-cdn.alpinelinux.org --build-arg PIPMIRROR=pypi.python.org .
+docker build -t mediakraken/mkretroarchweb:dev --build-arg ALPMIRROR=dl-cdn.alpinelinux.org --build-arg PIPMIRROR=pypi.python.org .
 
 # build the dosbox web
 cd ../ComposeMediaKrakenDosBoxWeb
-docker build -t mediakraken/mkdosboxweb --build-arg ALPMIRROR=dl-cdn.alpinelinux.org --build-arg PIPMIRROR=pypi.python.org .
+docker build -t mediakraken/mkdosboxweb:dev --build-arg ALPMIRROR=dl-cdn.alpinelinux.org --build-arg PIPMIRROR=pypi.python.org .
 
 # build the mumble
 cd ../ComposeMediaKrakenMumble
-docker build -t mediakraken/mkmumble --build-arg ALPMIRROR=dl-cdn.alpinelinux.org --build-arg PIPMIRROR=pypi.python.org .
+docker build -t mediakraken/mkmumble:dev --build-arg ALPMIRROR=dl-cdn.alpinelinux.org --build-arg PIPMIRROR=pypi.python.org .
 
 # build the mediabrainz
 cd ../ComposeMediaKrakenMusicBrainz
-docker build -t mediakraken/mkmusicbrainz --build-arg ALPMIRROR=dl-cdn.alpinelinux.org --build-arg PIPMIRROR=pypi.python.org .
+docker build -t mediakraken/mkmusicbrainz:dev --build-arg ALPMIRROR=dl-cdn.alpinelinux.org --build-arg PIPMIRROR=pypi.python.org .
 
 # build the inotify
 cd ../ComposeMediaKrakenInotify
-docker build -t mediakraken/mkinotify --build-arg ALPMIRROR=dl-cdn.alpinelinux.org --build-arg PIPMIRROR=pypi.python.org .
+docker build -t mediakraken/mkinotify:dev --build-arg ALPMIRROR=dl-cdn.alpinelinux.org --build-arg PIPMIRROR=pypi.python.org .
 
 # build the teamspeak
 cd ../ComposeMediaKrakenTeamspeak
-docker build -t mediakraken/mkteamspeak --build-arg ALPMIRROR=dl-cdn.alpinelinux.org --build-arg PIPMIRROR=pypi.python.org .
+docker build -t mediakraken/mkteamspeak:dev --build-arg ALPMIRROR=dl-cdn.alpinelinux.org --build-arg PIPMIRROR=pypi.python.org .
 
 # build the transmission
 cd ../ComposeMediaKrakenTransmission
-docker build -t mediakraken/mktransmission --build-arg ALPMIRROR=dl-cdn.alpinelinux.org --build-arg PIPMIRROR=pypi.python.org .
+docker build -t mediakraken/mktransmission:dev --build-arg ALPMIRROR=dl-cdn.alpinelinux.org --build-arg PIPMIRROR=pypi.python.org .
 
 # nuke old images (commented due to base ffmpeg)
 #../../purge_images_none.sh
