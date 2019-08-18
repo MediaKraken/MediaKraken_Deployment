b30f1821c8fa60304a0cfed99a6491f2d7b5c984
SpootDev <spootdev@gmail.com>
2019-08-17 19:08:54 -0500

    new

---
+++ docker/swarm/mediakraken_update_images.py
@@ -0,0 +1,24 @@
+"""
+  Copyright (C) 2019 Quinn D Granfor <spootdev@gmail.com>
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  version 2, as published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  General Public License version 2 for more details.
+
+  You should have received a copy of the GNU General Public License
+  version 2 along with this program; if not, write to the Free
+  Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+  MA 02110-1301, USA.
+"""
+
+with open('./docker-compose-stack.yml') as file_handle:
+    for line in file_handle:
+        if line.find('image: '):
+            docker_image = line.split('image: ')[1]
+            print(docker_image)
+file_handle.close()
