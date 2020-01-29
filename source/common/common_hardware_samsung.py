2758b1e52119189a04030077391d2eddd2abd5d6
SpootDev <spootdev@gmail.com>
2020-01-29 14:04:30 -0600

    remote ip

--- source/common/common_hardware_samsung.py
+++ source/common/common_hardware_samsung.py
@@ -25,8 +25,8 @@ class CommonHardwareSamsung:
     Class for interfacing with samsung TV equipment over network connection
     """
 
-    def __init__(self, source_ip, device_ip):
-        self.src = source_ip  # ip of remote
+    def __init__(self, source_remote_ip, device_ip):
+        self.src = source_remote_ip  # ip of remote
         self.mac = '00-AB-11-11-11-11'  # mac of remote
         self.remote = 'python remote'  # remote name
         self.dst = device_ip  # ip of tv
