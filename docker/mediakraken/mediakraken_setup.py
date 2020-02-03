6f06698776208da254ef61357bd34313b52a320e
SpootDev <spootdev@gmail.com>
2020-02-02 21:43:37 -0600

    fix secret write

--- docker/mediakraken/mediakraken_setup.py
+++ docker/mediakraken/mediakraken_setup.py
@@ -19,17 +19,18 @@
 import os
 import shlex
 import subprocess
+from base64 import b64encode
 
 if not os.path.isfile('mkstack_db_password.txt'):
     file_handle = open('mkstack_db_password.txt', 'w+')
-    file_handle.write(str(os.urandom(32)))
+    file_handle.write(b64encode(os.urandom(32).decode('utf-8')))
     file_handle.close()
     subprocess.call(shlex.split('docker secret create db_password ./mkstack_db_password.txt'),
                     stdout=subprocess.PIPE, shell=False)
 
 if not os.path.isfile('mkstack_secure_key.txt'):
     file_handle = open('mkstack_secure_key.txt', 'w+')
-    file_handle.write(str(os.urandom(32)))
+    file_handle.write(b64encode(os.urandom(32).decode('utf-8')))
     file_handle.close()
     subprocess.call(shlex.split('docker secret create secure_key ./mkstack_secure_key.txt'),
                     stdout=subprocess.PIPE, shell=False)
