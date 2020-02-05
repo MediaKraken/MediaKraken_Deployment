3e7b807cba3c6092a4afb846dd7632a18210772e
SpootDev <spootdev@gmail.com>
2020-02-05 15:11:41 -0600

    adjust pool

--- source/web_app/MediaKraken/extensions.py
+++ source/web_app/MediaKraken/extensions.py
@@ -11,11 +11,6 @@ from flask_login import LoginManager
 
 login_manager = LoginManager()
 
-from flask_sqlalchemy import SQLAlchemy as _BaseSQLAlchemy
-
-class SQLAlchemy(_BaseSQLAlchemy):
-    def apply_pool_defaults(self, app, options):
-        super(SQLAlchemy, self).apply_pool_defaults(self, app, options)
-        options["pool_pre_ping"] = True
+from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
