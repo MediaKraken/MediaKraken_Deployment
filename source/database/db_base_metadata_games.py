becf45bd574f5587b3e9df005641e9641c44c81e
SpootDev <spootdev@gmail.com>
2020-02-09 17:01:33 -0600

    game info

--- source/database/db_base_metadata_games.py
+++ source/database/db_base_metadata_games.py
@@ -142,7 +142,7 @@ def db_meta_game_image_random(self, return_image_type='Poster'):
                                                  ' from mm_media, mm_metadata_game_software_info'
                                                  ' where mm_media_metadata_guid = gi_id'
                                                  ' and ('
-                                                 'mm_metadata_localimage_json->\'Images\'->\'thegamesdb\'->>\''
+                                                 'gi_game_info_json->\'Images\'->\'thegamesdb\'->>\''
                            + return_image_type + '\'' + ')::text != \'null\''
                                                         ' order by random() limit 1')
     try:
