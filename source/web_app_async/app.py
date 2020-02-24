12f141c62dccd991a6f327f54f7a46be92b6d495
SpootDev <spootdev@gmail.com>
2020-02-24 13:56:36 -0600

    add public bp

--- source/web_app_async/app.py
+++ source/web_app_async/app.py
@@ -2,6 +2,8 @@ import hashlib
 import os
 
 import asyncpg
+from blueprint import bp_about
+from blueprint import bp_homepage
 from common import common_file
 from sanic import Sanic
 from sanic import response
@@ -9,6 +11,10 @@ from sanic_httpauth import HTTPBasicAuth
 
 app = Sanic(__name__)
 auth = HTTPBasicAuth()
+# setup the blueprints
+app.blueprint(bp_about)
+app.blueprint(bp_homepage)
+
 db_connection = None
 
 
@@ -42,6 +48,7 @@ def verify_password(username, password):
     if username in users:
         return users.get(username) == hash_password(app_salt, password)
     return False
+
 
 # @auth.login_required
 @app.route("/")
