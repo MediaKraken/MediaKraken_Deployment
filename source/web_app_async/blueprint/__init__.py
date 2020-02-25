0cedbfcf51c772e12b43ef2b813d460808534c10
SpootDev <spootdev@gmail.com>
2020-02-24 23:30:50 -0600

    name

--- source/web_app_async/blueprint/__init__.py
+++ source/web_app_async/blueprint/__init__.py
@@ -1,5 +1,5 @@
 from sanic import Blueprint
 
-from .public import public_content
+from .public import blueprint_public_content
 
-content_mediakraken = Blueprint.group(public_content, url_prefix='/')
+content_mediakraken = Blueprint.group(blueprint_public_content, url_prefix='/')
