47bd7f9326c5d81de1c39ad80c2c2aac54a5662b
SpootDev <spootdev@gmail.com>
2020-03-04 21:04:26 -0600

    add admin shells

--- source/web_app_sanic/blueprint/admin/__init__.py
+++ source/web_app_sanic/blueprint/admin/__init__.py
@@ -6,7 +6,7 @@ from .bp_admin_docker import blueprint_admin_docker
 from .bp_admin_library import blueprint_admin_library
 from .bp_admin_users import blueprint_admin_users
 
-blueprint_public_content = Blueprint.group(blueprint_admin_backup,
+blueprint_admin_content = Blueprint.group(blueprint_admin_backup,
                                            blueprint_admin_cron,
                                            blueprint_admin_docker,
                                            blueprint_admin_library,
