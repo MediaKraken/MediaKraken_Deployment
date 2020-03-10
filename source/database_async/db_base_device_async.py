c22ab382a4afd2d7de79fb05827f235ed89ed447
SpootDev <spootdev@gmail.com>
2020-03-10 15:15:52 -0500

    device db calls

---
+++ source/database_async/db_base_device_async.py
@@ -0,0 +1,55 @@
+import uuid
+
+
+def db_device_check(db_connection, device_type, device_name, device_ip):
+    """
+    Check to see if device exists already on db
+    """
+    db_connection.fetchval(
+        'select count(*) from mm_device'
+        ' where mm_device_type = %s mm_device_json->\'Name\' ? %s'
+        ' and mm_device_json->\'IP\' ? %s', (device_type, device_name, device_ip))
+
+
+def db_device_delete(db_connection, guid):
+    """
+    Remove a device from the database via uuid
+    """
+    db_connection.execute(
+        'delete from mm_device'
+        ' where mm_device_id = %s', (guid,))
+
+
+def db_device_list(db_connection, device_type=None, offset=0, records=None, search_value=None):
+    """
+    Return list of devices in database
+    """
+    if device_type is None:
+        return db_connection.fetch('select mm_device_id,'
+                                   ' mm_device_type,'
+                                   ' mm_device_json'
+                                   ' from mm_device'
+                                   ' order by mm_device_type'
+                                   ' offset %s limit %s', (offset, records))
+    else:
+        return db_connection.fetch('select mm_device_id,'
+                                   ' mm_device_type,'
+                                   ' mm_device_json'
+                                   ' from mm_device'
+                                   ' where mm_device_type = %s offset %s limit %s',
+                                   (device_type, offset, records))
+
+
+def db_device_upsert(db_connection, device_type, device_json):
+    """
+    Upsert a device into the database
+    """
+    new_guid = str(uuid.uuid4())
+    db_connection.execute('INSERT INTO mm_device (mm_device_id,'
+                          ' mm_device_type,'
+                          ' mm_device_json)'
+                          ' VALUES (%s, %s, %s)'
+                          ' ON CONFLICT ((mm_device_json->>"IP"))'
+                          ' DO UPDATE SET mm_device_type = %s, mm_device_json = %s',
+                          (new_guid, device_type, device_json, device_type, device_json))
+    return new_guid
--- source/web_app_sanic/blueprint/admin/bp_admin_hardware.py
+++ source/web_app_sanic/blueprint/admin/bp_admin_hardware.py
@@ -1,5 +1,6 @@
 import json
 
+import database_async as database_base_async
 from common import common_global
 from common import common_network_pika
 from sanic import Blueprint
@@ -21,24 +22,26 @@ async def url_bp_admin_hardware(request):
                                               route_key='mkhardware')
         flash("Scheduled hardware scan.")
     chromecast_list = []
-    for row_data in g.db_connection.db_device_list('Chromecast'):
-        if row_data['mm_device_json']['Model'] == 'Eureka Dongle':
-            device_model = 'Chromecast'
-        else:
-            device_model = row_data['mm_device_json']['Model']
-        chromecast_list.append((row_data['mm_device_id'],
-                                row_data['mm_device_json']['Name'],
-                                device_model,
-                                row_data['mm_device_json']['IP'],
-                                True))
+    async with request.app.db_pool.acquire() as db_connection:
+        for row_data in await database_base_async.db_device_list(db_connection, 'Chromecast'):
+            if row_data['mm_device_json']['Model'] == 'Eureka Dongle':
+                device_model = 'Chromecast'
+            else:
+                device_model = row_data['mm_device_json']['Model']
+            chromecast_list.append((row_data['mm_device_id'],
+                                    row_data['mm_device_json']['Name'],
+                                    device_model,
+                                    row_data['mm_device_json']['IP'],
+                                    True))
     tv_tuners = []
-    for row_data in g.db_connection.db_device_list('tvtuner'):
-        tv_tuners.append((row_data['mm_device_id'], row_data['mm_device_json']['HWModel']
-                          + " (" + row_data['mm_device_json']['Model'] + ")",
-                          row_data['mm_device_json']['IP'],
-                          len(row_data['mm_device_json']['Channels']),
-                          row_data['mm_device_json']['Firmware'],
-                          row_data['mm_device_json']['Active']))
+    async with request.app.db_pool.acquire() as db_connection:
+        for row_data in await database_base_async.db_device_list(db_connection, 'tvtuner'):
+            tv_tuners.append((row_data['mm_device_id'], row_data['mm_device_json']['HWModel']
+                              + " (" + row_data['mm_device_json']['Model'] + ")",
+                              row_data['mm_device_json']['IP'],
+                              len(row_data['mm_device_json']['Channels']),
+                              row_data['mm_device_json']['Firmware'],
+                              row_data['mm_device_json']['Active']))
     return {
         'data_chromecast': chromecast_list,
         'data_tuners': tv_tuners,
@@ -51,8 +54,8 @@ async def url_bp_admin_hardware_chromecast_delete(request):
     """
     Delete action 'page'
     """
-    g.db_connection.db_device_delete(request.form['id'])
-    g.db_connection.db_commit()
+    async with request.app.db_pool.acquire() as db_connection:
+        await database_base_async.db_device_delete(db_connection, request.form['id'])
     return json.dumps({'status': 'OK'})
 
 
@@ -68,18 +71,20 @@ async def url_bp_admin_hardware_chromecast_edit(request):
         if form.validate_on_submit():
             if request.form['action_type'] == 'Add':
                 # verify it doesn't exit and add
-                if g.db_connection.db_device_check(request.form['name'],
-                                                   request.form['ipaddr']) == 0:
-                    g.db_connection.db_device_insert('cast',
-                                                     json.dumps({'Name': request.form['name'],
-                                                                 'Model': "NA",
-                                                                 'IP': request.form['ipaddr']}))
-                    g.db_connection.db_commit()
-                    return redirect(request.app.url_for('admins_chromecasts.admin_chromecast'))
-                else:
-                    flash("Chromecast already in database.", 'error')
-                    return redirect(
-                        request.app.url_for('admins_chromecasts.admin_chromecast_edit_page'))
+                async with request.app.db_pool.acquire() as db_connection:
+                    if await database_base_async.db_device_check(db_connection,
+                                                                 request.form['name'],
+                                                                 request.form['ipaddr']) == 0:
+                        database_base_async.db_device_upsert(db_connection, 'cast',
+                                                             json.dumps(
+                                                                 {'Name': request.form['name'],
+                                                                  'Model': "NA",
+                                                                  'IP': request.form['ipaddr']}))
+                        return redirect(request.app.url_for('admins_chromecasts.admin_chromecast'))
+                    else:
+                        flash("Chromecast already in database.", 'error')
+                        return redirect(
+                            request.app.url_for('admins_chromecasts.admin_chromecast_edit_page'))
         else:
             flash_errors(form)
     return {
@@ -99,17 +104,20 @@ async def url_bp_admin_hardware_tvtuner_edit(request):
         if form.validate_on_submit():
             if request.form['action_type'] == 'Add':
                 # verify it doesn't exit and add
-                if g.db_connection.db_device_check(request.form['name'],
-                                                   request.form['ipaddr']) == 0:
-                    g.db_connection.db_device_insert('tvtuner',
-                                                     json.dumps({'Name': request.form['name'],
-                                                                 'Model': "NA",
-                                                                 'IP': request.form['ipaddr']}))
-                    g.db_connection.db_commit()
-                    return redirect(request.app.url_for('admins_tvtuners.admin_tvtuners'))
-                else:
-                    flash("TV Tuner already in database.", 'error')
-                    return redirect(request.app.url_for('admins_tvtuners.admin_tuner_edit_page'))
+                async with request.app.db_pool.acquire() as db_connection:
+                    if await database_base_async.db_device_check(db_connection,
+                                                                 request.form['name'],
+                                                                 request.form['ipaddr']) == 0:
+                        database_base_async.db_device_upsert(db_connection, 'tvtuner',
+                                                             json.dumps(
+                                                                 {'Name': request.form['name'],
+                                                                  'Model': "NA",
+                                                                  'IP': request.form['ipaddr']}))
+                        return redirect(request.app.url_for('admins_tvtuners.admin_tvtuners'))
+                    else:
+                        flash("TV Tuner already in database.", 'error')
+                        return redirect(
+                            request.app.url_for('admins_tvtuners.admin_tuner_edit_page'))
         else:
             flash_errors(form)
     return {
@@ -123,5 +131,6 @@ async def url_bp_admin_hardware_tvtuner_delete(request):
     """
     Delete action 'page'
     """
-    g.db_connection.db_device_delete(request.form['id'])
+    async with request.app.db_pool.acquire() as db_connection:
+        await database_base_async.db_device_delete(db_connection, request.form['id'])
     return json.dumps({'status': 'OK'})
