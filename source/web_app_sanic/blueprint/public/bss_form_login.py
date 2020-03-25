0e911d10f4bdd5afd1b0a5733621d4ee0fcfc7e7
SpootDev <spootdev@gmail.com>
2020-03-25 16:30:40 -0500

    sanicwtf with action instead

--- source/web_app_sanic/blueprint/public/bss_form_login.py
+++ source/web_app_sanic/blueprint/public/bss_form_login.py
@@ -1,9 +1,9 @@
-from wtforms import Form
+from sanic_wtf import SanicForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, EqualTo
 
 
-class BSSLoginForm(Form):
+class BSSLoginForm(SanicForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
