1f98d37a8462688a7c708abc95f7c1b9b3b238c0
SpootDev <spootdev@gmail.com>
2020-03-26 16:23:19 -0500

    link

--- source/web_app_sanic/blueprint/user/bp_user_homepage.py
+++ source/web_app_sanic/blueprint/user/bp_user_homepage.py
@@ -5,7 +5,7 @@ blueprint_user_homepage = Blueprint('name_blueprint_user_homepage', url_prefix='
 
 
 @blueprint_user_homepage.route('/home', methods=['GET', 'POST'])
-@common_global.jinja_template.template('user/user_home.html')
+@common_global.jinja_template.template('bss_user/bss_user_home.html')
 @common_global.auth.login_required
 async def url_bp_user_homepage(request):
     """
