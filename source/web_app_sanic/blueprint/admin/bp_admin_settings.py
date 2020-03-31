7df46c136df9e58aef91ee4bdaf07b89e52f1b15
SpootDev <spootdev@gmail.com>
2020-03-31 17:52:28 -0500

    fix title

--- source/web_app_sanic/blueprint/admin/bp_admin_settings.py
+++ source/web_app_sanic/blueprint/admin/bp_admin_settings.py
@@ -4,7 +4,7 @@ from sanic import Blueprint
 blueprint_admin_settings = Blueprint('name_blueprint_admin_settings', url_prefix='/admin')
 
 
-@blueprint_admin_settings.route("/admin_cloud", methods=["GET", "POST"])
+@blueprint_admin_settings.route("/admin_settings", methods=["GET", "POST"])
 @common_global.jinja_template.template('bss_admin/bss_admin_settings.html')
 @common_global.auth.login_required
 async def url_bp_admin_settings(request):
