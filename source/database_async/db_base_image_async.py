ab108baf76df76e3995274f9dfa42b138ecb04ba
SpootDev <spootdev@gmail.com>
2020-04-05 20:36:23 -0500

    fix read of extra table

--- source/database_async/db_base_image_async.py
+++ source/database_async/db_base_image_async.py
@@ -2,20 +2,14 @@ async def db_image_count(self, db_connection, class_guid, search_value=None):
     """
     Image list count
     """
-    return await db_connection.fetchval('select count(*) from mm_media,'
-                                        'mm_media.mm_media_class_guid'
-                                        ' where mm_media.mm_media_class_guid'
-                                        ' = mm_media_class.mm_media_class_guid'
-                                        ' and mm_media_class_guid = $1', class_guid)
+    return await db_connection.fetchval('select count(*) from mm_media'
+                                        ' where mm_media_class_guid = $1', class_guid)
 
 
 async def db_image_list(self, db_connection, class_guid, offset=0, records=None, search_value=None):
     """
     Image list
     """
-    return await db_connection.fetch('select mm_media_path from mm_media,'
-                                     'mm_media.mm_media_class_guid'
-                                     ' where mm_media.mm_media_class_guid'
-                                     ' = mm_media_class.mm_media_class_guid'
-                                     ' and mm_media_class_guid = $1 offset $2 limit $3',
+    return await db_connection.fetch('select mm_media_path from mm_media'
+                                     ' where mm_media_class_guid = $1 offset $2 limit $3',
                                      class_guid, offset, records)
