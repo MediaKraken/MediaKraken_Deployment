c57ff477deb9ad7264a4cbd93839cec2c2e5bf07
SpootDev <spootdev@gmail.com>
2020-04-05 19:02:47 -0500

    fix class field

--- source/database_async/db_base_image_async.py
+++ source/database_async/db_base_image_async.py
@@ -3,7 +3,7 @@ async def db_image_count(self, db_connection, class_guid, search_value=None):
     Image list count
     """
     return await db_connection.fetchval('select count(*) from mm_media,'
-                                        'mm_media_class'
+                                        'mm_media_class_guid'
                                         ' where mm_media.mm_media_class_guid'
                                         ' = mm_media_class.mm_media_class_guid'
                                         ' and mm_media_class_guid = $1', class_guid)
@@ -14,7 +14,7 @@ async def db_image_list(self, db_connection, class_guid, offset=0, records=None,
     Image list
     """
     return await db_connection.fetch('select mm_media_path from mm_media,'
-                                     'mm_media_class'
+                                     'mm_media_class_guid'
                                      ' where mm_media.mm_media_class_guid'
                                      ' = mm_media_class.mm_media_class_guid'
                                      ' and mm_media_class_guid = $1 offset $2 limit $3',
