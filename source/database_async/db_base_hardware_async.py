8128f74f304b6373b16f0d139d610c98946744ec
SpootDev <spootdev@gmail.com>
2020-04-05 19:26:36 -0500

    fix table name

--- source/database_async/db_base_hardware_async.py
+++ source/database_async/db_base_hardware_async.py
@@ -3,11 +3,11 @@ async def db_hardware_device_count(self, db_connection, hardware_manufacturer, m
     Return json for machine/model
     """
     if model_name is None:
-        return await db_connection.fetchval('select count(*) from mm_hardware_json'
-                                            ' where mm_hardware_manufacturer %% $1',
+        return await db_connection.fetchval('select count(*) from mm_hardware'
+                                            ' where mm_hardware_manufacturer = $1',
                                             hardware_manufacturer)
     else:
-        return await db_connection.fetchval('select count(*) from mm_hardware_json'
-                                            ' where mm_hardware_manufacturer %% $1'
-                                            ' and mm_hardware_model %% $2',
+        return await db_connection.fetchval('select count(*) from mm_hardware'
+                                            ' where mm_hardware_manufacturer = $1'
+                                            ' and mm_hardware_model = $2',
                                             hardware_manufacturer, model_name)
