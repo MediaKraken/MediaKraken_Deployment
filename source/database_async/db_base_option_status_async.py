7ca51b73c2837e4b3dcbc16851d76e81d98b7735
SpootDev <spootdev@gmail.com>
2020-04-07 15:55:11 -0500

    debug

--- source/database_async/db_base_option_status_async.py
+++ source/database_async/db_base_option_status_async.py
@@ -15,9 +15,11 @@ async def db_opt_json_read(self, db_connection):
     """
     Read options
     """
-    return json.loads(await db_connection.fetchval(
+    json_result = await db_connection.fetchval(
         'select mm_options_json'
-        ' from mm_options_and_status')['mm_options_json'])
+        ' from mm_options_and_status')
+    print(json_result, flush=True)
+    return json.loads(json_result)
 
 
 async def db_status_json_read(self, db_connection):
