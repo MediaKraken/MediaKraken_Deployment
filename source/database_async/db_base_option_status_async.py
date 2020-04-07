27a527b701516585c9afe044317efe80d2411449
SpootDev <spootdev@gmail.com>
2020-04-07 15:58:54 -0500

    json.loads not needed

--- source/database_async/db_base_option_status_async.py
+++ source/database_async/db_base_option_status_async.py
@@ -15,17 +15,17 @@ async def db_opt_json_read(self, db_connection):
     """
     Read options
     """
-    json_result = await db_connection.fetchval(
+    # comes in as DICT
+    return await db_connection.fetchval(
         'select mm_options_json'
         ' from mm_options_and_status')
-    print(json_result, flush=True)
-    return json.loads(json_result)
 
 
 async def db_status_json_read(self, db_connection):
     """
     Read options
     """
-    return json.loads(await db_connection.fetchval(
+    # comes in as DICT
+    return await db_connection.fetchval(
         'select mm_status_json'
-        ' from mm_options_and_status')['mm_status_json'])
+        ' from mm_options_and_status')
