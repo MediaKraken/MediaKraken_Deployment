e20b8b657074cd81bffaa801b20125120f7e1b76
SpootDev <spootdev@gmail.com>
2020-04-13 13:20:49 -0500

    fix exchange name

--- source/common/common_network_amqp_async.py
+++ source/common/common_network_amqp_async.py
@@ -7,7 +7,7 @@ async def com_net_amqp_send(body_data, rabbit_host_name='mkstack_rabbitmq',
                             exchange_name='mkque_ex', route_key='mkque'):
     transport, protocol = await aioamqp.connect(host=rabbit_host_name)
     channel = await protocol.channel()
-    await channel.exchange_declare(exchange_name='exchange', type_name='direct', durable=True)
+    await channel.exchange_declare(exchange_name=exchange_name, type_name='direct', durable=True)
     await channel.basic_publish(
         payload=json.dumps(body_data),
         exchange_name=exchange_name,
