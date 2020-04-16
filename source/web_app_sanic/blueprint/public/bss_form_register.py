3e89aebe2c720a84975076f8a707c295055a5a05
spootdev <spootdev@gmail.com>
2020-04-15 20:48:47 -0500

    more verification

--- source/web_app_sanic/blueprint/public/bss_form_register.py
+++ source/web_app_sanic/blueprint/public/bss_form_register.py
@@ -1,12 +1,14 @@
 from sanic_wtf import SanicForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, EqualTo
+from wtforms.validators import DataRequired, EqualTo, Email, Length
 
 
 class BSSRegisterForm(SanicForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=25)])
+    email = StringField('Email Address', validators=[DataRequired(), Email(), Length(min=4, max=25)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6, max=25)])
+    password2 = PasswordField('Repeat Password',
+                              validators=[DataRequired(), EqualTo('password', message='Passwords must match')])
     submit = SubmitField('Register')
 
     def __init__(self, *args, **kwargs):
