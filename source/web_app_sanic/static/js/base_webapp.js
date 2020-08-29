059d9fc6113cf0d0992b73600d6cc15b7578cbc8
SpootDev <spootdev@gmail.com>
2020-08-28 19:55:03 -0500

    image paths

--- source/web_app_sanic/static/js/base_webapp.js
+++ source/web_app_sanic/static/js/base_webapp.js
@@ -21,16 +21,16 @@ $(function(){
             });
         },
         items: {
-            "watched": {name: "Set Watched", icon: "/static/images/eye.png"},
-            "sync": {name: "Sync Media", icon: "/static/images/synced.jpg"},
-            "towatch": {name: "Add to Watch Queue", icon: "/static/images/rectangles.png"},
+            "watched": {name: "Set Watched", icon: "/static/img/eye.png"},
+            "sync": {name: "Sync Media", icon: "/static/img/synced.jpg"},
+            "towatch": {name: "Add to Watch Queue", icon: "/static/img/rectangles.png"},
             "sep1": "---------",
-            "favorite": {name: "Set Favorite", icon: "/static/images/favorite-mark.png"},
-            "like": {name: "Set Upvote", icon: "/static/images/thumbs-up.png"},
-            "dislike": {name: "Set Downvote", icon: "/static/images/dislike-thumb.png"},
-            "poo": {name: "Set Avoid", icon: "/static/images/pile-of-dung.png"},
+            "favorite": {name: "Set Favorite", icon: "/static/img/favorite-mark.png"},
+            "like": {name: "Set Upvote", icon: "/static/img/thumbs-up.png"},
+            "dislike": {name: "Set Downvote", icon: "/static/img/dislike-thumb.png"},
+            "poo": {name: "Set Avoid", icon: "/static/img/pile-of-dung.png"},
             "sep2": "---------",
-            "mismatch": {name: "Set Mismatch", icon: "/static/images/exclamation-circle-frame.png"},
+            "mismatch": {name: "Set Mismatch", icon: "/static/img/exclamation-circle-frame.png"},
         }
     });
 });
@@ -59,15 +59,15 @@ $(function(){
             });
         },
         items: {
-            "watched": {name: "Set Watched", icon: "/static/images/eye.png"},
-            "towatch": {name: "Add to Watch Queue", icon: "/static/images/rectangles.png"},
+            "watched": {name: "Set Watched", icon: "/static/img/eye.png"},
+            "towatch": {name: "Add to Watch Queue", icon: "/static/img/rectangles.png"},
             "sep1": "---------",
-            "favorite": {name: "Set Favorite", icon: "/static/images/favorite-mark.png"},
-            "like": {name: "Set Upvote", icon: "/static/images/thumbs-up.png"},
-            "dislike": {name: "Set Downvote", icon: "/static/images/dislike-thumb.png"},
-            "poo": {name: "Set Avoid", icon: "/static/images/pile-of-dung.png"},
+            "favorite": {name: "Set Favorite", icon: "/static/img/favorite-mark.png"},
+            "like": {name: "Set Upvote", icon: "/static/img/thumbs-up.png"},
+            "dislike": {name: "Set Downvote", icon: "/static/img/dislike-thumb.png"},
+            "poo": {name: "Set Avoid", icon: "/static/img/pile-of-dung.png"},
             "sep1": "---------",
-            "request": {name: "Request Media", icon: "/static/images/add-button.png"},
+            "request": {name: "Request Media", icon: "/static/img/add-button.png"},
         }
     });
 });
@@ -97,26 +97,26 @@ $(function(){
             window.console && console.log(m) || alert(m);
         },
         items: {
-            "watched": {name: "Set Episode Watched", icon: "/static/images/eye.png"},
-            "watched_season": {name: "Set Season Watched", icon: "/static/images/eye.png"},
-            "watched_show": {name: "Set Show Watched", icon: "/static/images/eye.png"},
-            "sync": {name: "Sync Episdoe Media", icon: "/static/images/synced.jpg"},
-            "sync_season": {name: "Sync Season Media", icon: "/static/images/synced.jpg"},
-            "sync_show": {name: "Sync Show Media", icon: "/static/images/synced.jpg"},
-            "towatch": {name: "Add to Watch Queue", icon: "/static/images/rectangles.png"},
-            "towatch_season": {name: "Add Season to Watch Queue", icon: "/static/images/rectangles.png"},
-            "towatch_show": {name: "Add Show to Watch Queue", icon: "/static/images/rectangles.png"},
+            "watched": {name: "Set Episode Watched", icon: "/static/img/eye.png"},
+            "watched_season": {name: "Set Season Watched", icon: "/static/img/eye.png"},
+            "watched_show": {name: "Set Show Watched", icon: "/static/img/eye.png"},
+            "sync": {name: "Sync Episdoe Media", icon: "/static/img/synced.jpg"},
+            "sync_season": {name: "Sync Season Media", icon: "/static/img/synced.jpg"},
+            "sync_show": {name: "Sync Show Media", icon: "/static/img/synced.jpg"},
+            "towatch": {name: "Add to Watch Queue", icon: "/static/img/rectangles.png"},
+            "towatch_season": {name: "Add Season to Watch Queue", icon: "/static/img/rectangles.png"},
+            "towatch_show": {name: "Add Show to Watch Queue", icon: "/static/img/rectangles.png"},
             "sep1": "---------",
-            "favorite": {name: "Set Episdoe Favorite", icon: "/static/images/favorite-mark.png"},
-            "favorite_season": {name: "Set Season Favorite", icon: "/static/images/favorite-mark.png"},
-            "favorite_show": {name: "Set Show Favorite", icon: "/static/images/favorite-mark.png"},
-            "poo": {name: "Set Episode Downvote", icon: "/static/images/pile-of-dung.png"},
-            "poo_season": {name: "Set Season Downvote", icon: "/static/images/pile-of-dung.png"},
-            "poo_show": {name: "Set Show Downvote", icon: "/static/images/pile-of-dung.png"},
+            "favorite": {name: "Set Episdoe Favorite", icon: "/static/img/favorite-mark.png"},
+            "favorite_season": {name: "Set Season Favorite", icon: "/static/img/favorite-mark.png"},
+            "favorite_show": {name: "Set Show Favorite", icon: "/static/img/favorite-mark.png"},
+            "poo": {name: "Set Episode Downvote", icon: "/static/img/pile-of-dung.png"},
+            "poo_season": {name: "Set Season Downvote", icon: "/static/img/pile-of-dung.png"},
+            "poo_show": {name: "Set Show Downvote", icon: "/static/img/pile-of-dung.png"},
             "sep2": "---------",
-            "mismatch": {name: "Set Episode Mismatch", icon: "/static/images/exclamation-circle-frame.png"},
-            "mismatch_season": {name: "Set Season Mismatch", icon: "/static/images/exclamation-circle-frame.png"},
-            "mismatch_show": {name: "Set Show Mismatch", icon: "/static/images/exclamation-circle-frame.png"},
+            "mismatch": {name: "Set Episode Mismatch", icon: "/static/img/exclamation-circle-frame.png"},
+            "mismatch_season": {name: "Set Season Mismatch", icon: "/static/img/exclamation-circle-frame.png"},
+            "mismatch_show": {name: "Set Show Mismatch", icon: "/static/img/exclamation-circle-frame.png"},
         }
     });
 });
