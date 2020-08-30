0666ff28dcc77a094d3e8d3bd1da104ab868cd70
SpootDev <spootdev@gmail.com>
2020-08-30 12:58:28 -0500

    $1 instead of %s

--- source/database_async/db_base_metadata_movie_async.py
+++ source/database_async/db_base_metadata_movie_async.py
@@ -93,7 +93,7 @@ async def db_meta_movie_json_update(self, db_connection, media_guid, metadata_js
     # update the metadata json
     """
     await db_connection.execute('update mm_metadata_movie'
-                                ' set mm_metadata_user_json = %s'
-                                ' where mm_metadata_guid = %s',
+                                ' set mm_metadata_user_json = $1'
+                                ' where mm_metadata_guid = $2',
                                 (metadata_json, media_guid))
     await db_connection.db_commit()
