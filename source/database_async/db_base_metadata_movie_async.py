fee78c086bcaa45c70e60446bd1fffcdbb073ccb
SpootDev <spootdev@gmail.com>
2020-08-30 13:09:55 -0500

    self

--- source/database_async/db_base_metadata_movie_async.py
+++ source/database_async/db_base_metadata_movie_async.py
@@ -96,4 +96,4 @@ async def db_meta_movie_json_update(self, db_connection, media_guid, metadata_js
                                 ' set mm_metadata_user_json = $1'
                                 ' where mm_metadata_guid = $2',
                                 metadata_json, media_guid)
-    await db_connection.db_commit()
+    await self.db_commit()
