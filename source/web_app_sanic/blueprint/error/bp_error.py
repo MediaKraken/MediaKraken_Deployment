0b29aaeee7765607e3c6da3fac3c8ea4cf234fd7
SpootDev <spootdev@gmail.com>
2020-09-02 22:05:10 -0500

    set the four types of errors

--- source/web_app_sanic/blueprint/error/bp_error.py
+++ source/web_app_sanic/blueprint/error/bp_error.py
@@ -4,7 +4,25 @@ from sanic import Blueprint
 blueprint_error = Blueprint('name_blueprint_error', url_prefix='/error')
 
 
-@blueprint_error.route('/error', methods=["GET"])
-@common_global.jinja_template.template('error/error.html')
-async def url_bp_public_error(request):
+@blueprint_error.route('/error401', methods=["GET"])
+@common_global.jinja_template.template('bss_error/bss_error_401.html')
+async def url_bp_public_error_401(request):
+    return {}
+
+
+@blueprint_error.route('/error403', methods=["GET"])
+@common_global.jinja_template.template('bss_error/bss_error_403.html')
+async def url_bp_public_error_403(request):
+    return {}
+
+
+@blueprint_error.route('/error404', methods=["GET"])
+@common_global.jinja_template.template('bss_error/bss_error_404.html')
+async def url_bp_public_error_404(request):
+    return {}
+
+
+@blueprint_error.route('/error500', methods=["GET"])
+@common_global.jinja_template.template('bss_error/bss_error_500.html')
+async def url_bp_public_error_500(request):
     return {}
