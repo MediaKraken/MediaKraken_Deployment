41d86a32fc28d314bbe97e088489e96d61c36662
SpootDev <spootdev@gmail.com>
2020-09-06 16:54:21 -0500

    prints

--- source/metadata/metadata_provider_themoviedb.py
+++ source/metadata/metadata_provider_themoviedb.py
@@ -68,6 +68,7 @@ def movie_fetch_save_tmdb(db_connection, tmdb_id, metadata_uuid):
     common_global.es_inst.com_elastic_index('info', {"meta movie tmdb save fetch": tmdb_id})
     # fetch and save json data via tmdb id
     result_json = common_global.api_instance.com_tmdb_metadata_by_id(tmdb_id)
+    print('return movie_fetch_save_tmdb:', result_json)
     if result_json is not None:
         common_global.es_inst.com_elastic_index('info', {"meta movie code": result_json.status_code,
                                                          "header": result_json.headers})
