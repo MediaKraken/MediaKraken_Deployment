3027b4d24a27a716eb9e4757c4eb619791320b6a
SpootDev <spootdev@gmail.com>
2020-09-12 15:08:28 -0500

    async mode ability

--- source/common/common_logging_elasticsearch.py
+++ source/common/common_logging_elasticsearch.py
@@ -16,12 +16,12 @@
   MA 02110-1301, USA.
 """
 
-import json
 import os
 import sys
 import time
 from datetime import datetime
 
+from elasticsearch import AsyncElasticsearch
 from elasticsearch import Elasticsearch
 
 
@@ -30,12 +30,16 @@ class CommonElasticsearch:
     Class for interfacing with Elasticsearch or docker logging directly
     """
 
-    def __init__(self, index_type='mediakraken', es_host='th-elk-1.beaverbay.local', es_port=9200,
-                 debug_override=None):
+    def __init__(self, index_type='mediakraken', es_host='th-elk-1.beaverbay.local',
+                 es_port=9200, debug_override=None, async_mode=False):
+        self.async_mode = async_mode
         if 'DEBUG' in os.environ and debug_override is None:
             self.debug = os.environ['DEBUG'].lower()
             if self.debug == 'es':
-                self.es_inst = Elasticsearch([{'host': es_host, 'port': es_port}])
+                if self.async_mode:
+                    self.es_inst = AsyncElasticsearch([{'host': es_host, 'port': es_port}])
+                else:
+                    self.es_inst = Elasticsearch([{'host': es_host, 'port': es_port}])
                 self.es_index = index_type
         else:
             self.debug = debug_override
@@ -48,31 +52,31 @@ class CommonElasticsearch:
         elif self.debug == 'es':
             # leave the try....as I don't want the container to fail if mkelk not accepting
             try:
-                self.es_inst.index(index=self.es_index, doc_type='MediaKraken',
-                                   body={"type": log_type,
-                                         "data": json.dumps(body_data),
-                                         "timestamp": datetime.now()})
+                if self.async_mode:
+                    await self.es_inst.index(index=self.es_index, doc_type='MediaKraken',
+                                             body={"type": log_type,
+                                                   "data": str(body_data),
+                                                   "timestamp": datetime.now()})
+                else:
+                    self.es_inst.index(index=self.es_index, doc_type='MediaKraken',
+                                       body={"type": log_type,
+                                             "data": str(body_data),
+                                             "timestamp": datetime.now()})
             except:
                 print((log_type, body_data), flush=True)
         # write log to host syslog
         elif self.debug == 'sys':
-            try:
-                sys.stdout.write(str({"type": log_type,
-                                      "data": json.dumps(body_data),
-                                      "timestamp": time.strftime("%Y%m%d%H%M%S")}))
-            except:
-                sys.stdout.write(str({"type": log_type,
-                                      "timestamp": time.strftime("%Y%m%d%H%M%S")}))
+            sys.stdout.write(str({"type": log_type,
+                                  "data": str(body_data),
+                                  "timestamp": time.strftime("%Y%m%d%H%M%S")}))
         # write log to host syslog
         elif self.debug == 'print':
-            try:
-                print(str({"type": log_type,
-                           "data": json.dumps(body_data),
-                           "timestamp": time.strftime("%Y%m%d%H%M%S")}), flush=True)
-            except:
-                print(str({"type": log_type,
-                           "data": str(body_data),
-                           "timestamp": time.strftime("%Y%m%d%H%M%S")}), flush=True)
+            print(str({"type": log_type,
+                       "data": str(body_data),
+                       "timestamp": time.strftime("%Y%m%d%H%M%S")}), flush=True)
 
     def com_elastic_get(self, id):
-        self.es_inst.get(index=self.es_index, doc_type='MediaKraken', id=id)
+        if self.async_mode:
+            await self.es_inst.get(index=self.es_index, doc_type='MediaKraken', id=id)
+        else:
+            self.es_inst.get(index=self.es_index, doc_type='MediaKraken', id=id)
