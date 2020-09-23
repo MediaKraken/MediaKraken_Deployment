629a774a0064806e944f7f16e4a526e755d64a5c
SpootDev <spootdev@gmail.com>
2020-09-22 20:44:59 -0500

    version call

--- source/database_async/db_base_postgresql_async.py
+++ source/database_async/db_base_postgresql_async.py
@@ -3,6 +3,23 @@ import inspect
 from common import common_logging_elasticsearch_httpx
 
 
+async def db_pgsql_version(self, db_connection=None):
+    await common_logging_elasticsearch_httpx.com_es_httpx_post_async(message_type='info',
+                                                                     message_text={
+                                                                         'function':
+                                                                             inspect.stack()[0][
+                                                                                 3],
+                                                                         'locals': locals(),
+                                                                         'caller':
+                                                                             inspect.stack()[1][
+                                                                                 3]})
+    if db_connection is None:
+        db_conn = self.db_connection
+    else:
+        db_conn = db_connection
+    return await db_conn.fetchval('SELECT version();')
+
+
 async def db_pgsql_parallel_workers(self, db_connection=None):
     """
     Return number of workers
