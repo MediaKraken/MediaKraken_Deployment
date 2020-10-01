c8cd1f35ef4ce4a5a664685f93c2d9bd9760ab2d
SpootDev <spootdev@gmail.com>
2020-09-30 20:22:47 -0500

    missing asyncs

--- source/async_bulk_themoviedb_netfetch.py
+++ source/async_bulk_themoviedb_netfetch.py
@@ -26,15 +26,15 @@ from common import common_config_ini
 from common import common_file
 from common import common_global
 from common import common_logging_elasticsearch_httpx
-from common import common_network
+from common import common_network_async
 from common import common_system
 
 
 async def main(loop):
     # start logging
-    common_logging_elasticsearch_httpx.com_es_httpx_post_async(message_type='info',
-                                                               message_text='START',
-                                                               index_name='async_bulk_themoviedb_netfetch')
+    await common_logging_elasticsearch_httpx.com_es_httpx_post_async(message_type='info',
+                                                                     message_text='START',
+                                                                     index_name='async_bulk_themoviedb_netfetch')
 
     fetch_date = '09_20_2020'
 
@@ -44,69 +44,73 @@ async def main(loop):
                                                       as_pool=False)
 
     force_dl = False
-    if db_connection.db_table_count('mm_metadata_movie', db_connection=db_connection) == 0 \
-            and db_connection.db_table_count('mm_download_que', db_connection=db_connection) == 0:
+    if await db_connection.db_table_count('mm_metadata_movie', db_connection=db_connection) == 0 \
+            and await db_connection.db_table_count('mm_download_que',
+                                                   db_connection=db_connection) == 0:
         force_dl = True
     # start up the range fetches for movie
     file_name = 'http://files.tmdb.org/p/exports/movie_ids_%s.json.gz' % fetch_date
-    common_network.mk_network_fetch_from_url_async(file_name, 'movie.gz')
+    await common_network_async.mk_network_fetch_from_url_async(file_name, 'movie.gz')
     json_data = common_file.com_file_ungzip('movie.gz').decode('utf-8')
     for json_row in json_data.splitlines():
         tmdb_to_fetch = str(json.loads(json_row)['id'])
         # check to see if we already have it
         if force_dl or (
-                db_connection.db_meta_tmdb_count(tmdb_to_fetch, db_connection=db_connection) == 0
-                and db_connection.db_download_que_exists(None,
-                                                         common_global.DLMediaType.Movie.value,
-                                                         'themoviedb',
-                                                         str(tmdb_to_fetch),
-                                                         db_connection=db_connection) is None):
-            db_connection.db_download_insert(provider='themoviedb',
-                                             que_type=common_global.DLMediaType.Movie.value,
-                                             down_json=json.dumps({"Status": "Fetch",
-                                                                   "ProviderMetaID": tmdb_to_fetch}),
-                                             down_new_uuid=uuid.uuid4(),
-                                             db_connection=db_connection
-                                             )
+                await db_connection.db_meta_tmdb_count(tmdb_to_fetch,
+                                                       db_connection=db_connection) == 0
+                and await db_connection.db_download_que_exists(None,
+                                                               common_global.DLMediaType.Movie.value,
+                                                               'themoviedb',
+                                                               str(tmdb_to_fetch),
+                                                               db_connection=db_connection) is None):
+            await db_connection.db_download_insert(provider='themoviedb',
+                                                   que_type=common_global.DLMediaType.Movie.value,
+                                                   down_json=json.dumps({"Status": "Fetch",
+                                                                         "ProviderMetaID": tmdb_to_fetch}),
+                                                   down_new_uuid=uuid.uuid4(),
+                                                   db_connection=db_connection
+                                                   )
     os.remove('movie.gz')
 
     force_dl = False
-    if db_connection.db_table_count('mm_metadata_tvshow', db_connection=db_connection) == 0 \
-            and db_connection.db_table_count('mm_download_que', db_connection=db_connection) == 0:
+    if await db_connection.db_table_count('mm_metadata_tvshow', db_connection=db_connection) == 0 \
+            and await db_connection.db_table_count('mm_download_que',
+                                                   db_connection=db_connection) == 0:
         force_dl = True
     # start up the range fetches for tv
     file_name = 'http://files.tmdb.org/p/exports/tv_series_ids_%s.json.gz' % fetch_date
-    common_network.mk_network_fetch_from_url_async(file_name, 'tv.gz')
+    await common_network_async.mk_network_fetch_from_url_async(file_name, 'tv.gz')
     json_data = common_file.com_file_ungzip('tv.gz').decode('utf-8')
     for json_row in json_data.splitlines():
         tmdb_to_fetch = str(json.loads(json_row)['id'])
         # check to see if we already have it
         if force_dl or (
-                db_connection.db_meta_tmdb_count(tmdb_to_fetch, db_connection=db_connection) == 0
-                and db_connection.db_download_que_exists(None,
-                                                         common_global.DLMediaType.TV.value,
-                                                         'themoviedb',
-                                                         tmdb_to_fetch,
-                                                         db_connection=db_connection) is None):
-            db_connection.db_download_insert(provider='themoviedb',
-                                             que_type=common_global.DLMediaType.TV.value,
-                                             down_json=json.dumps({"Status": "Fetch",
-                                                                   "ProviderMetaID": tmdb_to_fetch}),
-                                             down_new_uuid=uuid.uuid4(),
-                                             db_connection=db_connection
-                                             )
+                await db_connection.db_meta_tmdb_count(tmdb_to_fetch,
+                                                       db_connection=db_connection) == 0
+                and await db_connection.db_download_que_exists(None,
+                                                               common_global.DLMediaType.TV.value,
+                                                               'themoviedb',
+                                                               tmdb_to_fetch,
+                                                               db_connection=db_connection) is None):
+            await db_connection.db_download_insert(provider='themoviedb',
+                                                   que_type=common_global.DLMediaType.TV.value,
+                                                   down_json=json.dumps({"Status": "Fetch",
+                                                                         "ProviderMetaID": tmdb_to_fetch}),
+                                                   down_new_uuid=uuid.uuid4(),
+                                                   db_connection=db_connection
+                                                   )
     os.remove('tv.gz')
 
     # no reason to do the person....as the above meta will fetch them from cast/crew
 
     # commit all changes
-    db_connection.db_commit(db_connection=db_connection)
+    await db_connection.db_commit(db_connection=db_connection)
 
     # close DB
-    db_connection.db_close(db_connection=db_connection)
+    await db_connection.db_close(db_connection=db_connection)
 
-    common_logging_elasticsearch_httpx.com_es_httpx_post_async(message_type='info',
-                                                               message_text='STOP')
+    await common_logging_elasticsearch_httpx.com_es_httpx_post_async(message_type='info',
+                                                                     message_text='STOP')
 
 
 if __name__ == "__main__":
--- source/async_metadata_themoviedb_updates.py
+++ source/async_metadata_themoviedb_updates.py
@@ -31,9 +31,9 @@ from common import common_system
 
 async def main(loop):
     # start logging
-    common_logging_elasticsearch_httpx.com_es_httpx_post_async(message_type='info',
-                                                               message_text='START',
-                                                               index_name='async_metadata_themoviedb_updates')
+    await common_logging_elasticsearch_httpx.com_es_httpx_post_async(message_type='info',
+                                                                     message_text='START',
+                                                                     index_name='async_metadata_themoviedb_updates')
 
     # set signal exit breaks
     common_signal.com_signal_set_break()
@@ -49,84 +49,88 @@ async def main(loop):
         'https://api.themoviedb.org/3/movie/changes'
         '?api_key=%s' % option_config_json['API']['themoviedb']))['id']
     for movie_change in new_movie_data['results']:
-        common_logging_elasticsearch_httpx.com_es_httpx_post_async(message_type='info',
-                                                                   message_text={
-                                                                       'mov': movie_change['id']})
+        await common_logging_elasticsearch_httpx.com_es_httpx_post_async(message_type='info',
+                                                                         message_text={
+                                                                             'mov': movie_change[
+                                                                                 'id']})
         # verify it's not already in the database
-        if db_connection.db_meta_guid_by_tmdb(str(movie_change['id']),
-                                              db_connection=db_connection) is None:
+        if await db_connection.db_meta_guid_by_tmdb(str(movie_change['id']),
+                                                    db_connection=db_connection) is None:
             # verify there is not a dl que for this record
-            dl_meta = db_connection.db_download_que_exists(None,
-                                                           common_global.DLMediaType.Movie.value,
-                                                           'themoviedb',
-                                                           str(movie_change['id']),
-                                                           db_connection=db_connection)
-            common_logging_elasticsearch_httpx.com_es_httpx_post_async(message_type='info',
-                                                                       message_text={
-                                                                           'dl_meta': dl_meta})
+            dl_meta = await db_connection.db_download_que_exists(None,
+                                                                 common_global.DLMediaType.Movie.value,
+                                                                 'themoviedb',
+                                                                 str(movie_change['id']),
+                                                                 db_connection=db_connection)
+            await common_logging_elasticsearch_httpx.com_es_httpx_post_async(message_type='info',
+                                                                             message_text={
+                                                                                 'dl_meta': dl_meta})
             if dl_meta is None:
-                db_connection.db_download_insert(provider='themoviedb',
-                                                 que_type=common_global.DLMediaType.Movie.value,
-                                                 down_json=json.dumps({'Status': 'Fetch',
-                                                                       'ProviderMetaID': str(
-                                                                           movie_change['id'])}),
-                                                 down_new_uuid=uuid.uuid4(),
-                                                 db_connection=db_connection
-                                                 )
+                await db_connection.db_download_insert(provider='themoviedb',
+                                                       que_type=common_global.DLMediaType.Movie.value,
+                                                       down_json=json.dumps({'Status': 'Fetch',
+                                                                             'ProviderMetaID': str(
+                                                                                 movie_change[
+                                                                                     'id'])}),
+                                                       down_new_uuid=uuid.uuid4(),
+                                                       db_connection=db_connection
+                                                       )
         else:
             # it's on the database, so must update the record with latest information
-            db_connection.db_download_insert(provider='themoviedb',
-                                             que_type=common_global.DLMediaType.Movie.value,
-                                             down_json=json.dumps({'Status': 'Update',
-                                                                   'ProviderMetaID': str(
-                                                                       movie_change['id'])}),
-                                             down_new_uuid=uuid.uuid4(),
-                                             db_connection=db_connection
-                                             )
+            await db_connection.db_download_insert(provider='themoviedb',
+                                                   que_type=common_global.DLMediaType.Movie.value,
+                                                   down_json=json.dumps({'Status': 'Update',
+                                                                         'ProviderMetaID': str(
+                                                                             movie_change['id'])}),
+                                                   down_new_uuid=uuid.uuid4(),
+                                                   db_connection=db_connection
+                                                   )
     # TODO this should go through the limiter
     # process tv changes
     new_tv_data = json.loads(await common_network_async.mk_network_fetch_from_url_async(
         'https://api.themoviedb.org/3/tv/changes'
         '?api_key=%s' % option_config_json['API']['themoviedb']))['id']
     for tv_change in new_tv_data['results']:
-        common_logging_elasticsearch_httpx.com_es_httpx_post_async(message_type='info',
-                                                                   message_text={
-                                                                       'stuff': "tv: %s" %
-                                                                                tv_change['id']})
+        await common_logging_elasticsearch_httpx.com_es_httpx_post_async(message_type='info',
+                                                                         message_text={
+                                                                             'stuff': "tv: %s" %
+                                                                                      tv_change[
+                                                                                          'id']})
         # verify it's not already in the database
-        if db_connection.db_metatv_guid_by_tmdb(str(tv_change['id']),
-                                                db_connection=db_connection) is None:
-            dl_meta = db_connection.db_download_que_exists(None, common_global.DLMediaType.TV.value,
-                                                           'themoviedb', str(tv_change['id']),
-                                                           db_connection=db_connection)
+        if await db_connection.db_metatv_guid_by_tmdb(str(tv_change['id']),
+                                                      db_connection=db_connection) is None:
+            dl_meta = await db_connection.db_download_que_exists(None,
+                                                                 common_global.DLMediaType.TV.value,
+                                                                 'themoviedb', str(tv_change['id']),
+                                                                 db_connection=db_connection)
             if dl_meta is None:
-                db_connection.db_download_insert(provider='themoviedb',
-                                                 que_type=common_global.DLMediaType.TV.value,
-                                                 down_json=json.dumps({'Status': 'Fetch',
-                                                                       'ProviderMetaID': str(
-                                                                           tv_change['id'])}),
-                                                 down_new_uuid=uuid.uuid4(),
-                                                 db_connection=db_connection
-                                                 )
+                await db_connection.db_download_insert(provider='themoviedb',
+                                                       que_type=common_global.DLMediaType.TV.value,
+                                                       down_json=json.dumps({'Status': 'Fetch',
+                                                                             'ProviderMetaID': str(
+                                                                                 tv_change['id'])}),
+                                                       down_new_uuid=uuid.uuid4(),
+                                                       db_connection=db_connection
+                                                       )
         else:
             # it's on the database, so must update the record with latest information
-            db_connection.db_download_insert(provider='themoviedb',
-                                             que_type=common_global.DLMediaType.TV.value,
-                                             down_json=json.dumps({'Status': 'Update',
-                                                                   'ProviderMetaID': str(
-                                                                       tv_change['id'])}),
-                                             down_new_uuid=uuid.uuid4(),
-                                             db_connection=db_connection
-                                             )
+            await db_connection.db_download_insert(provider='themoviedb',
+                                                   que_type=common_global.DLMediaType.TV.value,
+                                                   down_json=json.dumps({'Status': 'Update',
+                                                                         'ProviderMetaID': str(
+                                                                             tv_change['id'])}),
+                                                   down_new_uuid=uuid.uuid4(),
+                                                   db_connection=db_connection
+                                                   )
 
     # commit all changes
-    db_connection.db_commit(db_connection=db_connection)
+    await db_connection.db_commit(db_connection=db_connection)
 
     # close DB
-    db_connection.db_close(db_connection=db_connection)
+    await db_connection.db_close(db_connection=db_connection)
 
-    common_logging_elasticsearch_httpx.com_es_httpx_post_async(message_type='info',
-                                                               message_text='STOP')
+    await common_logging_elasticsearch_httpx.com_es_httpx_post_async(message_type='info',
+                                                                     message_text='STOP')
 
 
 if __name__ == "__main__":
