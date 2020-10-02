109287ca93de9d0b23016a1b0877b8ed7e46c4d4
SpootDev <spootdev@gmail.com>
2020-10-01 19:22:47 -0500

    comments

--- source/test_asyncpg_mkdbclass.py
+++ source/test_asyncpg_mkdbclass.py
@@ -5,6 +5,7 @@ from common import common_logging_elasticsearch_httpx
 
 
 # works fine hitting the database and doing the db_connection thing
+# passing NONE as it's NOT the pool webapp!!!!
 
 async def main(loop):
     await common_logging_elasticsearch_httpx.com_es_httpx_post_async(message_type='info',
@@ -15,7 +16,8 @@ async def main(loop):
         await common_config_ini.com_config_read_async(loop=loop,
                                                       as_pool=False,
                                                       force_local=True)
-    db_result = await db_connection.db_table_count(table_name='mm_sync', db_connection=None)
+    db_result = await db_connection.db_table_count(table_name='mm_sync',
+                                                   db_connection=None)
     print(db_result)
 
 
