344fcb4ab0348fd469f8166ca40400edc2828f1b
SpootDev <spootdev@gmail.com>
2020-10-11 20:01:51 -0500

    fix termination

--- source/test_xml_generate.py
+++ source/test_xml_generate.py
@@ -11,16 +11,19 @@ print('Starting to write ~250mb XML file')
 
 with open('test_xml_sample.xml', 'w') as xml:
     books = ''
+
     for _ in range(2000000):
         title = ''.join(random.choices(string.ascii_uppercase, k=16))
         first_name = ''.join(random.choices(string.ascii_lowercase, k=8))
         last_name = ''.join(random.choices(string.ascii_lowercase, k=12))
         alive = random.choice(['yes', 'no'])
+
         books += f'''
     <Book>
         <Title>{title}</Title>
-        <Author alive="{alive}" />{first_name} {last_name}</Author>
+        <Author alive="{alive}">{first_name} {last_name}</Author>
     </Book>'''
+
     content = f'''<?xml version="1.0" encoding="utf-8"?>
 <Catalog>{books}
 </Catalog>
