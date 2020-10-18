b2bbf17b0393f6b51f1aa591037f8eb8cfa5ceca
spootdev <spootdev@gmail.com>
2020-10-18 13:50:32 -0500

    new path

--- source/bulk_gamesdb_netfetch.py
+++ source/bulk_gamesdb_netfetch.py
@@ -19,24 +19,22 @@
 import json
 
 from common import common_config_ini
-from common import common_metadata_provider_thegamesdb
+from metadata import metadata_provider_thegamesdb
 
 # open the database
 option_config_json, db_connection = common_config_ini.com_config_read()
 
-GAMESDB_CONNECTION = common_metadata_provider_thegamesdb.CommonMetadataGamesDB()
+GAMESDB_INST = metadata_provider_thegamesdb.CommonMetadataGamesDB()
 
 # grab and insert all platforms
 for platform in \
-        list(GAMESDB_CONNECTION.com_meta_gamesdb_platform_list()['Data']['Platforms'].items())[0]:
+        list(GAMESDB_INST.com_meta_gamesdb_platform_list()['Data']['Platforms'].items())[0]:
     if platform != 'Platform':
         for game_systems in platform:
             print(game_systems, flush=True)
-            if db_connection.db_meta_games_system_guid_by_short_name(
-                    game_systems['name']) is None:
+            if db_connection.db_meta_games_system_guid_by_short_name(game_systems['name']) is None:
                 # fetch platform info
-                platform_json = GAMESDB_CONNECTION.com_meta_gamesdb_platform_by_id(
-                    game_systems['id'])
+                platform_json = GAMESDB_INST.com_meta_gamesdb_platform_by_id(game_systems['id'])
                 # store record
                 try:
                     system_alias = game_systems['alias']
@@ -47,6 +45,7 @@ for platform in \
                                                           system_alias,
                                                           json.dumps(platform_json))
                 db_connection.db_commit()
+
 
 # commit all changes
 db_connection.db_commit()
