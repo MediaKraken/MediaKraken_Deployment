592a6a1011630d027e89ceb57677ca7e153f8a21
SpootDev <spootdev@gmail.com>
2021-01-31 17:19:43 -0600

    remove dupe code

--- source/database_async/db_base_async.py
+++ source/database_async/db_base_async.py
@@ -221,23 +221,3 @@ async def db_query(self, query_string, fetch_all=True, db_connection=None):
         return await db_conn.fetch(query_string)
     else:
         return await db_conn.fetchval(query_string)
-
-
-async def db_parallel_workers(self, db_connection=None):
-    """
-    Return number of workers
-    """
-    await common_logging_elasticsearch_httpx.com_es_httpx_post_async(message_type='info',
-                                                                     message_text={
-                                                                         'function':
-                                                                             inspect.stack()[0][
-                                                                                 3],
-                                                                         'locals': locals(),
-                                                                         'caller':
-                                                                             inspect.stack()[1][
-                                                                                 3]})
-    if db_connection is None:
-        db_conn = self.db_connection
-    else:
-        db_conn = db_connection
-    return await db_conn.fetchval('show max_parallel_workers_per_gather')
