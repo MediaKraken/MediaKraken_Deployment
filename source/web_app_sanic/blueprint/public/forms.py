3141578168aed0768ad646b825127be2a2c3c38d
SpootDev <spootdev@gmail.com>
2021-03-27 20:52:16 -0500

    cleanup

--- source/web_app_sanic/blueprint/public/forms.py
+++ source/web_app_sanic/blueprint/public/forms.py
@@ -33,36 +33,6 @@ class LoginForm(Form):
         return True
 
 
-# new user registration
-class RegisterForm(Form):
-    username = StringField('Username',
-                         validators=[DataRequired(), Length(min=3, max=25)])
-    email = StringField('Email',
-                      validators=[DataRequired(), Email(), Length(min=6, max=40)])
-    password = PasswordField('Password',
-                             validators=[DataRequired(), Length(min=6, max=40)])
-    confirm = PasswordField('Verify password',
-                            [DataRequired(), EqualTo('password', message='Passwords must match')])
-
-    def __init__(self, *args, **kwargs):
-        super(RegisterForm, self).__init__(*args, **kwargs)
-        self.user = None
-
-    def validate(self):
-        initial_validation = super(RegisterForm, self).validate()
-        if not initial_validation:
-            return False
-        user = User.query.filter_by(username=self.username.data).first()
-        if user:
-            self.username.errors.append("Username already registered")
-            return False
-        user = User.query.filter_by(email=self.email.data).first()
-        if user:
-            self.email.errors.append("Email already registered")
-            return False
-        return True
-
-
 class SearchForm(Form):
     """
     for searching media
