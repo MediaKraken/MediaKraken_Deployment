e1554faf32e02ea848c5cce5bab05fdff47bbae5
SpootDev <spootdev@gmail.com>
2021-04-05 19:34:58 -0500

    TextField

--- source/web_app_sanic/blueprint/public/bss_form_login.py
+++ source/web_app_sanic/blueprint/public/bss_form_login.py
@@ -1,10 +1,10 @@
 from sanic_wtf import SanicForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import TextField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 
 
 class BSSLoginForm(SanicForm):
-    username = StringField('Username', validators=[DataRequired()])
+    username = TextField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
 
