f95227b29846f244ef5bd519cc9c47eef6f79193
SpootDev <spootdev@gmail.com>
2021-04-05 22:56:24 -0500

    new wine image

--- docker/game_server/ComposeMediaKrakenBaseWine/display.sh
+++
@@ -1,9 +0,0 @@
-export DISPLAY=:99
-Xvfb :99 -screen 0 1000x1000x16 &
-xrandr –query
-sleep 5
-nohup startxfce4 &
-sleep 10
-xdotool mousemove 493 539 click 1
-xrandr –query
-winetricks -q msxml4 msxml6 corefonts vcrun2010 vcrun2013 vcrun2017 dotnet46
