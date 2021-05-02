a76355795e272754e46c7f551a391bed932fbda6
SpootDev <spootdev@gmail.com>
2021-05-02 13:51:49 -0500

    2018 version

--- docker/alpine/ComposeMediaKrakenBroadcastRust/src/main.rs
+++ docker/alpine/ComposeMediaKrakenBroadcastRust/src/main.rs
@@ -1,17 +1,49 @@
+//! Fetch info of all running containers concurrently
+
+use bollard::container::{InspectContainerOptions, ListContainersOptions};
+use bollard::models::ContainerSummary;
 use bollard::Docker;
-use bollard::image::ListImagesOptions;
 
-use futures_util::future::FutureExt;
-
+use std::collections::HashMap;
 use std::default::Default;
 
-async fn main() {
-    let images = &docker.list_images(Some(ListImagesOptions::<String> {
-        all: true,
-        ..Default::default()
-    })).await.unwrap();
+use futures_util::stream;
+use futures_util::stream::StreamExt;
 
-    for image in images {
-        println!("-> {:?}", image);
-    }
-};
+async fn conc(arg: (Docker, &ContainerSummary)) -> () {
+    let (docker, container) = arg;
+    println!(
+        "{:?}",
+        docker
+            .inspect_container(
+                container.id.as_ref().unwrap(),
+                None::<InspectContainerOptions>
+            )
+            .await
+            .unwrap()
+    )
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
+    let docker = Docker::connect_with_unix_defaults().unwrap();
+
+    let mut list_container_filters = HashMap::new();
+    list_container_filters.insert("status", vec!["running"]);
+
+    let containers = &docker
+        .list_containers(Some(ListContainersOptions {
+            all: true,
+            filters: list_container_filters,
+            ..Default::default()
+        }))
+        .await?;
+
+    let docker_stream = stream::repeat(docker);
+    docker_stream
+        .zip(stream::iter(containers))
+        .for_each_concurrent(2, conc)
+        .await;
+
+    Ok(())
+}
