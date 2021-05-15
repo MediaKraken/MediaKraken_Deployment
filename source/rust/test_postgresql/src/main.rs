a6ff8d45b683dc720bcdc86abcf4eae8c13399e2
SpootDev <spootdev@gmail.com>
2021-05-15 18:26:05 -0500

    add read exmample

--- source/rust/test_postgresql/src/main.rs
+++ source/rust/test_postgresql/src/main.rs
@@ -1,7 +1,7 @@
 // launch the vs tools app otherwise can't compile (windows os only)
 // cargo build --release
 
-use postgres::{Client, NoTls, Error};
+use postgres::{Client, Error, NoTls};
 
 fn main() -> Result<(), Error> {
     let mut client = Client::connect("postgresql://postgres:metaman@localhost/postgres", NoTls)?;
@@ -22,6 +22,28 @@ fn main() -> Result<(), Error> {
             )
     ")?;
 
-    Ok(())
+    client.batch_execute("
+        CREATE TABLE person (
+            id      SERIAL PRIMARY KEY,
+            name    TEXT NOT NULL,
+            data    BYTEA
+        )
+    ")?;
 
+    let name = "Ferris";
+    let data = None::<&[u8]>;
+    client.execute(
+        "INSERT INTO person (name, data) VALUES ($1, $2)",
+        &[&name, &data],
+    )?;
+
+    for row in client.query("SELECT id, name, data FROM person", &[])? {
+        let id: i32 = row.get(0);
+        let name: &str = row.get(1);
+        let data: Option<&[u8]> = row.get(2);
+
+        println!("found person: {} {} {:?}", id, name, data);
+    }
+
+    Ok(())
 }
