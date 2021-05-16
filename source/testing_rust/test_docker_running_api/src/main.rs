60cc0eb532ef01ca9cad35e4e1a97b6da0955296
SpootDev <spootdev@gmail.com>
2021-05-16 10:36:44 -0500

    new test

---
+++ source/testing_rust/test_docker_running_api/src/main.rs
@@ -0,0 +1,23 @@
+use shiplift::Docker;
+
+#[tokio::main]
+async fn main() {
+    let docker = Docker::new();
+    println!("docker images in stock");
+
+    let result = docker.containers().list(&Default::default()).await;
+
+    match result {
+        Ok(images) => {
+            for i in images {
+                println!(
+                    "{} {} {:?}",
+                    i.id,
+                    i.created,
+                    i.repo_tags.unwrap_or_else(|| vec!["none".into()])
+                );
+            }
+        }
+        Err(e) => eprintln!("Error: {}", e),
+    }
+}
