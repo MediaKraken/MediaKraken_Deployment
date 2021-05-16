2b72532488c5aba7b1df95fd509325ecc7f3dbba
SpootDev <spootdev@gmail.com>
2021-05-16 12:31:54 -0500

    find reactor image

--- source/testing_rust/test_docker_running_api/src/main.rs
+++ source/testing_rust/test_docker_running_api/src/main.rs
@@ -3,20 +3,23 @@ use shiplift::Docker;
 #[tokio::main]
 async fn main() {
     let docker = Docker::new();
-    println!("docker images in stock");
+    println!("docker containers that are running");
 
     let result = docker.containers().list(&Default::default()).await;
 
     match result {
         Ok(images) => {
             for i in images {
-                println!(
-                    "{} {} {:?} {}",
-                    i.id,
-                    i.created,
-                    i.labels,
-                    i.ports
-                );
+                if i.names[0] == "/mkstack_reactor" {
+                    println!(
+                        "{} {} {:?} {:?}",
+                        i.id,
+                        i.created,
+                        //i.labels,  // the actual labels in the dockerfile
+                        i.ports,
+                        i.names
+                    );
+                }
             }
         }
         Err(e) => eprintln!("Error: {}", e),
