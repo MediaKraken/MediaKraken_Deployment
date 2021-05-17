92c4212b53d3af23cf4100a42033eb792792828b
SpootDev <spootdev@gmail.com>
2021-05-17 18:59:49 -0500

    deeper tests

--- source/testing_rust/test_env_variables/src/main.rs
+++ source/testing_rust/test_env_variables/src/main.rs
@@ -1,24 +1,49 @@
 fn main() {
-use std::env;
+    use std::env;
 
 // these seem to return the same things
-for (key, value) in env::vars_os() {
-    println!("{:?}: {:?}", key, value);
-}
+    for (key, value) in env::vars_os() {
+        println!("{:?}: {:?}", key, value);
+    }
 
-for (key, value) in env::vars() {
-    println!("{:?}: {:?}", key, value);
-}
+    for (key, value) in env::vars() {
+        println!("{:?}: {:?}", key, value);
+    }
 
-let key = "OS";
-match env::var_os(key) {
-    Some(val) => println!("{}: {:?}", key, val),
-    None => println!("{} is not defined in the environment.", key)
-}
+    let key = "OS";
+    match env::var_os(key) {
+        Some(val) => println!("{}: {:?}", key, val),
+        None => println!("{} is not defined in the environment.", key)
+    }
 
-let key = "PATH";
-match env::var(key) {
-    Ok(val) => println!("{}: {:?}", key, val),
-    Err(e) => println!("couldn't interpret {}: {}", key, e),
-}
+    match env::var("PATH") {
+        Ok(val) => println!("{:?}", val),
+        Err(e) => println!("couldn't interpret {}", e),
+    }
+
+    match env::var("SWARMIP") {
+        Ok(val) => println!("{:?}", val),
+        Err(e) => println!("couldn't interpret {}", e),
+    }
+
+    let data = env::var_os("OS");
+    println!("{:?}", data);
+
+    match env::var("SWARMIP") {
+    Ok(mediakraken_ip) => {
+        println!("{:?}", mediakraken_ip);
+        },
+    Err(e) => {
+        println!("couldn't interpret swarm {}", e);
+        match env::var("HOST_IP") {
+            Ok(mediakraken_ip) => {
+                println!("{:?}", mediakraken_ip);
+                },
+            Err(e) => {
+                println!("couldn't interpret host {}", e);
+                }
+            }
+        }
+    }
+
 }
