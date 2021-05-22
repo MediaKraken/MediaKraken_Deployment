b875dcdf06457f4e458344a877825ab4d5bc8be2
SpootDev <spootdev@gmail.com>
2021-05-21 22:21:47 -0500

    env variables

--- docker/alpine/ComposeMediaKrakenBroadcastRust/src/main.rs
+++ docker/alpine/ComposeMediaKrakenBroadcastRust/src/main.rs
@@ -6,12 +6,23 @@ use std::env;
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
+    let host_port;
     // grab swarm or host port
-    if env::var("SWARMIP") != "None" {
-        let mediakraken_ip = env::var("SWARMIP");
-    }
-    else {
-        let mediakraken_ip = env::var("HOST_IP");
+    match env::var("SWARMIP") {
+    Ok(mediakraken_ip) => {
+        println!("{:?}", mediakraken_ip);
+        },
+    Err(e) => {
+        println!("couldn't interpret swarm {}", e);
+        match env::var("HOST_IP") {
+            Ok(mediakraken_ip) => {
+                println!("{:?}", mediakraken_ip);
+                },
+            Err(e) => {
+                println!("couldn't interpret host {}", e);
+                }
+            }
+        }
     }
     // Grab public port that the reactor is running on
     let docker = Docker::new();
@@ -20,31 +31,32 @@ async fn main() -> io::Result<()> {
         Ok(images) => {
             for i in images {
                 if i.names[0] == "/mkstack_reactor" {
-                    let host_port = i.ports[0].private_port;
-                    // Begin the broadcast receive loop
-                    let sock = UdpSocket::bind("0.0.0.0:9101").await?;
-                    let mut buf= [0; 1024];
-                    loop {
-                        let (len, addr) = sock.recv_from(&mut buf).await?;
-                        if len == 25 {
-                            let net_string = match str::from_utf8(&buf[..25]) {
-                                Ok(v) => v,
-                                Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
-                            };
-                            if net_string == "who is MediaKrakenServer?"
-                            {
-                                println!("{:?} bytes received {:?} {:?}", len, addr, net_string);
-                                println!("{:?} mk port", host_port);
-                                let mk_address = b"10.0.0.141:{}" % host_port;
-                                println!("{:?} mk_address", mk_address);
-                                let len = sock.send_to(mk_address, addr).await?;
-                                println!("{:?} bytes sent", len);
-                            }
-                        }
-                    }
+                    host_port = i.ports[0].private_port;
+                    break;
                 }
             }
         }
         Err(e) => eprintln!("Error: {}", e),
+    }
+    // Begin the broadcast receive loop
+    let sock = UdpSocket::bind("0.0.0.0:9101").await?;
+    let mut buf= [0; 1024];
+    loop {
+        let (len, addr) = sock.recv_from(&mut buf).await?;
+        if len == 25 {
+            let net_string = match str::from_utf8(&buf[..25]) {
+                Ok(v) => v,
+                Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+            };
+            if net_string == "who is MediaKrakenServer?"
+            {
+                println!("{:?} bytes received {:?} {:?}", len, addr, net_string);
+                println!("{:?} mk port", host_port);
+                let mk_address = b"10.0.0.141:{}" % host_port;
+                println!("{:?} mk_address", mk_address);
+                let len = sock.send_to(mk_address, addr).await?;
+                println!("{:?} bytes sent", len);
+            }
+        }
     }
 }
