ed176c8599ad0e3df960304cf8a7d19190eaf080
SpootDev <spootdev@gmail.com>
2021-05-23 18:57:50 -0500

    new tests

---
+++ source_rust/testing_rust/test_webapp/src/main.rs
@@ -0,0 +1,16 @@
+// http://127.0.0.1:8080/1/bob/index.html
+
+use actix_web::{get, web, App, HttpServer, Responder};
+
+#[get("/{id}/{name}/index.html")]
+async fn index(web::Path((id, name)): web::Path<(u32, String)>) -> impl Responder {
+    format!("Hello {}! id:{}", name, id)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(index))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
+}
