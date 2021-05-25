5f2f0dd9eef1c42a539630cc8993f1f5a175e679
SpootDev <spootdev@gmail.com>
2021-05-24 22:03:54 -0500

    new test

---
+++ source_rust/testing_rust/test_postgresql_tokio/src/main.rs
@@ -0,0 +1,25 @@
+use tokio_postgres::{NoTls, Error};
+
+#[tokio::main] // By default, tokio_postgres uses the tokio crate as its runtime.
+async fn main() -> Result<(), Error> {
+    // Connect to the database.
+    let (client, connection) =
+        tokio_postgres::connect("host=localhost user=postgres", NoTls).await?;
+
+    // The connection object performs the actual communication with the database,
+    // so spawn it off to run on its own.
+    tokio::spawn(async move {
+        if let Err(e) = connection.await {
+            eprintln!("connection error: {}", e);
+        }
+    });
+
+    // Now we can execute a simple statement that just returns its parameter.
+    let rows = client
+        .query("SELECT $1::TEXT", &[&"hello world"])
+        .await?;
+    // And then check that we got back the same string we sent over.
+    let value: &str = rows[0].get(0);
+    assert_eq!(value, "hello world");
+    Ok(())
+}
