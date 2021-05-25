e5b5d329a57123e32c32008afe43c2b500cd663b
SpootDev <spootdev@gmail.com>
2021-05-24 20:24:01 -0500

    new

---
+++ docker/alpine/ComposeMediaKrakenCronRust/src/main.rs
@@ -0,0 +1,82 @@
+use tokio::time::{Duration, sleep};
+use tokio_postgres::{Error, NoTls};
+use chrono::prelude::*;
+
+#[path = "../../../../source_rust/lib_logging/mk_lib_logging.rs"]
+mod mk_lib_logging;
+#[path = "../../../../source_rust/lib_database/mk_lib_database.rs"]
+mod mk_lib_database;
+#[path = "../../../../source_rust/lib_database/mk_lib_database_cron.rs"]
+mod mk_lib_database_cron;
+#[path = "../../../../source_rust/lib_network/mk_lib_network.rs"]
+mod mk_lib_network;
+
+#[tokio::main]
+fn main() -> Result<(), Box<dyn Error>> {
+    // start logging
+    mk_lib_logging::mk_logging_post_elk("info",
+                                        "START",
+                                        "cron_checker");
+    // grab db password
+    match env::var("POSTGRES_PASSWORD") {
+        Ok(db_pass) => println!("{:?}", db_pass),
+        Err(e) => println!("couldn't interpret {}", e),
+    }
+    // open the database
+    let db_client = mk_lib_database::mk_lib_database_open(db_pass);
+
+    // fire off wait for it script to verify rabbitmq is available
+    mk_lib_network::mk_network_service_available("mkstack_rabbitmq",
+                                                 "5672", "120")
+
+//     credentials = pika.PlainCredentials('guest', 'guest')
+//     parameters = pika.ConnectionParameters('mkstack_rabbitmq',
+//                                            socket_timeout = 30, credentials = credentials)
+//     connection = pika.BlockingConnection(parameters)
+//     channel = connection.channel()
+
+    // start loop for cron checks
+    loop {
+        for row_data in mk_lib_database_cron::mk_lib_database_cron_service_read() {
+            let mut time_frame;
+            if row_data["mm_cron_schedule"] == "Weekly" {
+                time_frame = datetime.timedelta(weeks = 1);
+            } else if row_data["mm_cron_schedule"].split(' ', 1)[0] == "Days" {
+                time_frame = datetime.timedelta(
+                    days = int(row_data["mm_cron_schedule"].split(' ', 1)[1]));
+            } else if row_data["mm_cron_schedule"].split(' ', 1)[0] == "Hours" {
+                time_frame = datetime.timedelta(
+                    hours = int(row_data["mm_cron_schedule"].split(' ', 1)[1]));
+            } else {
+                row_data["mm_cron_schedule"].split(' ', 1)[0] == "Minutes":
+                    time_frame = datetime.timedelta(
+                    minutes = int(row_data["mm_cron_schedule"].split(' ', 1)[1]));
+            }
+        let utc: DateTime<Utc> = Utc::now();
+        let date_check = utc - time_frame;
+        if row_data['mm_cron_last_run'] < date_check:
+            {
+            channel.basic_publish(exchange = row_data['mm_cron_json']['exchange_key'],
+                                  routing_key = row_data['mm_cron_json']['route_key'],
+                                  body = json.dumps(
+                                      {
+                                          'Type': row_data['mm_cron_json']['Type'],
+                                          'JSON': row_data['mm_cron_json']
+                                      }),
+                                  properties = pika.BasicProperties(
+                                      content_type ='text/plain',
+                                      delivery_mode = 2),
+        )}
+        db_connection.db_cron_time_update(row_data['mm_cron_name'])
+        db_connection.db_commit()
+        }
+        mk_lib_logging::mk_logging_post_elk("info",
+                                            row_data,
+                                            "cron_checker");
+        sleep(Duration::from_secs(60)).await;
+    }
+    // close the pika connection
+    //     connection.close()
+    // close the database
+    //     db_connection.db_close()
+}
