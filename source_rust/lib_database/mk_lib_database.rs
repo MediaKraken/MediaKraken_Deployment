061a60f5dfb659eec2a513e249846b3b38de9d18
SpootDev <spootdev@gmail.com>
2021-05-24 21:50:51 -0500

    pass client

--- source_rust/lib_database/mk_lib_database.rs
+++ source_rust/lib_database/mk_lib_database.rs
@@ -2,7 +2,7 @@ use tokio_postgres::{Error, NoTls};
 
 pub async fn mk_lib_database_open(database_password: &str) -> Result<(), Error> {
     let mut client = tokio_postgres::connect(
-        "postgresql://postgres:metaman@mkstack_database/postgres", NoTls)?;
+        format!("postgresql://postgres:{}@mkstack_database/postgres", database_password), NoTls)?;
     tokio::spawn(async move {
         if let Err(e) = connection.await {
             eprintln!("connection error: {}", e);
@@ -10,14 +10,14 @@ pub async fn mk_lib_database_open(database_password: &str) -> Result<(), Error> 
     });
 }
 
-pub async fn mk_lib_database_options() -> Result<(), Error> {
+pub async fn mk_lib_database_options(client: tokio_postgres::Client) -> Result<(), Error> {
     let row = client
         .query_one("select mm_options_json from mm_options_and_status")
         .await?;
     let mm_options_json: &str = row.try_get::<&str, serde_json::Value>("mm_options_json");
 }
 
-pub async fn mk_lib_database_status() -> Result<(), Error> {
+pub async fn mk_lib_database_status(client: tokio_postgres::Client) -> Result<(), Error> {
     let row = client
         .query_one("select mm_status_json from mm_options_and_status")
         .await?;
