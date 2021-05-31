bcd0e1f458049a8ee21bc939628778235399a230
SpootDev <spootdev@gmail.com>
2021-05-31 17:56:21 -0500

    flate2

--- source_rust/lib_compression/src/mk_lib_compression.rs
+++ source_rust/lib_compression/src/mk_lib_compression.rs
@@ -1,7 +1,7 @@
 
 pub fn mk_decompress_gzip(archive_file:&str) -> Result<String, std::io::Error> {
-    let file_handle = File::open(archive_file)?;
-    let mut gz = GzDecoder::new(file_handle);
+    let file_handle = std::fs::File::open(archive_file)?;
+    let mut gz = flate2::read::GzDecoder::new(file_handle);
     let mut gz_data = String::new();
     gz.read_to_string(&mut gz_data)?;
     Ok(gz_data)
