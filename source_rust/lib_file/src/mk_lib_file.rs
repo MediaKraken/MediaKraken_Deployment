80cf8c94b27775b63070776944b2077b9525aa05
SpootDev <spootdev@gmail.com>
2021-05-31 17:56:07 -0500

    dfine fs

--- source_rust/lib_file/src/mk_lib_file.rs
+++ source_rust/lib_file/src/mk_lib_file.rs
@@ -1,10 +1,9 @@
-use std::fs::File;
 use std::io;
 use std::io::prelude::*;
 use walkdir::{DirEntry, WalkDir};
 
 pub fn mk_read_file_data(file_to_read: &str) -> io::Result<()> {
-    let mut file_handle = File::open(file_to_read)?;
+    let mut file_handle = std::fs::File::open(file_to_read)?;
     let mut buffer = String::new();
     file_handle.read_to_string(&mut buffer)?;
     Ok(())
