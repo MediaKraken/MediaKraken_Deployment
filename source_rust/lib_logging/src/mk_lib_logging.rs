05c541381e84c12552df22fdd7986f8577e7fc61
SpootDev <spootdev@gmail.com>
2021-05-31 18:37:30 -0500

    cleanup

--- source_rust/lib_logging/src/mk_lib_logging.rs
+++ source_rust/lib_logging/src/mk_lib_logging.rs
@@ -1,5 +1,4 @@
 use chrono::prelude::*;
-use reqwest::StatusCode;
 
 // attohttpc is not async
 // pub fn mk_logging_post_elk(message_type:&str, message_text:&str,
@@ -24,15 +23,6 @@ pub async fn mk_logging_post_elk(message_type: &str, message_text: &str,
     let utc: DateTime<Utc> = Utc::now();
     let data = serde_json::json!({"@timestamp": utc.format("%Y-%m-%dT%H:%M:%S.%f").to_string(),
         "message": message_text, "type": message_type, "user": {"id": "metaman"}});
-    // let resp = attohttpc::post(
-    //     format!("http://th-elk-1.beaverbay.local:9200/{}/MediaKraken", index_name))
-    //     .header("Content-Type", "application/json")
-    //     .json(&data).send()?;
-    // println!("Status: {:?}", resp.status());
-    // println!("Headers:\n{:#?}", resp.headers());
-    // if resp.is_success() {
-    //     println!("{}", resp.text()?);
-    // }
     let client = reqwest::Client::new();
     let resp = client.post(format!("http://th-elk-1.beaverbay.local:9200/{}/MediaKraken",
                                    index_name))
@@ -40,5 +30,4 @@ pub async fn mk_logging_post_elk(message_type: &str, message_text: &str,
         .json(&data)
         .send()
         .await;
-    // Ok(())
 }
