f12c7e9a0e3c6ba5fe4b43be98c4c7893f067ace
SpootDev <spootdev@gmail.com>
2021-05-30 21:06:46 -0500

    query

--- source_rust/testing_rust/test_postgresql_tokio/src/main.rs
+++ source_rust/testing_rust/test_postgresql_tokio/src/main.rs
@@ -1,10 +1,32 @@
 use tokio_postgres::{NoTls, Error};
+// use serde::{Serialize, Deserialize};
+
+fn print_type_of<T>(_: &T) {
+    println!("Var Type: {}", std::any::type_name::<T>())
+}
+
+/*
+{
+    "Type": "Update Metadata",
+    "program": "/mediakraken/async_metadata_themoviedb_updates.py",
+    "route_key": "themoviedb",
+    "exchange_key": "mkque_metadata_ex"
+}
+*/
+
+// #[derive(Serialize, Deserialize, Debug)]
+// struct CronJson {
+//   cron_type: String,
+//   program: String,
+//   route_key: String,
+//   ex_key: String
+// }
 
 #[tokio::main] // By default, tokio_postgres uses the tokio crate as its runtime.
 async fn main() -> Result<(), Error> {
     // Connect to the database.
     let (client, connection) =
-        tokio_postgres::connect("host=localhost user=postgres", NoTls).await?;
+        tokio_postgres::connect("host=localhost user=postgres password=metaman", NoTls).await?;
 
     // The connection object performs the actual communication with the database,
     // so spawn it off to run on its own.
@@ -21,5 +43,18 @@ async fn main() -> Result<(), Error> {
     // And then check that we got back the same string we sent over.
     let value: &str = rows[0].get(0);
     assert_eq!(value, "hello world");
+
+    for row in client.query("select mm_cron_guid, mm_cron_schedule, mm_cron_last_run,\
+         mm_cron_json from mm_cron where mm_cron_enabled = False", &[]).await? {
+        // let id: i32 = row.get(0);
+        // let name: &str = row.get(1);
+        // let data: Option<&[u8]> = row.get(2);
+        //println!("found person: {} {} {:?}", id, name, data);
+        println!("stuff: {:?}", row);
+
+        let cron_json = row.try_get::<&str, serde_json::Value>("mm_cron_json")?;
+        println!("ha: {:?}", cron_json["Type"]);
+    }
+
     Ok(())
 }
