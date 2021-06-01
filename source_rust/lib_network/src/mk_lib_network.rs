7ef89ef032acbe6a67a096b9ce17a41473e8cbf5
SpootDev <spootdev@gmail.com>
2021-05-31 19:51:25 -0500

    copy method

--- source_rust/lib_network/src/mk_lib_network.rs
+++ source_rust/lib_network/src/mk_lib_network.rs
@@ -1,4 +1,6 @@
 use std::io::Write;
+use std::io::Cursor;
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
 // pub fn mk_download_file_from_url(url_to_download: &str, file_save_path: &str) -> attohttpc::Result {
 //     let resp = attohttpc::get(url_to_download).send()?;
@@ -12,17 +14,31 @@ use std::io::Write;
 //     Ok(())
 // }
 
-pub async fn mk_download_file_from_url(url_to_download: &str, file_save_path: &str) -> Result<()> {
-    let resp = reqwest::get(url_to_download).await?;
-    let path = std::path::Path::new(file_save_path);
-    let mut file_handle = match std::fs::File::create(&path) {
-        Err(why) => panic!("couldn't create {}", why),
-        Ok(file_handle) => {
-            let content = resp.bytes().await?;
-            file_handle.write_all(content.as_bytes())?;
-        }
-    };
+pub async fn mk_download_file_from_url(url: String, file_name: String) -> Result<()> {
+    let response = reqwest::get(url).await?;
+    let mut file = std::fs::File::create(file_name)?;
+    let mut content =  Cursor::new(response.bytes().await?);
+    std::io::copy(&mut content, &mut file)?;
+    Ok(())
 }
+
+// #[tokio::main]
+// async fn main() {
+//     fetch_url("https://georgik.rocks/wp-content/uploads/sianim.gif".to_string(), "siriel.gif".to_string()).await.unwrap();
+// }
+
+// pub async fn mk_download_file_from_url(url_to_download: &str, file_save_path: &str) -> reqwest::Result {
+//     let resp = reqwest::get(url_to_download).await?;
+//     let path = std::path::Path::new(file_save_path);
+//     let mut file_handle = match std::fs::File::create(&path) {
+//         Err(why) => panic!("couldn't create {}", why),
+//         Ok(file_handle) => {
+//             let content = resp.bytes().await?;
+//             file_handle.write_all(content.as_bytes())?;
+//         }
+//     };
+//     Ok(())
+// }
 
 // wait_seconds - 120 typically
 pub async fn mk_network_service_available(host_dns: &str, host_port: &str,
