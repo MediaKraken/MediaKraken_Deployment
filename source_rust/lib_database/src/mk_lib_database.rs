6a48c0cee4c68a29b4180a5b2fe10f63520bfb54
SpootDev <spootdev@gmail.com>
2021-05-31 20:48:21 -0500

    as str

--- source_rust/lib_database/src/mk_lib_database.rs
+++ source_rust/lib_database/src/mk_lib_database.rs
@@ -2,7 +2,8 @@ use tokio_postgres::{Error, NoTls};
 
 pub async fn mk_lib_database_open(database_password: &str) -> Result<(), Error> {
     let (client, connection) = tokio_postgres::connect(
-        format!("postgresql://postgres:{}@mkstack_database/postgres", database_password), NoTls)?;
+        format!("postgresql://postgres:{}@mkstack_database/postgres",
+                database_password).as_str(), NoTls)?;
     tokio::spawn(async move {
         if let Err(e) = connection.await {
             eprintln!("connection error: {}", e);
@@ -14,14 +15,12 @@ pub async fn mk_lib_database_options(client: tokio_postgres::Client) -> Result<(
     let row = client
         .query_one("select mm_options_json from mm_options_and_status", &[])
         .await?;
-    let mm_options_json: &str = row.try_get::<&str, serde_json::Value>("mm_options_json");
-    mm_options_json
+    let mm_options_json: &str = row.try_get::<&str, serde_json::Value>("mm_options_json")?;
 }
 
 pub async fn mk_lib_database_status(client: tokio_postgres::Client) -> Result<(), Error> {
     let row = client
         .query_one("select mm_status_json from mm_options_and_status", &[])
         .await?;
-    let mm_status_json: &str = row.try_get::<&str, serde_json::Value>("mm_status_json");
-    mm_status_json
+    let mm_status_json: &str = row.try_get::<&str, serde_json::Value>("mm_status_json")?;
 }
--- source_rust/lib_database/src/mk_lib_database_cron.rs
+++ source_rust/lib_database/src/mk_lib_database_cron.rs
@@ -1,15 +1,15 @@
-
-pub async fn mk_lib_database_cron_service_read(client: tokio_postgres::Client) -> Result<()> {
+pub async fn mk_lib_database_cron_service_read(client: tokio_postgres::Client)
+                                               -> Result<(), Error> {
     let rows = client
         .query("select mm_cron_guid, mm_cron_schedule, mm_cron_last_run,\
          mm_cron_json from mm_cron where mm_cron_enabled = true", &[])
         .await?;
-    let mm_cron_json: &str = rows.try_get::<&str, serde_json::Value>("mm_cron_json");
-    mm_cron_json
+    let mm_cron_json: &str = rows.try_get::<&str, serde_json::Value>("mm_cron_json")?;
 }
 
 
-pub async fn mk_lib_database_cron_time_update(client: tokio_postgres::Client) -> Result<()> {
+pub async fn mk_lib_database_cron_time_update(client: tokio_postgres::Client)
+                                              -> Result<(), Error> {
     let rows = client
         .query("select mm_cron_guid, mm_cron_schedule, mm_cron_last_run,\
          mm_cron_json from mm_cron where mm_cron_enabled = true", &[])
