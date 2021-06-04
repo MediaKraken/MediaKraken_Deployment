c65c10dfd6674ed339251926c949d7dad4cc3c26
SpootDev <spootdev@gmail.com>
2021-06-03 20:31:48 -0500

    rename

--- source_rust/test_modules/src/main.rs
+++ source_rust/test_modules/src/main.rs
@@ -1,6 +1,6 @@
-extern crate lib_logging;
+extern crate mk_lib_logging;
 
 fn main() {
    println!("inside main of test ");
-   lib_logging::mk_lib_logging::mk_logging_post_elk("info", "test message", "mk_test");
+   mk_lib_logging::mk_lib_logging::mk_logging_post_elk("info", "test message", "mk_test");
 }
