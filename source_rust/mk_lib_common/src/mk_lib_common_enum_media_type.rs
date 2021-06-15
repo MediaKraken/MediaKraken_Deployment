4c3ddc12acf5e2cd312b15c545be992d81d7f84d
SpootDev <spootdev@gmail.com>
2021-06-14 22:15:26 -0500

    new

---
+++ source_rust/mk_lib_common/src/mk_lib_common_enum_media_type.rs
@@ -0,0 +1,50 @@
+enum DLMediaType {
+    Movie = 1, // "Movie"
+    TV = 2, // "TV: Show"
+    Person = 3,
+
+    Sports = 4, // "Sports"
+    Game = 5, // "Video Game"
+    Publication = 6,
+    Picture = 7, // "Picture"
+    Anime = 8, // "Anime"
+    Music = 9, // "Music"
+    Adult = 10,
+
+    AdultImage = 1000,
+    AdultMovie = 1001, // "Movie: Adult"
+    AdultScene = 1002,
+
+    GameCHD = 501, // "Game CHD"
+    GameCinematics = 502,
+    GameIntro = 503, // "Video Game: Intro"
+    GameISO = 504, // "Game ISO"
+    GameROM = 505, // "Game ROM"
+    GameSpeedrun = 506, // "Video Game: Speedrun"
+    GameSuperplay = 507, // "Video Game: Superplay"
+
+    MovieHome = 111, // "Home Movie"
+    MovieExtras = 112, // "Movie: Extras"
+    MovieSoundtrack = 113,
+    MovieSubtitle = 114, // "Movie: Subtitle"
+    MovieTheme = 115, // "Movie: Theme"
+    MovieTrailer = 116, // "Movie: Trailer"
+
+    MusicAlbum = 901, // "Music Album"
+    MusicLyrics = 902, // "Music Lyric"
+    MusicSong = 903,
+    MusicVideo = 904, // "Music Video"
+
+    PublicationBook = 601, // "Book"
+    PublicationComic = 602, // "Comic"
+    PublicationComicStrip = 603, // "Comic Strip"
+    PublicationMagazine = 604, // "Magazine"
+    PublicationGraphicNovel = 605,  // "Graphic Novel"
+
+    TVEpisode = 201, // "TV: Episode"
+    TVExtras = 202, // "TV: Extras"
+    TVSeason = 203, // "TV: Season"
+    TVSubtitle = 204, // "TV: Subtitle"
+    TVTheme = 205, // "TV: Theme"
+    TVTrailer = 206, // "TV: Trailer"
+}
