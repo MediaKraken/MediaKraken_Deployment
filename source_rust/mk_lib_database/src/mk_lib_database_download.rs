72c44007924bb99e6bf924af97d35181cff17c83
SpootDev <spootdev@gmail.com>
2021-06-14 19:27:33 -0500

    new call

--- source_rust/mk_lib_database/src/mk_lib_database_download.rs
+++ source_rust/mk_lib_database/src/mk_lib_database_download.rs
@@ -1,3 +1,15 @@
+pub async fn mk_lib_database_downloads_exists(client: &tokio_postgres::Client,
+                                              metadata_id: i32)
+                                              -> Result<bool, Error> {
+    let row = client
+        .query_one("select exists(select 1 from mm_download_que \
+        where mdq_provider_id = $1 limit 1) as found_record limit 1",
+                   &[&metadata_id]).await?;
+    println!("row: {:?}", row);
+    let id: bool = row.get("found_record");
+    Ok(id)
+}
+
 // // cargo test -- --show-output
 // #[cfg(test)]
 // mod test_mk_lib_common {
