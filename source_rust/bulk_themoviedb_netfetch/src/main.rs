58148d66aa409e3cf61285e9fef4d0acce42ec42
SpootDev <spootdev@gmail.com>
2021-06-14 22:16:08 -0500

    incoude download code

--- source_rust/bulk_themoviedb_netfetch/src/main.rs
+++ source_rust/bulk_themoviedb_netfetch/src/main.rs
@@ -1,7 +1,4 @@
-//use std::collections::HashMap;
 use serde::{Deserialize, Serialize};
-// launch the vs tools app otherwise can't compile (windows os only)
-//use serde_json::{Value};
 use std::error::Error;
 
 #[path = "../../mk_lib_common/src/mk_lib_common.rs"]
@@ -10,6 +7,8 @@ mod mk_lib_common;
 mod mk_lib_compression;
 #[path = "../../mk_lib_database/src/mk_lib_database.rs"]
 mod mk_lib_database;
+#[path = "../../mk_lib_database/src/mk_lib_database_download.rs"]
+mod mk_lib_database_download;
 #[path = "../../mk_lib_database/src/mk_lib_database_metadata.rs"]
 mod mk_lib_database_metadata;
 #[path = "../../mk_lib_logging/src/mk_lib_logging.rs"]
@@ -18,12 +17,19 @@ mod mk_lib_logging;
 mod mk_lib_networks;
 
 #[derive(Serialize, Deserialize)]
-struct Metadata {
+struct MetadataMovie {
     adult: bool,
     id: i32,
     original_title: String,
     popularity: f32,
     video: bool,
+}
+
+#[derive(Serialize, Deserialize)]
+struct MetadataTV {
+    id: i32,
+    original_name: String,
+    popularity: f32,
 }
 
 #[tokio::main]
@@ -35,47 +41,37 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     let fetch_date: String = "06_11_2021".to_string();
 
-    // // open the database
+    // open the database
     let db_client = &mk_lib_database::mk_lib_database_open().await?;
-    println!("here after exit of open");
-    mk_lib_common::print_type_of_variable(&db_client);
 
     // grab the movie id's
     // let _fetch_result = mk_lib_networks::mk_download_file_from_url(
     //     format!("http://files.tmdb.org/p/exports/movie_ids_{}.json.gz", fetch_date),
     //     "movie.gz".to_string());
-    println!("b4json");
     let json_result = mk_lib_compression::mk_decompress_gzip("movie.gz").unwrap();
-    println!("afterjson");
-    // Parse the string of data into serde_json::Value.
     // Please note that the data is NOT in id order
     for json_item in json_result.split('\n') {
-        let metadata_struct: Metadata = serde_json::from_str(json_item)?;
-        // println!("Id: {} Adult: {}", metadata_struct.id, metadata_struct.adult);
-        // mk_lib_common::print_type_of_variable(&metadata_struct.id);
+        let metadata_struct: MetadataMovie = serde_json::from_str(json_item)?;
         let result = mk_lib_database_metadata::mk_lib_database_metadata_exists_movie(db_client,
                                                                                      metadata_struct.id).await.unwrap();
         if result == false {
-            println!("waffles! {}", p.id)
+            println!("waffles! {}", metadata_struct.id)
         }
     }
-    //
-    // // grab the TV id's
+
+    // grab the TV id's
     // let _fetch_result = mk_lib_networks::mk_download_file_from_url(
     //     format!("http://files.tmdb.org/p/exports/tv_series_ids_{}.json.gz", fetch_date),
     //     "tv.gz".to_string());
-    // let json_result = mk_lib_compression::mk_decompress_gzip("tv.gz").unwrap();
-    // // Parse the string of data into serde_json::Value.
-    // // Please note that the data is NOT in id order
-    // for json_item in json_result.split('\n') {
-    //     let json_line: HashMap<String, Value> = serde_json::from_str(&json_item).unwrap();
-    //     println!("{:?}, {:?}", json_line["id"], json_line["adult"]);
-    //     if mk_lib_database_metadata::mk_lib_database_metadata_exists_tv(db_client,
-    //                                                                        &json_line["id"].as_str().unwrap()) == 1 {
-    //
-    //     }
-    // }
-
+    let json_result = mk_lib_compression::mk_decompress_gzip("tv.gz").unwrap();
+    for json_item in json_result.split('\n') {
+        let metadata_struct: MetadataTV = serde_json::from_str(json_item)?;
+        let result = mk_lib_database_metadata::mk_lib_database_metadata_exists_tv(db_client,
+                                                                                  metadata_struct.id).await.unwrap();
+        if result == false {
+            println!("waffles! {}", metadata_struct.id)
+        }
+    }
 
     // let name = "Ferris";
     // let data = None::<&[u8]>;
--- source_rust/mk_lib_database/src/mk_lib_database_download.rs
+++ source_rust/mk_lib_database/src/mk_lib_database_download.rs
@@ -1,6 +1,9 @@
-pub async fn mk_lib_database_downloads_exists(client: &tokio_postgres::Client,
-                                              metadata_id: i32)
-                                              -> Result<bool, Error> {
+use tokio_postgres::{Error};
+use uuid::Uuid;
+
+pub async fn mk_lib_database_download_exists(client: &tokio_postgres::Client,
+                                             metadata_id: i32)
+                                             -> Result<bool, Error> {
     let row = client
         .query_one("select exists(select 1 from mm_download_que \
         where mdq_provider_id = $1 limit 1) as found_record limit 1",
@@ -9,6 +12,37 @@ pub async fn mk_lib_database_downloads_exists(client: &tokio_postgres::Client,
     let id: bool = row.get("found_record");
     Ok(id)
 }
+
+pub async fn mk_lib_database_download_insert(client: &tokio_postgres::Client,
+                                             metadata_id: i32,
+                                             metadata_provider: String,
+                                             metadata_que_type: u8)
+                                             -> Result<bool, Error> {
+    let row = client
+        .query_one("insert into mm_download_que (mdq_id, \
+        mdq_provider, \
+        mdq_que_type, \
+        mdq_new_uuid, \
+        mdq_provider_id, \
+        mdq_status) \
+        values ($1, $2, $3, $4, $5, $6)",
+                   &[&Uuid::new_v4(), &metadata_id]).await?;
+    println!("row download insert: {:?}", row);
+    let id: bool = row.get("found_record");
+    Ok(id)
+}
+
+// Uuid::new_v4();
+// new_guid = uuid.uuid4()
+// await db_conn.execute('insert into mm_download_que (mdq_id,'
+//                       ' mdq_provider,'
+//                       ' mdq_que_type,'
+//                       ' mdq_new_uuid,'
+//                       ' mdq_provider_id,'
+//                       ' mdq_status)'
+//                       ' values ($1, $2, $3, $4, $5, $6)',
+//                       new_guid, provider, que_type, down_new_uuid,
+//                       down_json['ProviderMetaID'], down_json['Status'])
 
 // // cargo test -- --show-output
 // #[cfg(test)]
