a0d68dd936080fed281cb706bc9ada54395c74d0
SpootDev <spootdev@gmail.com>
2021-06-14 19:23:28 -0500

    pep

--- source_rust/mk_lib_logging/src/mk_lib_logging.rs
+++ source_rust/mk_lib_logging/src/mk_lib_logging.rs
@@ -6,7 +6,7 @@ pub async fn mk_logging_post_elk(message_type: &str, message_text: &str,
     let data = serde_json::json!({"@timestamp": utc.format("%Y-%m-%dT%H:%M:%S.%f").to_string(),
         "message": message_text, "type": message_type, "user": {"id": "metaman"}});
     let client = reqwest::Client::new();
-    let resp = client.post(format!("http://th-elk-1.beaverbay.local:9200/{}/MediaKraken",
+    let _resp = client.post(format!("http://th-elk-1.beaverbay.local:9200/{}/MediaKraken",
                                    index_name))
         .header("Content-Type", "application/json")
         .json(&data)
