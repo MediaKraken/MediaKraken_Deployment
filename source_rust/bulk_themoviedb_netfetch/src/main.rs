e85573f351b8086b94ce62dfd8fd8caf75ffb57d
SpootDev <spootdev@gmail.com>
2021-06-15 19:25:57 -0500

    check lines, download que, etc

--- source_rust/bulk_themoviedb_netfetch/src/main.rs
+++ source_rust/bulk_themoviedb_netfetch/src/main.rs
@@ -1,5 +1,6 @@
 use serde::{Deserialize, Serialize};
 use std::error::Error;
+use uuid::Uuid;
 
 #[path = "../../mk_lib_common/src/mk_lib_common.rs"]
 mod mk_lib_common;
@@ -41,53 +42,67 @@ async fn main() -> Result<(), Box<dyn Error>> {
                                         "START",
                                         "bulk_themoviedb_netfetch").await;
 
-    let fetch_date: String = "06_11_2021".to_string();
+    let fetch_date: String = "06_13_2021".to_string();
 
     // open the database
     let db_client = &mk_lib_database::mk_lib_database_open().await?;
 
     // grab the movie id's
-    // let _fetch_result = mk_lib_networks::mk_download_file_from_url(
-    //     format!("http://files.tmdb.org/p/exports/movie_ids_{}.json.gz", fetch_date),
-    //     "movie.gz".to_string());
+    let _fetch_result_movie = mk_lib_networks::mk_download_file_from_url(
+        format!("http://files.tmdb.org/p/exports/movie_ids_{}.json.gz", fetch_date),
+        "movie.gz".to_string());
     let json_result = mk_lib_compression::mk_decompress_gzip("movie.gz").unwrap();
     // Please note that the data is NOT in id order
     for json_item in json_result.split('\n') {
-        let metadata_struct: MetadataMovie = serde_json::from_str(json_item)?;
-        let result = mk_lib_database_metadata::mk_lib_database_metadata_exists_movie(db_client,
-                                                                                     metadata_struct.id).await.unwrap();
-        if result == false {
-            println!("waffles! {}", metadata_struct.id)
+        if !json_item.trim().is_empty() {
+            let metadata_struct: MetadataMovie = serde_json::from_str(json_item)?;
+            let result = mk_lib_database_metadata::mk_lib_database_metadata_exists_movie(db_client,
+                                                                                         metadata_struct.id).await.unwrap();
+            if result == false {
+                let download_result = mk_lib_database_download::mk_lib_database_download_exists(db_client,
+                                                                                                "themoviedb".to_string(),
+                                                                                                mk_lib_common_enum_media_type::DLMediaType::MOVIE,
+                                                                                                metadata_struct.id).await.unwrap();
+                if download_result == false {
+                    mk_lib_database_download::mk_lib_database_download_insert(db_client,
+                                                                              "themoviedb".to_string(),
+                                                                              mk_lib_common_enum_media_type::DLMediaType::MOVIE,
+                                                                              Uuid::new_v4(),
+                                                                              metadata_struct.id,
+                                                                              "Fetch".to_string(),
+                    ).await;
+                }
+            }
         }
     }
 
     // grab the TV id's
-    // let _fetch_result = mk_lib_networks::mk_download_file_from_url(
-    //     format!("http://files.tmdb.org/p/exports/tv_series_ids_{}.json.gz", fetch_date),
-    //     "tv.gz".to_string());
+    let _fetch_result_tv = mk_lib_networks::mk_download_file_from_url(
+        format!("http://files.tmdb.org/p/exports/tv_series_ids_{}.json.gz", fetch_date),
+        "tv.gz".to_string());
     let json_result = mk_lib_compression::mk_decompress_gzip("tv.gz").unwrap();
     for json_item in json_result.split('\n') {
-        let metadata_struct: MetadataTV = serde_json::from_str(json_item)?;
-        let result = mk_lib_database_metadata::mk_lib_database_metadata_exists_tv(db_client,
-                                                                                  metadata_struct.id).await.unwrap();
-        if result == false {
-            println!("waffles! {}", metadata_struct.id)
+        if !json_item.trim().is_empty() {
+            let metadata_struct: MetadataTV = serde_json::from_str(json_item)?;
+            let result = mk_lib_database_metadata::mk_lib_database_metadata_exists_tv(db_client,
+                                                                                      metadata_struct.id).await.unwrap();
+            if result == false {
+                let download_result = mk_lib_database_download::mk_lib_database_download_exists(db_client,
+                                                                                                "themoviedb".to_string(),
+                                                                                                mk_lib_common_enum_media_type::DLMediaType::TV,
+                                                                                                metadata_struct.id).await.unwrap();
+                if download_result == false {
+                    mk_lib_database_download::mk_lib_database_download_insert(db_client,
+                                                                              "themoviedb".to_string(),
+                                                                              mk_lib_common_enum_media_type::DLMediaType::TV,
+                                                                              Uuid::new_v4(),
+                                                                              metadata_struct.id,
+                                                                              "Fetch".to_string(),
+                    ).await;
+                }
+            }
         }
     }
-
-    // let name = "Ferris";
-    // let data = None::<&[u8]>;
-    // client.execute(
-    //     "INSERT INTO person (name, data) VALUES ($1, $2)",
-    //     &[&name, &data],
-    // )?;
-    //
-    // for row in client.query("SELECT id, name, data FROM person", &[])? {
-    //     let id: i32 = row.get(0);
-    //     let name: &str = row.get(1);
-    //     let data: Option<&[u8]> = row.get(2);
-    //     println!("found person: {} {} {:?}", id, name, data);
-    // }
 
     // stop logging
     mk_lib_logging::mk_logging_post_elk("info",
