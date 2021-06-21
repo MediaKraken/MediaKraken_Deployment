8aa56007839341f351d6e8c1a72271467a114120
SpootDev <spootdev@gmail.com>
2021-06-20 22:04:39 -0500

    missing }

--- docker/alpine/ComposeMediaKrakenBroadcastRust/src/main.rs
+++ docker/alpine/ComposeMediaKrakenBroadcastRust/src/main.rs
@@ -14,14 +14,15 @@ async fn main() -> io::Result<()> {
                 if source_ip.is_ipv4() {
                     println!("{:?}", source_ip);
                     let mediakraken_ip = iface.ips.iter().find(|ip| ip.is_ipv4())
-                    .map(|ip| match ip.ip() {
-                        IpAddr::V4(ip) => ip,
-                        _ => unreachable!(),
-                    }).unwrap();
+                        .map(|ip| match ip.ip() {
+                            IpAddr::V4(ip) => ip,
+                            _ => unreachable!(),
+                        }).unwrap();
                     println!("{:?}", mediakraken_ip);
                 }
             }
         }
+    }
     let mut host_port: u64 = 8903;
 
     // Grab public port that the reactor is running on
