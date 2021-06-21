705f0fc58e73c3c437cb76e2b3d3f7dbe431dbba
SpootDev <spootdev@gmail.com>
2021-06-20 21:58:02 -0500

    pull out ens18 ip v4

--- source_rust/testing_rust/test_host_ips/src/main.rs
+++ source_rust/testing_rust/test_host_ips/src/main.rs
@@ -1,4 +1,5 @@
 use pnet::datalink;
+use std::net::{AddrParseError, IpAddr, Ipv4Addr};
 
 // "lo" [V4(Ipv4Network { addr: 127.0.0.1, prefix: 8 }), V6(Ipv6Network { addr: ::1, prefix: 128 })]
 // "ens18" [V4(Ipv4Network { addr: 10.0.0.159, prefix: 24 }), V6(Ipv6Network { addr: fe80::a860:feff:fe84:2a0f, prefix: 64 })]
@@ -10,6 +11,26 @@ use pnet::datalink;
 
 fn main() {
     for iface in datalink::interfaces() {
-        println!("{:?} {:?}", iface.name, iface.ips[0]);
+        // println!("{:?} {:?} {:?}", iface.name, iface.ips[0], iface);
+        if iface.name == "ens18" {
+            for source_ip in iface.ips.iter() {
+                if source_ip.is_ipv4() {
+                    println!("{:?}", source_ip);
+                    let source_ip = iface.ips.iter().find(|ip| ip.is_ipv4())
+                    .map(|ip| match ip.ip() {
+                        IpAddr::V4(ip) => ip,
+                        _ => unreachable!(),
+                    }).unwrap();
+                    println!("{:?}", source_ip);
+                }
+            }
+        }
+        // following line bombs when there is no V4, only V6
+        //let source_ip = iface.ips.iter().find(|ip| ip.is_ipv4())
+        // .map(|ip| match ip.ip() {
+        //     IpAddr::V4(ip) => ip,
+        //     _ => unreachable!(),
+        // }).unwrap();
+        //println!("{:?}", source_ip);
     }
 }
