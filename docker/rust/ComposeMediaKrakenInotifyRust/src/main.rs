cd1850ede8e6597461c5cc483ed4c8eaaccd2d1d
SpootDev <spootdev@gmail.com>
2021-07-01 16:52:16 -0500

    add rabbit events

--- docker/rust/ComposeMediaKrakenInotifyRust/src/main.rs
+++ docker/rust/ComposeMediaKrakenInotifyRust/src/main.rs
@@ -1,3 +1,4 @@
+use amiquip::{AmqpProperties, Connection, Exchange, Publish, Result};
 use inotify::{
     EventMask,
     Inotify,
@@ -35,6 +36,15 @@ async fn main() -> Result<(), Box<dyn Error>> {
     // open the database
     let db_client = &mk_lib_database::mk_lib_database_open().await?;
 
+    // open rabbit connection
+    let mut rabbit_connection = Connection::insecure_open(
+        "amqp://guest:guest@mkstack_rabbitmq:5672")?;
+    // Open a channel - None says let the library choose the channel ID.
+    let rabbit_channel = rabbit_connection.open_channel(None)?;
+
+    // Get a handle to the direct exchange on our channel.
+    let rabbit_exchange = Exchange::direct(&rabbit_channel);
+
     let mut inotify = Inotify::init()
         .expect("Failed to initialize inotify");
 
@@ -46,8 +56,6 @@ async fn main() -> Result<(), Box<dyn Error>> {
         ).expect("Failed to add inotify watch");
     }
 
-    println!("Watching current directory for activity...");
-
     let mut buffer = [0u8; 4096];
     loop {
         let events = inotify.read_events_blocking(&mut buffer)
@@ -56,20 +64,38 @@ async fn main() -> Result<(), Box<dyn Error>> {
         for event in events {
             if event.mask.contains(EventMask::CREATE) {
                 if event.mask.contains(EventMask::ISDIR) {
+                    rabbit_exchange.publish(Publish::with_properties(format!("{'Type': 'Dir Create', 'JSON': {:?}}}", event.name).as_bytes(),
+                                                                     "mkinotify".to_string(),
+                                                                     AmqpProperties::default().with_delivery_mode(2).with_content_type("text/plain".to_string())))?;
                     println!("Directory created: {:?}", event.name);
                 } else {
+                    rabbit_exchange.publish(Publish::with_properties(format!("{'Type': 'File Create', 'JSON': {:?}}}", event.name).as_bytes(),
+                                                                     "mkinotify".to_string(),
+                                                                     AmqpProperties::default().with_delivery_mode(2).with_content_type("text/plain".to_string())))?;
                     println!("File created: {:?}", event.name);
                 }
             } else if event.mask.contains(EventMask::DELETE) {
                 if event.mask.contains(EventMask::ISDIR) {
+                    rabbit_exchange.publish(Publish::with_properties(format!("{'Type': 'Dir Delete', 'JSON': {:?}}}", event.name).as_bytes(),
+                                                                     "mkinotify".to_string(),
+                                                                     AmqpProperties::default().with_delivery_mode(2).with_content_type("text/plain".to_string())))?;
                     println!("Directory deleted: {:?}", event.name);
                 } else {
+                    rabbit_exchange.publish(Publish::with_properties(format!("{'Type': 'File Delete', 'JSON': {:?}}}", event.name).as_bytes(),
+                                                                     "mkinotify".to_string(),
+                                                                     AmqpProperties::default().with_delivery_mode(2).with_content_type("text/plain".to_string())))?;
                     println!("File deleted: {:?}", event.name);
                 }
             } else if event.mask.contains(EventMask::MODIFY) {
                 if event.mask.contains(EventMask::ISDIR) {
+                    rabbit_exchange.publish(Publish::with_properties(format!("{'Type': 'Dir Modify', 'JSON': {:?}}}", event.name).as_bytes(),
+                                                                     "mkinotify".to_string(),
+                                                                     AmqpProperties::default().with_delivery_mode(2).with_content_type("text/plain".to_string())))?;
                     println!("Directory modified: {:?}", event.name);
                 } else {
+                    rabbit_exchange.publish(Publish::with_properties(format!("{'Type': 'File Modify', 'JSON': {:?}}}", event.name).as_bytes(),
+                                                                     "mkinotify".to_string(),
+                                                                     AmqpProperties::default().with_delivery_mode(2).with_content_type("text/plain".to_string())))?;
                     println!("File modified: {:?}", event.name);
                 }
             }
