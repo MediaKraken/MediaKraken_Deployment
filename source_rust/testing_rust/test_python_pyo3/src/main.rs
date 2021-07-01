1b6d43f02a373b04ebcc12992955e326b3d14800
SpootDev <spootdev@gmail.com>
2021-07-01 18:27:59 -0500

    main rust

--- source_rust/testing_rust/test_python_pyo3/src/main.rs
+++ source_rust/testing_rust/test_python_pyo3/src/main.rs
@@ -1,16 +1,22 @@
 use pyo3::prelude::*;
-use pyo3::wrap_pyfunction;
+use pyo3::types::IntoPyDict;
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+fn main() -> Result<(), ()> {
+    Python::with_gil(|py| {
+        main_(py).map_err(|e| {
+          // We can't display Python exceptions via std::fmt::Display,
+          // so print the error here manually.
+          e.print_and_set_sys_last_vars(py);
+        })
+    })
 }
 
-/// A Python module implemented in Rust.
-#[pymodule]
-fn string_sum(py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
-
+fn main_(py: Python) -> PyResult<()> {
+    let sys = py.import("sys")?;
+    let version: String = sys.get("version")?.extract()?;
+    let locals = [("os", py.import("os")?)].into_py_dict(py);
+    let code = "os.getenv('USER') or os.getenv('USERNAME') or 'Unknown'";
+    let user: String = py.eval(code, None, Some(&locals))?.extract()?;
+    println!("Hello {}, I'm Python {}", user, version);
     Ok(())
 }
