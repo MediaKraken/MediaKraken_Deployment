7bc653b276ac0829412703d4e90f1c8726ec84a4
SpootDev <spootdev@gmail.com>
2021-07-02 11:41:08 -0500

    test container

---
+++ docker/rust/ComposeMediaKrakenTESTReqwestRust/src/main.rs
@@ -0,0 +1,20 @@
+#[cfg(debug_assertions)]
+#[path = "../../../../source_rust/mk_lib_network/src/mk_lib_network.rs"]
+mod mk_lib_network;
+
+#[cfg(not(debug_assertions))]
+#[path = "mk_lib_network.rs"]
+mod mk_lib_network;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let fetch_date: String = "06_20_2021".to_string();
+
+    // grab the movie id's
+    // files.tmdb.org = 13.227.42.62
+    let _fetch_result_movie = mk_lib_network::mk_download_file_from_url(
+        format!("http://files.tmdb.org/p/exports/movie_ids_{}.json.gz", fetch_date),
+        "/myapp/movie.gz".to_string()).await;
+    let json_result = mk_lib_compression::mk_decompress_gzip("/myapp/movie.gz").unwrap();
+    Ok(())
+}
