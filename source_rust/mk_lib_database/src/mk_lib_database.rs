e3cb29fbee0fc96376c7f6f5d1377e6beccff6cc
SpootDev <spootdev@gmail.com>
2021-07-04 18:58:36 -0500

    option/status read

--- source_rust/mk_lib_database/src/mk_lib_database.rs
+++ source_rust/mk_lib_database/src/mk_lib_database.rs
@@ -32,19 +32,21 @@ pub async fn mk_lib_database_open() -> Result<tokio_postgres::Client, Error> {
     Ok(client)
 }
 
-// pub async fn mk_lib_database_options(client: tokio_postgres::Client) -> Result<(), Error> {
-//     let row = client
-//         .query_one("select mm_options_json from mm_options_and_status", &[])
-//         .await?;
-//     let mm_options_json: &str = row.try_get::<&str, serde_json::Value>("mm_options_json")?;
-// }
-//
-// pub async fn mk_lib_database_status(client: tokio_postgres::Client) -> Result<(), Error> {
-//     let row = client
-//         .query_one("select mm_status_json from mm_options_and_status", &[])
-//         .await?;
-//     let mm_status_json: &str = row.try_get::<&str, serde_json::Value>("mm_status_json")?;
-// }
+pub async fn mk_lib_database_options(client: tokio_postgres::Client) -> Result<(), Error> {
+    let row = client
+        .query_one("select mm_options_json from mm_options_and_status", &[])
+        .await?;
+    let mm_options_json: &str = row.try_get::<&str, serde_json::Value>("mm_options_json")?;
+    Ok(mm_options_json)
+}
+
+pub async fn mk_lib_database_status(client: tokio_postgres::Client) -> Result<(), Error> {
+    let row = client
+        .query_one("select mm_status_json from mm_options_and_status", &[])
+        .await?;
+    let mm_status_json: &str = row.try_get::<&str, serde_json::Value>("mm_status_json")?;
+    Ok(mm_status_json)
+}
 
 // // cargo test -- --show-output
 // #[cfg(test)]
