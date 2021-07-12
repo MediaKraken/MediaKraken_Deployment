f5ca9d6c2d1c1db5b568d575f04359875976626d
SpootDev <spootdev@gmail.com>
2021-07-12 18:16:37 -0500

    status updates

--- source_rust/mk_lib_database/src/mk_lib_database_library.rs
+++ source_rust/mk_lib_database/src/mk_lib_database_library.rs
@@ -28,6 +28,36 @@ pub async fn mk_lib_database_library_path_status(client: &tokio_postgres::Client
     Ok(rows)
 }
 
+
+pub async fn mk_lib_database_library_path_status_update(client: &tokio_postgres::Client,
+                                                        library_uuid: uuid::Uuid,
+                                                        library_status_json: serde_json::Value)
+                                                        -> Result<(), Error> {
+    client.query("update mm_media_dir set mm_media_dir_status = $1 where mm_media_dir_guid = $2",
+                 &[&library_status_json, &library_uuid]).await?;
+    Ok(())
+}
+
+pub async fn mk_lib_database_library_path_timestamp_update(client: &tokio_postgres::Client,
+                                                           library_uuid: uuid::Uuid)
+                                                           -> Result<(), Error> {
+    client.query("update mm_media_dir set mm_media_dir_last_scanned = NOW() \
+     where mm_media_dir_guid = $1)",
+                 &[&library_uuid]).await?;
+    Ok(())
+}
+
+pub async fn mk_lib_database_library_file_exists(client: &tokio_postgres::Client,
+                                                 file_name: String)
+                                                 -> Result<bool, Error> {
+    let row = client
+        .query_one("select exists(select 1 from mm_media \
+        where mm_media_path = $1 limit 1) \
+        as found_record limit 1", &[&file_name]).await?;
+    let id: bool = row.get("found_record");
+    Ok(id)
+}
+
 // // cargo test -- --show-output
 // #[cfg(test)]
 // mod test_mk_lib_common {
