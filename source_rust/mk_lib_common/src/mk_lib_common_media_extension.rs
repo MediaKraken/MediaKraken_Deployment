efb16e42c98018dca8dc6a3b55dbac1c4d584776
SpootDev <spootdev@gmail.com>
2021-07-12 18:16:43 -0500

    new

---
+++ source_rust/mk_lib_common/src/mk_lib_common_media_extension.rs
@@ -0,0 +1,68 @@
+static MEDIA_EXTENSION: [String; 31] = [
+    "webm",
+    "mkv",
+    "flv",
+    "vob",
+    "ogv",
+    "ogg",
+    "drc",
+    "mng",
+    "avi",
+    "mov",
+    "qt",
+    "wmv",
+    "wma",
+    "yuv",
+    "rm",
+    "rmvb",
+    "asf",
+    "mp4",
+    "m4p",
+    "m4v",
+    "mpg",
+    "mp2",
+    "mpeg",
+    "mpe",
+    "mp3",
+    "flac",
+    "mpv",
+    "m2v",
+    "m4v",
+    "nsv",
+    "pdf",
+    "lrc",
+];
+
+static SUBTITLE_EXTENSION: [String; 7] = [
+    "srt",
+    "smi",
+    "ssa",
+    "ass",
+    "vtt",
+    "sub",
+    "idx",
+];
+
+static MEDIA_EXTENSION_SKIP_FFMPEG: [String; 6] = [
+    "pdf",
+    "zip",
+    "7z",
+    "iso",
+    "chd",
+    "lrc",
+];
+
+static GAME_EXTENSION: [String; 5] = [
+    "iso",
+    "chd",
+    "zip",
+    "7z",
+    "rar",
+];
+
+static COMIC_BOOK_EXTENSION: [String; 4] = [
+    "cbr",
+    "cbz",
+    "cbt",
+    "pdf"
+];
